2a09a3e2aa9a36741c9fa43caa125e601019c503
Shawn O. Pearce <sop@google.com>
2008-11-14 16:56:58 -0800

    Initial project setup of Gerrit 2
    Gerrit 2 is a ground-up rewrite of Gerrit, using GWT (Google Web
    Toolkit) for the client side user interface and a Java servlet
    based backend.
    
    Signed-off-by: Shawn O. Pearce <sop@google.com>
    
    Former-commit-id: 23571ab1fa7fedc262d6c21510614353e9d8a4dc

---
+++ webapp/src/com/google/gerrit/client/GerritConstants.java
@@ -0,0 +1,145 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright 2008 Google Inc.
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.client
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|client
+package|;
+end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gwt
+operator|.
+name|i18n
+operator|.
+name|client
+operator|.
+name|Constants
+import|;
+end_import
+
+begin_interface
+DECL|interface|GerritConstants
+specifier|public
+interface|interface
+name|GerritConstants
+extends|extends
+name|Constants
+block|{
+DECL|method|leftMenuCodeReviews ()
+name|String
+name|leftMenuCodeReviews
+parameter_list|()
+function_decl|;
+DECL|method|menuMyChanges ()
+name|String
+name|menuMyChanges
+parameter_list|()
+function_decl|;
+DECL|method|menuMyUnclaimedChanges ()
+name|String
+name|menuMyUnclaimedChanges
+parameter_list|()
+function_decl|;
+DECL|method|menuAllUnclaimedChanges ()
+name|String
+name|menuAllUnclaimedChanges
+parameter_list|()
+function_decl|;
+DECL|method|menuAllRecentChanges ()
+name|String
+name|menuAllRecentChanges
+parameter_list|()
+function_decl|;
+DECL|method|menuMyStarredChanges ()
+name|String
+name|menuMyStarredChanges
+parameter_list|()
+function_decl|;
+DECL|method|leftMenuAdmin ()
+name|String
+name|leftMenuAdmin
+parameter_list|()
+function_decl|;
+DECL|method|menuPeople ()
+name|String
+name|menuPeople
+parameter_list|()
+function_decl|;
+DECL|method|menuGroups ()
+name|String
+name|menuGroups
+parameter_list|()
+function_decl|;
+DECL|method|menuProjects ()
+name|String
+name|menuProjects
+parameter_list|()
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
---
+++ webapp/src/com/google/gerrit/client/changes/ChangeConstants.java
@@ -0,0 +1,157 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright 2008 Google Inc.
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.client.changes
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|client
+operator|.
+name|changes
+package|;
+end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gwt
+operator|.
+name|i18n
+operator|.
+name|client
+operator|.
+name|Constants
+import|;
+end_import
+
+begin_interface
+DECL|interface|ChangeConstants
+specifier|public
+interface|interface
+name|ChangeConstants
+extends|extends
+name|Constants
+block|{
+DECL|method|mineHeading ()
+name|String
+name|mineHeading
+parameter_list|()
+function_decl|;
+DECL|method|mineByMe ()
+name|String
+name|mineByMe
+parameter_list|()
+function_decl|;
+DECL|method|mineForReview ()
+name|String
+name|mineForReview
+parameter_list|()
+function_decl|;
+DECL|method|mineClosed ()
+name|String
+name|mineClosed
+parameter_list|()
+function_decl|;
+DECL|method|starredHeading ()
+name|String
+name|starredHeading
+parameter_list|()
+function_decl|;
+DECL|method|changeTableColumnID ()
+name|String
+name|changeTableColumnID
+parameter_list|()
+function_decl|;
+DECL|method|changeTableColumnSubject ()
+name|String
+name|changeTableColumnSubject
+parameter_list|()
+function_decl|;
+DECL|method|changeTableColumnOwner ()
+name|String
+name|changeTableColumnOwner
+parameter_list|()
+function_decl|;
+DECL|method|changeTableColumnReviewers ()
+name|String
+name|changeTableColumnReviewers
+parameter_list|()
+function_decl|;
+DECL|method|changeTableColumnProject ()
+name|String
+name|changeTableColumnProject
+parameter_list|()
+function_decl|;
+DECL|method|changeTableColumnLastUpdate ()
+name|String
+name|changeTableColumnLastUpdate
+parameter_list|()
+function_decl|;
+DECL|method|changeTableNone ()
+name|String
+name|changeTableNone
+parameter_list|()
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
