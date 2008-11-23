d587b099a85f5bfe0e7e06dfe3ac803e390330c8
Shawn O. Pearce <sop@google.com>
2008-11-22 21:19:40 -0800

    Create a basic ReviewDb schema and user Account entity
    Signed-off-by: Shawn O. Pearce <sop@google.com>
    
    Former-commit-id: f1c3801ee26ef1657406c1129caf1cc114c1efc9

---
+++ webapp/src/com/google/gerrit/client/reviewdb/ReviewDb.java
@@ -0,0 +1,142 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright 2008 Google Inc.
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.client.reviewdb
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|client
+operator|.
+name|reviewdb
+package|;
+end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gwtorm
+operator|.
+name|client
+operator|.
+name|Relation
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gwtorm
+operator|.
+name|client
+operator|.
+name|Schema
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gwtorm
+operator|.
+name|client
+operator|.
+name|Sequence
+import|;
+end_import
+
+begin_comment
+comment|/** The review service database schema. */
+end_comment
+
+begin_interface
+DECL|interface|ReviewDb
+specifier|public
+interface|interface
+name|ReviewDb
+extends|extends
+name|Schema
+block|{
+annotation|@
+name|Relation
+DECL|method|accounts ()
+name|AccountAccess
+name|accounts
+parameter_list|()
+function_decl|;
+comment|/** Create the next unique id for an {@link Account}. */
+annotation|@
+name|Sequence
+DECL|method|nextAccountId ()
+name|int
+name|nextAccountId
+parameter_list|()
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
