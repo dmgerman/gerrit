a810577b110d9de2a0c808c120c0deb21d8c84bd
Shawn O. Pearce <sop@google.com>
2008-12-04 09:23:14 -0800

    Send global system configuration data to the client on module load
    We need some key data about how this Gerrit has been setup on the
    client side; in particular the approval categories and their set
    of known values will be needed frequently in the different change
    display screens.  Caching this in a static within the page will
    reduce the size of each individual RPC, as we only need to send
    this information once.
    
    The data is usually shoved into the top of the host page, so it is
    available immediately when the module initializes itself.  If it
    isn't there an RPC is used to load the data, but this should only
    happen while debugging under hosted mode.
    
    Signed-off-by: Shawn O. Pearce <sop@google.com>
    
    Former-commit-id: 304ef2dcf5ea630bc799cc4f47d1cefeeebfc804

---
+++ webapp/src/com/google/gerrit/server/SystemInfoServiceSrv.java
@@ -0,0 +1,104 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright 2008 Google Inc.
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.server
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+package|;
+end_package
+
+begin_comment
+comment|/** Publishes {@link SystemInfoServiceImpl} over JSON. */
+end_comment
+
+begin_class
+DECL|class|SystemInfoServiceSrv
+specifier|public
+class|class
+name|SystemInfoServiceSrv
+extends|extends
+name|GerritJsonServlet
+block|{
+annotation|@
+name|Override
+DECL|method|createServiceHandle ()
+specifier|protected
+name|Object
+name|createServiceHandle
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+return|return
+operator|new
+name|SystemInfoServiceImpl
+argument_list|(
+name|GerritServer
+operator|.
+name|getInstance
+argument_list|()
+argument_list|)
+return|;
+block|}
+block|}
+end_class
+
+end_unit
+
