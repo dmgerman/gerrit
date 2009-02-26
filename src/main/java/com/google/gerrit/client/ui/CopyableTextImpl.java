a238650724f87324db007c96ebf2a1d2a66d6ab4
Shawn O. Pearce <sop@google.com>
2009-02-26 11:21:59 -0800

    Only load the flash clippy button if flash plugin is available
    If the browser doesn't have flash installed, just don't even
    offer the clippy support, and instead rely on the native JS
    based text box.
    
    Signed-off-by: Shawn O. Pearce <sop@google.com>
    
    Former-commit-id: fa6ee5de8ef0573c65be9aaa3edc566c24aadd5a

---
+++ src/main/java/com/google/gerrit/client/ui/CopyableTextImpl.java
@@ -0,0 +1,87 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright 2009 Google Inc.
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.client.ui
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|client
+operator|.
+name|ui
+package|;
+end_package
+
+begin_class
+DECL|class|CopyableTextImpl
+class|class
+name|CopyableTextImpl
+block|{
+DECL|method|inject (final CopyableText widget)
+name|void
+name|inject
+parameter_list|(
+specifier|final
+name|CopyableText
+name|widget
+parameter_list|)
+block|{   }
+block|}
+end_class
+
+end_unit
+
