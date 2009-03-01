794571812468be950d9927d3af7feef4ef04907e
Shawn O. Pearce <sop@google.com>
2009-03-01 14:23:56 -0800

    Add the break out magic used by Gerrit to prevent embedding in iframes
    This may be useful to applications that want to ensure they are
    only run from a top-level window context, where other pages can't
    obscure or present confusing UI, leading the user into doing an
    action they don't actually want to perform.
    
    Signed-off-by: Shawn O. Pearce <sop@google.com>
    
    Former-commit-id: 46a7b809dd1b2432dcb0136e58884ae47e29f796

--- src/main/java/com/google/gwtexpui/user/client/UserAgent.java
+++ src/main/java/com/google/gwtexpui/user/client/UserAgent.java
@@ -66,6 +66,38 @@ name|client
 package|;
 end_package
 
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gwt
+operator|.
+name|core
+operator|.
+name|client
+operator|.
+name|GWT
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gwt
+operator|.
+name|user
+operator|.
+name|client
+operator|.
+name|Window
+import|;
+end_import
+
 begin_comment
 comment|/**  * User agent feature tests we don't create permutations for.  *<p>  * Some features aren't worth creating full permutations in GWT for, as each new  * boolean permutation (only two settings) doubles the compile time required. If  * the setting only affects a couple of lines of JavaScript code, the slightly  * larger cache files for user agents that lack the functionality requested is  * trivial compared to the time developers lose building their application.  */
 end_comment
@@ -95,6 +127,63 @@ name|boolean
 name|hasFlash
 parameter_list|()
 comment|/*-{     if (navigator.plugins&& navigator.plugins.length) {       if (navigator.plugins['Shockwave Flash'])     return true;       if (navigator.plugins['Shockwave Flash 2.0']) return true;      } else if (navigator.mimeTypes&& navigator.mimeTypes.length) {       var mimeType = navigator.mimeTypes['application/x-shockwave-flash'];       if (mimeType&& mimeType.enabledPlugin) return true;      } else {       try { new ActiveXObject('ShockwaveFlash.ShockwaveFlash.7'); return true; } catch (e) {}       try { new ActiveXObject('ShockwaveFlash.ShockwaveFlash.6'); return true; } catch (e) {}       try { new ActiveXObject('ShockwaveFlash.ShockwaveFlash');   return true; } catch (e) {}     }     return false;   }-*/
+function_decl|;
+comment|/**    * Test for and disallow running this application in an&lt;iframe&gt;.    *<p>    * If the application is running within an iframe this method requests a    * browser generated redirect to pop the application out of the iframe into    * the top level window, and then aborts execution by throwing an exception.    * This is call should be placed early within the module's onLoad() method,    * before any real UI can be initialized that an attacking site could try to    * snip out and present in a confusing context.    *<p>    * If the break out works, execution will restart automatically in a proper    * top level window, where the script has full control over the display. If    * the break out fails, execution will abort and stop immediately, preventing    * UI widgets from being created, leaving the user with an empty frame.    */
+DECL|method|assertNotInIFrame ()
+specifier|public
+specifier|static
+name|void
+name|assertNotInIFrame
+parameter_list|()
+block|{
+if|if
+condition|(
+name|GWT
+operator|.
+name|isScript
+argument_list|()
+operator|&&
+name|amInsideIFrame
+argument_list|()
+condition|)
+block|{
+name|bustOutOfIFrame
+argument_list|(
+name|Window
+operator|.
+name|Location
+operator|.
+name|getHref
+argument_list|()
+argument_list|)
+expr_stmt|;
+throw|throw
+operator|new
+name|RuntimeException
+argument_list|()
+throw|;
+block|}
+block|}
+DECL|method|amInsideIFrame ()
+specifier|private
+specifier|static
+specifier|native
+name|boolean
+name|amInsideIFrame
+parameter_list|()
+comment|/*-{ return top.location != $wnd.location; }-*/
+function_decl|;
+DECL|method|bustOutOfIFrame (String newloc)
+specifier|private
+specifier|static
+specifier|native
+name|void
+name|bustOutOfIFrame
+parameter_list|(
+name|String
+name|newloc
+parameter_list|)
+comment|/*-{ top.location.href = newloc }-*/
 function_decl|;
 DECL|method|UserAgent ()
 specifier|private
