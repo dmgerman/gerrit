183de3d933b0ec504169083833b915050f375ff1
Shawn O. Pearce <sop@google.com>
2009-05-26 18:29:22 -0700

    Document some of the core database entity graph
    Signed-off-by: Shawn O. Pearce <sop@google.com>
    
    Former-commit-id: 7153febf6c9a2ed842545be0a635808fdc2ff349

--- src/main/java/com/google/gerrit/client/reviewdb/ReviewDb.java
+++ src/main/java/com/google/gerrit/client/reviewdb/ReviewDb.java
@@ -123,7 +123,7 @@ import|;
 end_import
 
 begin_comment
-comment|/** The review service database schema. */
+comment|/**  * The review service database schema.  *<p>  * Root entities that are at the top level of some important data graph:  *<ul>  *<li>{@link Project}: Configuration for a single Git repository.</li>  *<li>{@link Account}: Per-user account registration, preferences, identity.</li>  *<li>{@link Change}: All review information about a single proposed change.</li>  *<li>{@link SystemConfig}: Server-wide settings, managed by administrator.</li>  *</ul>  */
 end_comment
 
 begin_interface
