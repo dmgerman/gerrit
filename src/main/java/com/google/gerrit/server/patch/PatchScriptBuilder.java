dmgerman/gerrit

commit 54824fe1b88c74e1c32cfc5e5c5265a50a0b8b44

Always show comments in patch views, even if no edit exists
If a comment exists but is not within the context region of an edit,
we still want to ensure the comment shows to the reader, as it may
still be an important part of the review.

By creating a 0-length edit around a visible comment we ensure that
the packContent() method will format the necessary context lines to
the destination buffers, and we also ensure that the patch views in
the client UI will draw a hunk around the region.

Bug: GERRIT-66
Signed-off-by: Shawn O. Pearce <sop@google.com>

Former-commit-id: 974daeef21212e0f91c8f2b196b54ab1b9a97ab9

src/main/java/com/google/gerrit/server/patch/PatchScriptBuilder.java
@@ -108,9 +108,41 @@ name|gerrit
 operator|.
 name|client
 operator|.
+name|patches
+operator|.
+name|CommentDetail
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|client
+operator|.
 name|reviewdb
 operator|.
 name|Patch
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|client
+operator|.
+name|reviewdb
+operator|.
+name|PatchLineComment
 import|;
 end_import
 
@@ -326,6 +358,16 @@ name|java
 operator|.
 name|util
 operator|.
+name|Comparator
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
 name|List
 import|;
 end_import
@@ -366,6 +408,52 @@ name|PatchScriptBuilder
 operator|.
 name|class
 argument_list|)
+decl_stmt|;
+DECL|field|EDIT_SORT
+specifier|private
+specifier|static
+specifier|final
+name|Comparator
+argument_list|<
+name|Edit
+argument_list|>
+name|EDIT_SORT
+init|=
+operator|new
+name|Comparator
+argument_list|<
+name|Edit
+argument_list|>
+argument_list|()
+block|{
+annotation|@
+name|Override
+specifier|public
+name|int
+name|compare
+parameter_list|(
+specifier|final
+name|Edit
+name|o1
+parameter_list|,
+specifier|final
+name|Edit
+name|o2
+parameter_list|)
+block|{
+return|return
+name|o1
+operator|.
+name|getBeginA
+argument_list|()
+operator|-
+name|o2
+operator|.
+name|getBeginA
+argument_list|()
+return|;
+block|}
+block|}
 decl_stmt|;
 DECL|field|header
 specifier|private
@@ -503,13 +591,17 @@ operator|=
 name|c
 expr_stmt|;
 block|}
-DECL|method|toPatchScript (final DiffCacheContent content)
+DECL|method|toPatchScript (final DiffCacheContent content, final CommentDetail comments)
 name|PatchScript
 name|toPatchScript
 parameter_list|(
 specifier|final
 name|DiffCacheContent
 name|content
+parameter_list|,
+specifier|final
+name|CommentDetail
+name|comments
 parameter_list|)
 throws|throws
 name|CorruptEntityException
@@ -612,6 +704,11 @@ operator|.
 name|getEdits
 argument_list|()
 expr_stmt|;
+name|ensureCommentsVisible
+argument_list|(
+name|comments
+argument_list|)
+expr_stmt|;
 name|dstA
 operator|.
 name|setMissingNewlineAtEnd
@@ -652,11 +749,19 @@ name|size
 argument_list|()
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|fh
+operator|!=
+literal|null
+condition|)
+block|{
 name|packHeader
 argument_list|(
 name|fh
 argument_list|)
 expr_stmt|;
+block|}
 if|if
 condition|(
 name|srcA
@@ -780,6 +885,461 @@ argument_list|,
 name|edits
 argument_list|)
 return|;
+block|}
+DECL|method|ensureCommentsVisible (final CommentDetail comments)
+specifier|private
+name|void
+name|ensureCommentsVisible
+parameter_list|(
+specifier|final
+name|CommentDetail
+name|comments
+parameter_list|)
+throws|throws
+name|CorruptEntityException
+block|{
+if|if
+condition|(
+name|comments
+operator|.
+name|getCommentsA
+argument_list|()
+operator|.
+name|isEmpty
+argument_list|()
+operator|&&
+name|comments
+operator|.
+name|getCommentsB
+argument_list|()
+operator|.
+name|isEmpty
+argument_list|()
+condition|)
+block|{
+comment|// No comments, no additional dummy edits are required.
+comment|//
+return|return;
+block|}
+comment|// Construct empty Edit blocks around each location where a comment is.
+comment|// This will force the later packContent method to include the regions
+comment|// containing comments, potentially combining those regions together if
+comment|// they have overlapping contexts. UI renders will also be able to make
+comment|// correct hunks from this, but because the Edit is empty they will not
+comment|// style it specially.
+comment|//
+specifier|final
+name|List
+argument_list|<
+name|Edit
+argument_list|>
+name|empty
+init|=
+operator|new
+name|ArrayList
+argument_list|<
+name|Edit
+argument_list|>
+argument_list|()
+decl_stmt|;
+name|int
+name|lastLine
+decl_stmt|;
+name|lastLine
+operator|=
+operator|-
+literal|1
+expr_stmt|;
+for|for
+control|(
+name|PatchLineComment
+name|plc
+range|:
+name|comments
+operator|.
+name|getCommentsA
+argument_list|()
+control|)
+block|{
+specifier|final
+name|int
+name|a
+init|=
+name|plc
+operator|.
+name|getLine
+argument_list|()
+decl_stmt|;
+if|if
+condition|(
+name|lastLine
+operator|!=
+name|a
+condition|)
+block|{
+name|empty
+operator|.
+name|add
+argument_list|(
+operator|new
+name|Edit
+argument_list|(
+name|a
+operator|-
+literal|1
+argument_list|,
+name|mapA2B
+argument_list|(
+name|a
+operator|-
+literal|1
+argument_list|)
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|lastLine
+operator|=
+name|a
+expr_stmt|;
+block|}
+block|}
+name|lastLine
+operator|=
+operator|-
+literal|1
+expr_stmt|;
+for|for
+control|(
+name|PatchLineComment
+name|plc
+range|:
+name|comments
+operator|.
+name|getCommentsB
+argument_list|()
+control|)
+block|{
+specifier|final
+name|int
+name|b
+init|=
+name|plc
+operator|.
+name|getLine
+argument_list|()
+decl_stmt|;
+if|if
+condition|(
+name|lastLine
+operator|!=
+name|b
+condition|)
+block|{
+name|empty
+operator|.
+name|add
+argument_list|(
+operator|new
+name|Edit
+argument_list|(
+name|mapB2A
+argument_list|(
+name|b
+operator|-
+literal|1
+argument_list|)
+argument_list|,
+name|b
+operator|-
+literal|1
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|lastLine
+operator|=
+name|b
+expr_stmt|;
+block|}
+block|}
+comment|// Build the final list as a copy, as we cannot modify the cached
+comment|// edit list we started out with. Also sort the final list by the
+comment|// index in A, so packContent can combine them correctly later.
+comment|//
+specifier|final
+name|List
+argument_list|<
+name|Edit
+argument_list|>
+name|n
+init|=
+operator|new
+name|ArrayList
+argument_list|<
+name|Edit
+argument_list|>
+argument_list|(
+name|edits
+operator|.
+name|size
+argument_list|()
+operator|+
+name|empty
+operator|.
+name|size
+argument_list|()
+argument_list|)
+decl_stmt|;
+name|n
+operator|.
+name|addAll
+argument_list|(
+name|edits
+argument_list|)
+expr_stmt|;
+name|n
+operator|.
+name|addAll
+argument_list|(
+name|empty
+argument_list|)
+expr_stmt|;
+name|Collections
+operator|.
+name|sort
+argument_list|(
+name|n
+argument_list|,
+name|EDIT_SORT
+argument_list|)
+expr_stmt|;
+name|edits
+operator|=
+name|n
+expr_stmt|;
+block|}
+DECL|method|mapA2B (final int a)
+specifier|private
+name|int
+name|mapA2B
+parameter_list|(
+specifier|final
+name|int
+name|a
+parameter_list|)
+throws|throws
+name|CorruptEntityException
+block|{
+if|if
+condition|(
+name|edits
+operator|.
+name|isEmpty
+argument_list|()
+condition|)
+block|{
+comment|// Magic special case of an unmodified file.
+comment|//
+return|return
+name|a
+return|;
+block|}
+if|if
+condition|(
+name|a
+operator|<
+name|edits
+operator|.
+name|get
+argument_list|(
+literal|0
+argument_list|)
+operator|.
+name|getBeginA
+argument_list|()
+condition|)
+block|{
+comment|// Special case of context at start of file.
+comment|//
+return|return
+name|a
+return|;
+block|}
+for|for
+control|(
+name|Edit
+name|e
+range|:
+name|edits
+control|)
+block|{
+if|if
+condition|(
+name|e
+operator|.
+name|getBeginA
+argument_list|()
+operator|<=
+name|a
+operator|&&
+name|a
+operator|<=
+name|e
+operator|.
+name|getEndA
+argument_list|()
+condition|)
+block|{
+return|return
+name|e
+operator|.
+name|getBeginB
+argument_list|()
+operator|+
+operator|(
+name|a
+operator|-
+name|e
+operator|.
+name|getBeginA
+argument_list|()
+operator|)
+return|;
+block|}
+block|}
+name|log
+operator|.
+name|error
+argument_list|(
+literal|"In "
+operator|+
+name|patchKey
+operator|+
+literal|" cannot remap A "
+operator|+
+name|a
+operator|+
+literal|" to B"
+argument_list|)
+expr_stmt|;
+throw|throw
+operator|new
+name|CorruptEntityException
+argument_list|(
+name|patchKey
+argument_list|)
+throw|;
+block|}
+DECL|method|mapB2A (final int b)
+specifier|private
+name|int
+name|mapB2A
+parameter_list|(
+specifier|final
+name|int
+name|b
+parameter_list|)
+throws|throws
+name|CorruptEntityException
+block|{
+if|if
+condition|(
+name|edits
+operator|.
+name|isEmpty
+argument_list|()
+condition|)
+block|{
+comment|// Magic special case of an unmodified file.
+comment|//
+return|return
+name|b
+return|;
+block|}
+if|if
+condition|(
+name|b
+operator|<
+name|edits
+operator|.
+name|get
+argument_list|(
+literal|0
+argument_list|)
+operator|.
+name|getBeginB
+argument_list|()
+condition|)
+block|{
+comment|// Special case of context at start of file.
+comment|//
+return|return
+name|b
+return|;
+block|}
+for|for
+control|(
+name|Edit
+name|e
+range|:
+name|edits
+control|)
+block|{
+if|if
+condition|(
+name|e
+operator|.
+name|getBeginB
+argument_list|()
+operator|<=
+name|b
+operator|&&
+name|b
+operator|<=
+name|e
+operator|.
+name|getEndB
+argument_list|()
+condition|)
+block|{
+return|return
+name|e
+operator|.
+name|getBeginA
+argument_list|()
+operator|+
+operator|(
+name|b
+operator|-
+name|e
+operator|.
+name|getBeginB
+argument_list|()
+operator|)
+return|;
+block|}
+block|}
+name|log
+operator|.
+name|error
+argument_list|(
+literal|"In "
+operator|+
+name|patchKey
+operator|+
+literal|" cannot remap B "
+operator|+
+name|b
+operator|+
+literal|" to A"
+argument_list|)
+expr_stmt|;
+throw|throw
+operator|new
+name|CorruptEntityException
+argument_list|(
+name|patchKey
+argument_list|)
+throw|;
 block|}
 DECL|method|eq (final ObjectId a, final ObjectId b)
 specifier|private

src/main/java/com/google/gerrit/server/patch/DiffCacheContent.java
@@ -294,7 +294,7 @@ name|java
 operator|.
 name|util
 operator|.
-name|ArrayList
+name|Arrays
 import|;
 end_import
 
@@ -327,6 +327,10 @@ name|DiffCacheContent
 implements|implements
 name|Serializable
 block|{
+comment|// Note: If we modify our version, also modify DiffCacheKey, so
+comment|// the on disk cache is fully destroyed and recreated when the
+comment|// schema has changed.
+comment|//
 DECL|field|serialVersionUID
 specifier|private
 specifier|static
@@ -334,7 +338,7 @@ specifier|final
 name|long
 name|serialVersionUID
 init|=
-literal|2L
+literal|3L
 decl_stmt|;
 DECL|method|create (final Repository db, final DiffCacheKey key, final FileHeader file)
 specifier|public
@@ -499,6 +503,76 @@ name|n
 argument_list|)
 return|;
 block|}
+DECL|method|createEmpty (final Repository db, final ObjectId treeIsh, final String path)
+specifier|public
+specifier|static
+name|DiffCacheContent
+name|createEmpty
+parameter_list|(
+specifier|final
+name|Repository
+name|db
+parameter_list|,
+specifier|final
+name|ObjectId
+name|treeIsh
+parameter_list|,
+specifier|final
+name|String
+name|path
+parameter_list|)
+throws|throws
+name|MissingObjectException
+throws|,
+name|IncorrectObjectTypeException
+throws|,
+name|IOException
+block|{
+specifier|final
+name|ObjectId
+name|blob
+init|=
+name|find
+argument_list|(
+name|db
+argument_list|,
+name|treeIsh
+argument_list|,
+name|path
+argument_list|)
+decl_stmt|;
+if|if
+condition|(
+name|blob
+operator|==
+literal|null
+condition|)
+block|{
+throw|throw
+operator|new
+name|IOException
+argument_list|(
+literal|"path \""
+operator|+
+name|path
+operator|+
+literal|"\" not in "
+operator|+
+name|treeIsh
+operator|.
+name|name
+argument_list|()
+argument_list|)
+throw|;
+block|}
+return|return
+operator|new
+name|DiffCacheContent
+argument_list|(
+name|blob
+argument_list|)
+return|;
+block|}
 DECL|method|toId (final AbbreviatedObjectId a)
 specifier|private
 specifier|static
@@ -609,12 +683,6 @@ literal|0
 argument_list|)
 return|;
 block|}
-DECL|field|noDifference
-specifier|private
-specifier|transient
-name|boolean
-name|noDifference
-decl_stmt|;
 DECL|field|oldId
 specifier|private
 specifier|transient
@@ -642,14 +710,33 @@ name|Edit
 argument_list|>
 name|edits
 decl_stmt|;
-DECL|method|DiffCacheContent ()
-specifier|public
+DECL|method|DiffCacheContent (final ObjectId o)
+specifier|private
 name|DiffCacheContent
-parameter_list|()
+parameter_list|(
+specifier|final
+name|ObjectId
+name|o
+parameter_list|)
 block|{
-name|noDifference
+name|oldId
 operator|=
-literal|true
+name|o
+expr_stmt|;
+name|newId
+operator|=
+name|o
+expr_stmt|;
+name|header
+operator|=
+literal|null
+expr_stmt|;
+name|edits
+operator|=
+name|Collections
+operator|.
+name|emptyList
+argument_list|()
 expr_stmt|;
 block|}
 DECL|method|DiffCacheContent (final FileHeader h, final ObjectId o, final ObjectId n)
@@ -669,10 +756,6 @@ name|ObjectId
 name|n
 parameter_list|)
 block|{
-name|noDifference
-operator|=
-literal|false
-expr_stmt|;
 name|header
 operator|=
 name|compact
@@ -690,6 +773,10 @@ name|n
 expr_stmt|;
 if|if
 condition|(
+name|h
+operator|==
+literal|null
+operator|||
 name|h
 operator|instanceof
 name|CombinedFileHeader
@@ -715,10 +802,15 @@ else|else
 block|{
 name|edits
 operator|=
+name|Collections
+operator|.
+name|unmodifiableList
+argument_list|(
 name|h
 operator|.
 name|toEditList
 argument_list|()
+argument_list|)
 expr_stmt|;
 block|}
 block|}
@@ -729,7 +821,9 @@ name|isNoDifference
 parameter_list|()
 block|{
 return|return
-name|noDifference
+name|header
+operator|==
+literal|null
 return|;
 block|}
 DECL|method|getOldId ()
@@ -787,20 +881,6 @@ parameter_list|)
 throws|throws
 name|IOException
 block|{
-name|out
-operator|.
-name|writeBoolean
-argument_list|(
-name|noDifference
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|noDifference
-condition|)
-block|{
-return|return;
-block|}
 name|ObjectIdSerialization
 operator|.
 name|write
@@ -819,9 +899,16 @@ argument_list|,
 name|newId
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|header
+operator|!=
+literal|null
+condition|)
+block|{
 specifier|final
 name|int
-name|len
+name|hdrLen
 init|=
 name|end
 argument_list|(
@@ -837,7 +924,7 @@ name|out
 operator|.
 name|writeInt
 argument_list|(
-name|len
+name|hdrLen
 argument_list|)
 expr_stmt|;
 name|out
@@ -854,9 +941,20 @@ operator|.
 name|getStartOffset
 argument_list|()
 argument_list|,
-name|len
+name|hdrLen
 argument_list|)
 expr_stmt|;
+block|}
+else|else
+block|{
+name|out
+operator|.
+name|writeInt
+argument_list|(
+literal|0
+argument_list|)
+expr_stmt|;
+block|}
 name|out
 operator|.
 name|writeInt
@@ -930,20 +1028,6 @@ parameter_list|)
 throws|throws
 name|IOException
 block|{
-name|noDifference
-operator|=
-name|in
-operator|.
-name|readBoolean
-argument_list|()
-expr_stmt|;
-if|if
-condition|(
-name|noDifference
-condition|)
-block|{
-return|return;
-block|}
 name|oldId
 operator|=
 name|ObjectIdSerialization
@@ -963,6 +1047,22 @@ name|in
 argument_list|)
 expr_stmt|;
 specifier|final
+name|int
+name|hdrLen
+init|=
+name|in
+operator|.
+name|readInt
+argument_list|()
+decl_stmt|;
+if|if
+condition|(
+name|hdrLen
+operator|>
+literal|0
+condition|)
+block|{
+specifier|final
 name|byte
 index|[]
 name|buf
@@ -970,10 +1070,7 @@ init|=
 operator|new
 name|byte
 index|[
-name|in
-operator|.
-name|readInt
-argument_list|()
+name|hdrLen
 index|]
 decl_stmt|;
 name|in
@@ -990,6 +1087,14 @@ argument_list|(
 name|buf
 argument_list|)
 expr_stmt|;
+block|}
+else|else
+block|{
+name|header
+operator|=
+literal|null
+expr_stmt|;
+block|}
 specifier|final
 name|int
 name|editCount
@@ -999,32 +1104,37 @@ operator|.
 name|readInt
 argument_list|()
 decl_stmt|;
-name|edits
-operator|=
-operator|new
-name|ArrayList
-argument_list|<
-name|Edit
-argument_list|>
-argument_list|(
+if|if
+condition|(
 name|editCount
-argument_list|)
-expr_stmt|;
+operator|>
+literal|0
+condition|)
+block|{
+specifier|final
+name|Edit
+index|[]
+name|editArray
+init|=
+operator|new
+name|Edit
+index|[
+name|editCount
+index|]
+decl_stmt|;
 for|for
 control|(
 name|int
-name|n
+name|i
 init|=
-name|editCount
-operator|-
-literal|1
-init|;
 literal|0
-operator|<=
-name|n
+init|;
+name|i
+operator|<
+name|editCount
 condition|;
-name|n
-operator|--
+name|i
+operator|++
 control|)
 block|{
 specifier|final
@@ -1063,10 +1173,11 @@ operator|.
 name|readInt
 argument_list|()
 decl_stmt|;
-name|edits
-operator|.
-name|add
-argument_list|(
+name|editArray
+index|[
+name|i
+index|]
+operator|=
 operator|new
 name|Edit
 argument_list|(
@@ -1078,7 +1189,31 @@ name|beginB
 argument_list|,
 name|endB
 argument_list|)
+expr_stmt|;
+block|}
+name|edits
+operator|=
+name|Collections
+operator|.
+name|unmodifiableList
+argument_list|(
+name|Arrays
+operator|.
+name|asList
+argument_list|(
+name|editArray
 argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
+name|edits
+operator|=
+name|Collections
+operator|.
+name|emptyList
+argument_list|()
 expr_stmt|;
 block|}
 block|}

src/main/java/com/google/gerrit/server/patch/DiffCacheEntryFactory.java
@@ -136,6 +136,20 @@ name|jgit
 operator|.
 name|lib
 operator|.
+name|ObjectId
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|spearce
+operator|.
+name|jgit
+operator|.
+name|lib
+operator|.
 name|Repository
 import|;
 end_import
@@ -245,6 +259,15 @@ argument_list|()
 argument_list|)
 decl_stmt|;
 specifier|final
+name|ObjectId
+name|newId
+init|=
+name|key
+operator|.
+name|getNewId
+argument_list|()
+decl_stmt|;
+specifier|final
 name|List
 argument_list|<
 name|String
@@ -349,10 +372,7 @@ name|args
 operator|.
 name|add
 argument_list|(
-name|key
-operator|.
-name|getNewId
-argument_list|()
+name|newId
 operator|.
 name|name
 argument_list|()
@@ -505,9 +525,19 @@ argument_list|()
 condition|)
 block|{
 return|return
-operator|new
 name|DiffCacheContent
+operator|.
+name|createEmpty
+argument_list|(
+name|db
+argument_list|,
+name|newId
+argument_list|,
+name|key
+operator|.
+name|getFileName
 argument_list|()
+argument_list|)
 return|;
 block|}
 elseif|else

src/main/java/com/google/gerrit/server/patch/DiffCacheKey.java
@@ -196,7 +196,7 @@ specifier|final
 name|long
 name|serialVersionUID
 init|=
-literal|2L
+literal|3L
 decl_stmt|;
 DECL|field|projectKey
 specifier|private

src/main/java/com/google/gerrit/server/patch/PatchScriptAction.java
@@ -110,6 +110,38 @@ name|gerrit
 operator|.
 name|client
 operator|.
+name|patches
+operator|.
+name|CommentDetail
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|client
+operator|.
+name|reviewdb
+operator|.
+name|Account
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|client
+operator|.
 name|reviewdb
 operator|.
 name|AccountGeneralPreferences
@@ -160,6 +192,22 @@ name|client
 operator|.
 name|reviewdb
 operator|.
+name|PatchLineComment
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|client
+operator|.
+name|reviewdb
+operator|.
 name|PatchSet
 import|;
 end_import
@@ -193,6 +241,22 @@ operator|.
 name|reviewdb
 operator|.
 name|ReviewDb
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|client
+operator|.
+name|rpc
+operator|.
+name|Common
 import|;
 end_import
 
@@ -952,11 +1016,25 @@ operator|.
 name|getObjectValue
 argument_list|()
 decl_stmt|;
+specifier|final
+name|CommentDetail
+name|comments
+init|=
+name|allComments
+argument_list|(
+name|db
+argument_list|)
+decl_stmt|;
 if|if
 condition|(
 name|dcc
 operator|.
 name|isNoDifference
+argument_list|()
+operator|&&
+name|comments
+operator|.
+name|isEmpty
 argument_list|()
 condition|)
 block|{
@@ -978,6 +1056,8 @@ operator|.
 name|toPatchScript
 argument_list|(
 name|dcc
+argument_list|,
+name|comments
 argument_list|)
 return|;
 block|}
@@ -1402,6 +1482,116 @@ argument_list|()
 argument_list|)
 throw|;
 block|}
+block|}
+DECL|method|allComments (final ReviewDb db)
+specifier|private
+name|CommentDetail
+name|allComments
+parameter_list|(
+specifier|final
+name|ReviewDb
+name|db
+parameter_list|)
+throws|throws
+name|OrmException
+block|{
+specifier|final
+name|CommentDetail
+name|r
+init|=
+operator|new
+name|CommentDetail
+argument_list|(
+name|psa
+argument_list|,
+name|psb
+argument_list|)
+decl_stmt|;
+specifier|final
+name|String
+name|pn
+init|=
+name|patchKey
+operator|.
+name|get
+argument_list|()
+decl_stmt|;
+for|for
+control|(
+name|PatchLineComment
+name|p
+range|:
+name|db
+operator|.
+name|patchComments
+argument_list|()
+operator|.
+name|published
+argument_list|(
+name|changeId
+argument_list|,
+name|pn
+argument_list|)
+control|)
+block|{
+name|r
+operator|.
+name|include
+argument_list|(
+name|p
+argument_list|)
+expr_stmt|;
+block|}
+specifier|final
+name|Account
+operator|.
+name|Id
+name|me
+init|=
+name|Common
+operator|.
+name|getAccountId
+argument_list|()
+decl_stmt|;
+if|if
+condition|(
+name|me
+operator|!=
+literal|null
+condition|)
+block|{
+for|for
+control|(
+name|PatchLineComment
+name|p
+range|:
+name|db
+operator|.
+name|patchComments
+argument_list|()
+operator|.
+name|draft
+argument_list|(
+name|changeId
+argument_list|,
+name|pn
+argument_list|,
+name|me
+argument_list|)
+control|)
+block|{
+name|r
+operator|.
+name|include
+argument_list|(
+name|p
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+return|return
+name|r
+return|;
 block|}
 block|}
 end_class

commit a16abf404903512f392d9a4db4516a2d603cbda4

Fix bad diff display near empty comment caused edits
In 54824fe1b88c [formerly 974daeef2121] we fixed GERRIT-66, a bug about not showing comments
if they were outside of the context of an edit being displayed in the
viewer.  Unfortunately the fixed hinged around creating an empty Edit
object, thus forcing the hunk surrounding the comment to appear.  This
threw off the display code, causing it to render some lines twice, and
in other cases, to omit lines entirely.

We now filter out the unnecessary empty Edit instances within a hunk,
as they served their purpose to hold open the hunk's context large
enough for the viewer to get the lines necessary, but leaving them in
nearby to other Edits may cause display errors.

Bug: GERRIT-220
Signed-off-by: Shawn O. Pearce <sop@google.com>

Former-commit-id: 6775ab969674b2a2fdac627f2f049fc986aab486

src/main/java/com/google/gerrit/server/patch/PatchScriptBuilder.java
@@ -732,10 +732,17 @@ expr_stmt|;
 block|}
 name|edits
 operator|=
+operator|new
+name|ArrayList
+argument_list|<
+name|Edit
+argument_list|>
+argument_list|(
 name|contentAct
 operator|.
 name|getEdits
 argument_list|()
+argument_list|)
 expr_stmt|;
 name|ensureCommentsVisible
 argument_list|(
@@ -852,9 +859,18 @@ expr_stmt|;
 block|}
 name|edits
 operator|=
-name|Collections
+operator|new
+name|ArrayList
+argument_list|<
+name|Edit
+argument_list|>
+argument_list|(
+literal|1
+argument_list|)
+expr_stmt|;
+name|edits
 operator|.
-name|singletonList
+name|add
 argument_list|(
 operator|new
 name|Edit
@@ -924,10 +940,17 @@ comment|// list around the whitespace edit list.
 comment|//
 name|edits
 operator|=
+operator|new
+name|ArrayList
+argument_list|<
+name|Edit
+argument_list|>
+argument_list|(
 name|contentWS
 operator|.
 name|getEdits
 argument_list|()
+argument_list|)
 expr_stmt|;
 name|ensureCommentsVisible
 argument_list|(
@@ -1124,42 +1147,10 @@ name|b
 expr_stmt|;
 block|}
 block|}
-comment|// Build the final list as a copy, as we cannot modify the cached
-comment|// edit list we started out with. Also sort the final list by the
-comment|// index in A, so packContent can combine them correctly later.
+comment|// Sort the final list by the index in A, so packContent can combine
+comment|// them correctly later.
 comment|//
-specifier|final
-name|List
-argument_list|<
-name|Edit
-argument_list|>
-name|n
-init|=
-operator|new
-name|ArrayList
-argument_list|<
-name|Edit
-argument_list|>
-argument_list|(
 name|edits
-operator|.
-name|size
-argument_list|()
-operator|+
-name|empty
-operator|.
-name|size
-argument_list|()
-argument_list|)
-decl_stmt|;
-name|n
-operator|.
-name|addAll
-argument_list|(
-name|edits
-argument_list|)
-expr_stmt|;
-name|n
 operator|.
 name|addAll
 argument_list|(
@@ -1170,14 +1161,10 @@ name|Collections
 operator|.
 name|sort
 argument_list|(
-name|n
+name|edits
 argument_list|,
 name|EDIT_SORT
 argument_list|)
-expr_stmt|;
-name|edits
-operator|=
-name|n
 expr_stmt|;
 block|}
 DECL|method|safeAdd (final List<Edit> empty, final Edit toAdd)
@@ -2008,6 +1995,7 @@ operator|.
 name|size
 argument_list|()
 condition|)
+block|{
 name|curEdit
 operator|=
 name|edits
@@ -2017,6 +2005,42 @@ argument_list|(
 name|curIdx
 argument_list|)
 expr_stmt|;
+while|while
+condition|(
+name|curEdit
+operator|.
+name|getType
+argument_list|()
+operator|==
+name|Edit
+operator|.
+name|Type
+operator|.
+name|EMPTY
+operator|&&
+name|curEdit
+operator|!=
+name|endEdit
+condition|)
+block|{
+name|edits
+operator|.
+name|remove
+argument_list|(
+name|curIdx
+argument_list|)
+expr_stmt|;
+name|curEdit
+operator|=
+name|edits
+operator|.
+name|get
+argument_list|(
+name|curIdx
+argument_list|)
+expr_stmt|;
+block|}
+block|}
 block|}
 block|}
 block|}