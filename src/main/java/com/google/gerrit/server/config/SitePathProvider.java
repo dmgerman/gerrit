422b806411edced16470bf34c11f7f3b5e88a9aa
Shawn O. Pearce <sop@google.com>
2009-07-29 09:17:29 -0700

    Correct copyright dates in SitePath support to be 2009
    Signed-off-by: Shawn O. Pearce <sop@google.com>
    
    Former-commit-id: 425ea924cb143a790157e254ccb4b9d0fe62251c

--- src/main/java/com/google/gerrit/server/config/SitePathProvider.java
+++ src/main/java/com/google/gerrit/server/config/SitePathProvider.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|// Copyright (C) 2008 The Android Open Source Project
+comment|// Copyright (C) 2009 The Android Open Source Project
 end_comment
 
 begin_comment
