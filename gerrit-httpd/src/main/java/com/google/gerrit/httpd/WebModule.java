bbd6112cf7f29ddb294a0832ae12c323dab6bc80
Shawn O. Pearce <sop@google.com>
2010-01-14 18:21:18 -0800

    Enable smart HTTP under /p/ URLs
    The new smart HTTP Git protocol is now supported through the Gerrit
    web application context by using the URL /p/$project.git.
    
    Like SSH, push requires user authentication.  However, since
    user authentication is currently based solely upon HTTP cookies,
    and cookies aren't supported by the C Git command line client,
    using push is not currently possible.  We need to introduce our
    own password based user authentication process.
    
    Change-Id: I76a894226a583ce6f47fd246f7d0f88b77ea4505
    Signed-off-by: Shawn O. Pearce <sop@google.com>
    
    Former-commit-id: db5fb94dc12d6ef724c417be48f76f203cacb397

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/WebModule.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/WebModule.java
@@ -840,6 +840,15 @@ name|GerritRequestModule
 argument_list|()
 argument_list|)
 expr_stmt|;
+name|install
+argument_list|(
+operator|new
+name|ProjectServlet
+operator|.
+name|Module
+argument_list|()
+argument_list|)
+expr_stmt|;
 name|bind
 argument_list|(
 name|SshInfo
