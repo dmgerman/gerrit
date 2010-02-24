11930d5051371ec116dc169c17e7dd1594b4a193
Kenny Root <kroot@google.com>
2010-02-24 00:29:20 -0800

    Add stream-events command
    The stream-events command outputs patchset added, comment added,
    change merged, and change abandoned events in JSON format over SSH.
    Streaming ends when the client disconnects or aborts the command.
    
    Change-Id: Iba3ff56c23ab91cf3b855d5dde6c40d9735b643f
    
    Former-commit-id: 15ac1b83f54b47c09bd8ad9ed8564df375f55c3a

---
+++ gerrit-server/src/main/java/com/google/gerrit/common/ChangeListener.java
@@ -0,0 +1,102 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright (C) 2010 The Android Open Source Project
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.common
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|common
+package|;
+end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|common
+operator|.
+name|ChangeHookRunner
+operator|.
+name|ChangeEvent
+import|;
+end_import
+
+begin_interface
+DECL|interface|ChangeListener
+specifier|public
+interface|interface
+name|ChangeListener
+block|{
+DECL|method|onChangeEvent (ChangeEvent event)
+specifier|public
+name|void
+name|onChangeEvent
+parameter_list|(
+name|ChangeEvent
+name|event
+parameter_list|)
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
