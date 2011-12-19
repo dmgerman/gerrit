8a652127363ad3e2188f45f1e1bce9e4e07edeb3
Dave Borowitz <dborowitz@google.com>
2011-12-19 11:02:06 -0800

    Remove PatchLineCommentAccess.publish()
    This was modified in ae122ef21 to be the various publishBy* methods, and
    was accidentally added back in a rebase.
    
    Change-Id: I2ed54feaa1e5c9ce40d0aadb9581289fc3076516
    
    Former-commit-id: 690c5161b95350b3fbcdcff011eae9c17bf9f1d6

--- gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/PatchLineCommentAccess.java
+++ gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/PatchLineCommentAccess.java
@@ -209,32 +209,6 @@ function_decl|;
 annotation|@
 name|Query
 argument_list|(
-literal|"WHERE key.patchKey = ? AND status = '"
-operator|+
-name|PatchLineComment
-operator|.
-name|STATUS_PUBLISHED
-operator|+
-literal|"' ORDER BY lineNbr,writtenOn"
-argument_list|)
-DECL|method|published (Patch.Key patch)
-name|ResultSet
-argument_list|<
-name|PatchLineComment
-argument_list|>
-name|published
-parameter_list|(
-name|Patch
-operator|.
-name|Key
-name|patch
-parameter_list|)
-throws|throws
-name|OrmException
-function_decl|;
-annotation|@
-name|Query
-argument_list|(
 literal|"WHERE key.patchKey.patchSetId.changeId = ?"
 operator|+
 literal|" AND key.patchKey.fileName = ? AND status = '"
