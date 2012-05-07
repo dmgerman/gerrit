60cb6fc43b5448dbc5610050beaf2a35d64b7788
Luca Milanesio <luca.milanesio@gmail.com>
2012-05-07 15:54:09 -0700

    PluginLoader to scan and parse all plugins
    JAR manifests are inspected and the module class is retrieved and
    loaded. Each plugin JAR is loaded using a separate UrlClassLoader
    using Gerrit classLoader as basis.
    
    Change-Id: Ic60bacd528100f502e2b7c5514bbb02ec39ab939
    Signed-off-by: Luca Milanesio <luca.milanesio@gmail.com>
    
    Former-commit-id: cf5cd23d079b7c3980b55fb1105b7b036537b2f2

---
+++ gerrit-server/src/main/java/com/google/gerrit/common/Plugin.java
@@ -0,0 +1,156 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright (C) 2012 The Android Open Source Project
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.common
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|common
+package|;
+end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|inject
+operator|.
+name|Module
+import|;
+end_import
+
+begin_class
+DECL|class|Plugin
+specifier|public
+class|class
+name|Plugin
+block|{
+DECL|field|name
+specifier|public
+specifier|final
+name|String
+name|name
+decl_stmt|;
+DECL|field|moduleClass
+specifier|public
+specifier|final
+name|Class
+argument_list|<
+name|?
+extends|extends
+name|Module
+argument_list|>
+name|moduleClass
+decl_stmt|;
+DECL|method|Plugin (String name, Class<? extends Module> moduleClass)
+specifier|public
+name|Plugin
+parameter_list|(
+name|String
+name|name
+parameter_list|,
+name|Class
+argument_list|<
+name|?
+extends|extends
+name|Module
+argument_list|>
+name|moduleClass
+parameter_list|)
+block|{
+name|this
+operator|.
+name|name
+operator|=
+name|name
+expr_stmt|;
+name|this
+operator|.
+name|moduleClass
+operator|=
+name|moduleClass
+expr_stmt|;
+block|}
+annotation|@
+name|Override
+DECL|method|toString ()
+specifier|public
+name|String
+name|toString
+parameter_list|()
+block|{
+return|return
+literal|"Plugin ["
+operator|+
+name|name
+operator|+
+literal|"]: ModuleClass="
+operator|+
+name|moduleClass
+operator|.
+name|getName
+argument_list|()
+return|;
+block|}
+block|}
+end_class
+
+end_unit
+
