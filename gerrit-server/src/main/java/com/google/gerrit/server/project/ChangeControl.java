1d56c3ec4970424686f46971637cb5b47dd2f2c0
Edwin Kempin <edwin.kempin@sap.com>
2012-05-09 21:09:55 +0200

    Add a new permission to permit rebasing changes in the web UI
    So far only the change owner and submitters are permitted to rebase a
    change in the web UI by pushing the 'Rebase Change' button. This change
    introduces a new access right that can be assigned to other users to
    permit them to do the rebase in the web UI as well.
    
    Change-Id: I21e9981bd685f1d69e792abbd7eeaf1a60d039b8
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>
    
    Former-commit-id: 94313bca2843539e9601cf53ddfdc76edef7d080

--- gerrit-server/src/main/java/com/google/gerrit/server/project/ChangeControl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/ChangeControl.java
@@ -1218,6 +1218,12 @@ argument_list|()
 operator|.
 name|canSubmit
 argument_list|()
+operator|||
+name|getRefControl
+argument_list|()
+operator|.
+name|canRebase
+argument_list|()
 return|;
 block|}
 comment|/** Can this user restore this change? */
