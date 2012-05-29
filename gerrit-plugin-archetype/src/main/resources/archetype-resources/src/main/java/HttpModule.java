d349fe01a4e54fc7278b93ecacf2038b7af462f5
Sasa Zivkov <sasa.zivkov@sap.com>
2012-05-29 22:29:33 +0200

    Add maven archetype for creating gerrit plugin projects.
    Until now creation of a new Gerrit plugin meant either starting from
    scratch or copying and modifying and existing plugin.
    
    This change introduces a maven archetype for creating new Gerrit plugin
    projects. Assuming gerrit-plugin-api and gerrit-plugin-archetype are
    installed in a maven repository one can create a new Gerrit plugin
    project by executing a command like:
    
      mvn archetype:generate -DarchetypeGroupId=com.google.gerrit \
        -DarchetypeArtifactId=gerrit-plugin-archetype \
        -DarchetypeVersion=2.5-SNAPSHOT \
        -DgroupId=com.google.gerrit \
        -DartifactId=testPlugin
    
    Maven will ask for additional properties and then create the plugin in
    the current directory. To change the default property values answer 'n'
    when maven asks for confirm properties configuration. It will then ask
    again for all properties including those with predefined default values.
    
    A missing part is to not generate [Ssh|Http]Module.java skeletons in
    case when user choses 'n' for some of Gerrit-[Ssh|Http]Module
    properties.
    
    Change-Id: I0b91957415de85eee029c91a6ac7e27096d6a452
    Signed-off-by: Sasa Zivkov <sasa.zivkov@sap.com>
    
    Former-commit-id: 844c51ed40e1ec98278cc21c2e116eeef09fba5b

---
+++ gerrit-plugin-archetype/src/main/resources/archetype-resources/src/main/java/HttpModule.java
@@ -0,0 +1,101 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright (C) 2012 The Android Open Source Project
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+package|package
+name|$
+block|{
+package|package
+block|}
+end_package
+
+begin_empty_stmt
+empty_stmt|;
+end_empty_stmt
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|inject
+operator|.
+name|servlet
+operator|.
+name|ServletModule
+import|;
+end_import
+
+begin_class
+DECL|class|HttpModule
+class|class
+name|HttpModule
+extends|extends
+name|ServletModule
+block|{
+annotation|@
+name|Override
+DECL|method|configureServlets ()
+specifier|protected
+name|void
+name|configureServlets
+parameter_list|()
+block|{
+comment|// TODO
+block|}
+block|}
+end_class
+
+end_unit
+
---
+++ gerrit-plugin-archetype/src/main/resources/archetype-resources/src/main/java/SshModule.java
@@ -0,0 +1,101 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright (C) 2012 The Android Open Source Project
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+package|package
+name|$
+block|{
+package|package
+block|}
+end_package
+
+begin_empty_stmt
+empty_stmt|;
+end_empty_stmt
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|sshd
+operator|.
+name|PluginCommandModule
+import|;
+end_import
+
+begin_class
+DECL|class|SshModule
+class|class
+name|SshModule
+extends|extends
+name|PluginCommandModule
+block|{
+annotation|@
+name|Override
+DECL|method|configureCommands ()
+specifier|protected
+name|void
+name|configureCommands
+parameter_list|()
+block|{
+comment|// command("my-command").to(MyCommand.class);
+block|}
+block|}
+end_class
+
+end_unit
+
