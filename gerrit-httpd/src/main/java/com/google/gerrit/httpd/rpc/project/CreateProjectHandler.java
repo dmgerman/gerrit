b21e58151ddd4960e9cb3776d0c29bd73358541e
Chad Horohoe <chorohoe@wikimedia.org>
2012-06-11 15:30:40 -0400

    Removing a couple of unused imports
    Change-Id: I210237ebf3bd44d59d00fbc039a4cc5ab77175cf
    
    Former-commit-id: efc334aa21bb4c8b8ccd6862d9b7bc17d1d0031f

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/rpc/project/CreateProjectHandler.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/rpc/project/CreateProjectHandler.java
@@ -240,20 +240,6 @@ end_import
 
 begin_import
 import|import
-name|org
-operator|.
-name|eclipse
-operator|.
-name|jgit
-operator|.
-name|lib
-operator|.
-name|Constants
-import|;
-end_import
-
-begin_import
-import|import
 name|java
 operator|.
 name|util
