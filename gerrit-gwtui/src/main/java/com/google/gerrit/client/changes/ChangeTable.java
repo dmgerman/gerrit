dmgerman/gerrit

commit 8d3d401a5c1060f366202ff7e12d35c70e30f835

Allow users of ChangeTable to control styling of change rows
Currently it is hard-coded in the ChangeTable that outdated and
abandoned changes should be highlighted. With this change users of the
ChangeTable can now control from outside which changes should be
highlighted and how the highlighting should be done.

The ChangeTable which shows the dependencies on the ChangeScreen is
using the new ability so that outdated and abandoned changes are now
only highlighted in the 'Depends On' section, but not anymore in the
'Needed By' section.

Change-Id: If3140a3ad0a47d4c6813fc3aef06adb1c959f49b
Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

Former-commit-id: af4138b9657ca978138ca56b89380e94e7e75ab1

gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeTable.java
@@ -1818,7 +1818,7 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-DECL|method|populateChangeRow (final int row, final ChangeInfo c)
+DECL|method|populateChangeRow (final int row, final ChangeInfo c, final ChangeRowFormatter changeRowFormatter)
 specifier|private
 name|void
 name|populateChangeRow
@@ -1830,6 +1830,10 @@ parameter_list|,
 specifier|final
 name|ChangeInfo
 name|c
+parameter_list|,
+specifier|final
+name|ChangeRowFormatter
+name|changeRowFormatter
 parameter_list|)
 block|{
 name|ChangeCache
@@ -1979,54 +1983,27 @@ expr_stmt|;
 block|}
 if|if
 condition|(
-operator|!
-name|c
-operator|.
-name|isLatest
-argument_list|()
+name|changeRowFormatter
+operator|!=
+literal|null
 condition|)
 block|{
-name|s
-operator|+=
-literal|" [OUTDATED]"
-expr_stmt|;
-name|table
+specifier|final
+name|String
+name|rowStyle
+init|=
+name|changeRowFormatter
 operator|.
-name|getRowFormatter
-argument_list|()
-operator|.
-name|addStyleName
+name|getRowStyle
 argument_list|(
-name|row
-argument_list|,
-name|Gerrit
-operator|.
-name|RESOURCES
-operator|.
-name|css
-argument_list|()
-operator|.
-name|outdated
-argument_list|()
+name|c
 argument_list|)
-expr_stmt|;
-block|}
-elseif|else
+decl_stmt|;
 if|if
 condition|(
-name|Change
-operator|.
-name|Status
-operator|.
-name|ABANDONED
-operator|.
-name|equals
-argument_list|(
-name|c
-operator|.
-name|getStatus
-argument_list|()
-argument_list|)
+name|rowStyle
+operator|!=
+literal|null
 condition|)
 block|{
 name|table
@@ -2038,38 +2015,19 @@ name|addStyleName
 argument_list|(
 name|row
 argument_list|,
-name|Gerrit
-operator|.
-name|RESOURCES
-operator|.
-name|css
-argument_list|()
-operator|.
-name|outdated
-argument_list|()
+name|rowStyle
 argument_list|)
 expr_stmt|;
 block|}
-else|else
-block|{
-name|table
+name|s
+operator|=
+name|changeRowFormatter
 operator|.
-name|getRowFormatter
-argument_list|()
-operator|.
-name|removeStyleName
+name|getDisplayText
 argument_list|(
-name|row
+name|c
 argument_list|,
-name|Gerrit
-operator|.
-name|RESOURCES
-operator|.
-name|css
-argument_list|()
-operator|.
-name|outdated
-argument_list|()
+name|s
 argument_list|)
 expr_stmt|;
 block|}
@@ -3362,6 +3320,11 @@ DECL|field|rows
 name|int
 name|rows
 decl_stmt|;
+DECL|field|changeRowFormatter
+specifier|private
+name|ChangeRowFormatter
+name|changeRowFormatter
+decl_stmt|;
 DECL|method|Section ()
 specifier|public
 name|Section
@@ -3468,6 +3431,23 @@ name|titleText
 argument_list|)
 expr_stmt|;
 block|}
+block|}
+DECL|method|setChangeRowFormatter (final ChangeRowFormatter changeRowFormatter)
+specifier|public
+name|void
+name|setChangeRowFormatter
+parameter_list|(
+specifier|final
+name|ChangeRowFormatter
+name|changeRowFormatter
+parameter_list|)
+block|{
+name|this
+operator|.
+name|changeRowFormatter
+operator|=
+name|changeRowFormatter
+expr_stmt|;
 block|}
 DECL|method|display (final List<ChangeInfo> changeList)
 specifier|public
@@ -3637,6 +3617,8 @@ operator|+
 name|i
 argument_list|,
 name|c
+argument_list|,
+name|changeRowFormatter
 argument_list|)
 expr_stmt|;
 name|cids
@@ -3712,6 +3694,34 @@ break|break;
 block|}
 block|}
 block|}
+block|}
+DECL|interface|ChangeRowFormatter
+specifier|public
+specifier|static
+interface|interface
+name|ChangeRowFormatter
+block|{
+comment|/**      * Returns the name of the CSS style that should be applied to the change      * row.      *      * @param c the change for which the styling should be returned      * @return the name of the CSS style that should be applied to the change      *         row      */
+DECL|method|getRowStyle (ChangeInfo c)
+name|String
+name|getRowStyle
+parameter_list|(
+name|ChangeInfo
+name|c
+parameter_list|)
+function_decl|;
+comment|/**      * Returns the text that should be displayed for the change.      *      * @param c the change for which the display text should be returned      * @param displayText the current display text      * @return the new display text      */
+DECL|method|getDisplayText (ChangeInfo c, String displayText)
+name|String
+name|getDisplayText
+parameter_list|(
+name|ChangeInfo
+name|c
+parameter_list|,
+name|String
+name|displayText
+parameter_list|)
+function_decl|;
 block|}
 block|}
 end_class

gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeScreen.java
@@ -1259,6 +1259,102 @@ name|changeScreenDependsOn
 argument_list|()
 argument_list|)
 expr_stmt|;
+name|dependsOn
+operator|.
+name|setChangeRowFormatter
+argument_list|(
+operator|new
+name|ChangeTable
+operator|.
+name|ChangeRowFormatter
+argument_list|()
+block|{
+annotation|@
+name|Override
+specifier|public
+name|String
+name|getRowStyle
+parameter_list|(
+name|ChangeInfo
+name|c
+parameter_list|)
+block|{
+if|if
+condition|(
+operator|!
+name|c
+operator|.
+name|isLatest
+argument_list|()
+operator|||
+name|Change
+operator|.
+name|Status
+operator|.
+name|ABANDONED
+operator|.
+name|equals
+argument_list|(
+name|c
+operator|.
+name|getStatus
+argument_list|()
+argument_list|)
+condition|)
+block|{
+return|return
+name|Gerrit
+operator|.
+name|RESOURCES
+operator|.
+name|css
+argument_list|()
+operator|.
+name|outdated
+argument_list|()
+return|;
+block|}
+return|return
+literal|null
+return|;
+block|}
+annotation|@
+name|Override
+specifier|public
+name|String
+name|getDisplayText
+parameter_list|(
+specifier|final
+name|ChangeInfo
+name|c
+parameter_list|,
+specifier|final
+name|String
+name|displayText
+parameter_list|)
+block|{
+if|if
+condition|(
+operator|!
+name|c
+operator|.
+name|isLatest
+argument_list|()
+condition|)
+block|{
+return|return
+name|displayText
+operator|+
+literal|" [OUTDATED]"
+return|;
+block|}
+return|return
+name|displayText
+return|;
+block|}
+block|}
+argument_list|)
+expr_stmt|;
 name|neededBy
 operator|=
 operator|new

commit bdcb5a7cc70efd77aa7f85bf8781f5952a766aa9

Remove red background for outdated dependency after rebase
In the Dependencies table on the ChangeScreen an outdated dependency is
highlighted by a red background. If the user clicks on the rebase
button and the rebase is successful the outdated dependency gets fixed,
but in the Dependencies table the dependency is still highlighted by
the red background because the corresponding style is not removed. This
was broken by 8d3d401a5c1060f366202ff7e12d35c70e30f835 [formerly af4138b9657ca978138ca56b89380e94e7e75ab1].

Change-Id: I7ae672a6adea721bac91c7b6584cca6d0b8f3a74
Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

Former-commit-id: 8903b407ac1c9a97b0e18bbfc64a3545aaad5152

gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeTable.java
@@ -1988,6 +1988,13 @@ operator|!=
 literal|null
 condition|)
 block|{
+name|removeChangeStyle
+argument_list|(
+name|row
+argument_list|,
+name|changeRowFormatter
+argument_list|)
+expr_stmt|;
 specifier|final
 name|String
 name|rowStyle
@@ -2151,6 +2158,69 @@ argument_list|,
 name|c
 argument_list|)
 expr_stmt|;
+block|}
+DECL|method|removeChangeStyle (int row, final ChangeRowFormatter changeRowFormatter)
+specifier|private
+name|void
+name|removeChangeStyle
+parameter_list|(
+name|int
+name|row
+parameter_list|,
+specifier|final
+name|ChangeRowFormatter
+name|changeRowFormatter
+parameter_list|)
+block|{
+specifier|final
+name|ChangeInfo
+name|oldChange
+init|=
+name|getRowItem
+argument_list|(
+name|row
+argument_list|)
+decl_stmt|;
+if|if
+condition|(
+name|oldChange
+operator|==
+literal|null
+condition|)
+block|{
+return|return;
+block|}
+specifier|final
+name|String
+name|oldRowStyle
+init|=
+name|changeRowFormatter
+operator|.
+name|getRowStyle
+argument_list|(
+name|oldChange
+argument_list|)
+decl_stmt|;
+if|if
+condition|(
+name|oldRowStyle
+operator|!=
+literal|null
+condition|)
+block|{
+name|table
+operator|.
+name|getRowFormatter
+argument_list|()
+operator|.
+name|removeStyleName
+argument_list|(
+name|row
+argument_list|,
+name|oldRowStyle
+argument_list|)
+expr_stmt|;
+block|}
 block|}
 DECL|method|link (final Account.Id id)
 specifier|private