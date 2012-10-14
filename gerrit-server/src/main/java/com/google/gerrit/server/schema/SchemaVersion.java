67ab521a1c3b9a9426ae58c39944f9cd6c7fd3bf
Arnaud Fabre <fabre.arnaud@gmail.com>
2012-10-14 19:05:42 +0200

    ChangeMessageAccess: Add index for byPathSet method.
    When using a sql datastore, ChangeMessageAccess#byPathSet ends
    up creating a request filtering results with columns patchset_change_id
    and patchset_patch_set_id, none of which is indexed. This results
    in a full table scan.
    
    Since change I6bc6391f456d9faeab7f1212b9c0463e7514cc05, change messages
    are retrieved by patch set for highlighting purpose in ListChanges.
    
    This can have a significant impact on dashboard. On a gerrit instance
    I manage, which has about 70,000 lines in change_messages, one user
    dashboard with 137 displayed changes went from an average display
    time of 2.4s to 0.7s by adding this index.
    
    Change-Id: If92f5b4f924617c69a1318f31a5ca01f320af792
    
    Former-commit-id: f77e9a9109b2bdd21991e5d5c53953434bef5d83

--- gerrit-server/src/main/java/com/google/gerrit/server/schema/SchemaVersion.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/SchemaVersion.java
@@ -246,11 +246,11 @@ specifier|static
 specifier|final
 name|Class
 argument_list|<
-name|Schema_72
+name|Schema_73
 argument_list|>
 name|C
 init|=
-name|Schema_72
+name|Schema_73
 operator|.
 name|class
 decl_stmt|;
