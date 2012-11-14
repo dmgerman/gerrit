f772b7500c49e33b5f8d6ec435bfee79b9b8d84f
Brad Larson <bklarson@gmail.com>
2012-11-14 15:45:17 -0600

    Use REST API when abandoning from web interface
    AbandonChange and AbandonChangeHandler are no longer needed.  We now
    use the new Abandon class to do the heavy lifting of abandoning
    changes.
    
    Change-Id: I7a0fd798cd66734bfcfb701fd87e1cdd9595b88a
    
    Former-commit-id: f0713bfe39968e844d49d7a699a895099866b7ba

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/rpc/changedetail/ChangeModule.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/rpc/changedetail/ChangeModule.java
@@ -160,15 +160,6 @@ parameter_list|()
 block|{
 name|factory
 argument_list|(
-name|AbandonChangeHandler
-operator|.
-name|Factory
-operator|.
-name|class
-argument_list|)
-expr_stmt|;
-name|factory
-argument_list|(
 name|AlterTopicHandler
 operator|.
 name|Factory
