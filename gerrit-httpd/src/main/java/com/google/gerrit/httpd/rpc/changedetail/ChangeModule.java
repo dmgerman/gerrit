a4c06d9ff06829adda4c13adc5c05c39a3e26dca
David Pursehouse <david.pursehouse@sonymobile.com>
2012-11-22 17:59:44 +0900

    Implement revert change via REST API
    Add a new REST API to revert a change, and refactor
    the web UI to use it.
    
    Change-Id: I3040050df359c2071addbdfb14ae3235f84d9ad9
    
    Former-commit-id: dcd13ce381e47936beb159a4ce20b87269fa7d1a

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/rpc/changedetail/ChangeModule.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/rpc/changedetail/ChangeModule.java
@@ -178,15 +178,6 @@ argument_list|)
 expr_stmt|;
 name|factory
 argument_list|(
-name|RevertChange
-operator|.
-name|Factory
-operator|.
-name|class
-argument_list|)
-expr_stmt|;
-name|factory
-argument_list|(
 name|RebaseChangeHandler
 operator|.
 name|Factory
