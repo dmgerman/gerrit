098df283ef42f8d88eebd533182e00616fae83ed
Edwin Kempin <edwin.kempin@sap.com>
2013-02-12 14:47:23 +0100

    Add acceptance tests for listing groups via REST
    Change-Id: I4a2300aee907330ecbba01dca033d07140b78865
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>
    
    Former-commit-id: dd5f811175b53bbcb9b0b9e1873edddaa0788320

---
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/group/GroupInfo.java
@@ -0,0 +1,116 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright (C) 2013 The Android Open Source Project
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.acceptance.rest.group
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|acceptance
+operator|.
+name|rest
+operator|.
+name|group
+package|;
+end_package
+
+begin_class
+DECL|class|GroupInfo
+specifier|public
+class|class
+name|GroupInfo
+block|{
+DECL|field|id
+specifier|public
+name|String
+name|id
+decl_stmt|;
+DECL|field|name
+specifier|public
+name|String
+name|name
+decl_stmt|;
+DECL|field|url
+specifier|public
+name|String
+name|url
+decl_stmt|;
+DECL|field|options
+specifier|public
+name|GroupOptionsInfo
+name|options
+decl_stmt|;
+DECL|field|description
+specifier|public
+name|String
+name|description
+decl_stmt|;
+DECL|field|group_id
+specifier|public
+name|Integer
+name|group_id
+decl_stmt|;
+DECL|field|owner_id
+specifier|public
+name|String
+name|owner_id
+decl_stmt|;
+block|}
+end_class
+
+end_unit
+
