63c7493bdc010d1b66ba6c93e6677a0640b94a86
Dave Borowitz <dborowitz@google.com>
2013-02-15 17:34:31 -0800

    Use per-project label types in Prolog submit rules
    Change-Id: I89fcb0b94a4f1af61d4cff7dccb18cc3dac821cd
    
    Former-commit-id: 03fbaf8655fafdba6c54b8c98c0126a699458b08

--- gerrit-server/src/main/java/com/google/gerrit/server/project/ProjectState.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/ProjectState.java
@@ -673,7 +673,7 @@ decl_stmt|;
 annotation|@
 name|Inject
 DECL|method|ProjectState ( final ProjectCache projectCache, final AllProjectsName allProjectsName, final ProjectControl.AssistedFactory projectControlFactory, final PrologEnvironment.Factory envFactory, final GitRepositoryManager gitMgr, final RulesCache rulesCache, final LabelTypes labelTypes, @Assisted final ProjectConfig config)
-specifier|protected
+specifier|public
 name|ProjectState
 parameter_list|(
 specifier|final
