d60483fccd439fc43bf499862f0f375fc27f0651
Shawn Pearce <sop@google.com>
2013-03-05 16:22:32 -0800

    Delete unused (and broke) ListChangesOption.fromValue() method
    This method does not work when the enum values are ordered
    differently from the value passed in the construct. Since its
    unused, just delete it. Nobody needs it so there is no value
    in trying to fix the code.
    
    Change-Id: I381bac6e54156813b127dfda36024b0297966dc1
    
    Former-commit-id: d669bda1deaf6dadab971efd2c3dfe2d9e7173c5

--- gerrit-common/src/main/java/com/google/gerrit/common/changes/ListChangesOption.java
+++ gerrit-common/src/main/java/com/google/gerrit/common/changes/ListChangesOption.java
@@ -175,26 +175,6 @@ return|return
 name|value
 return|;
 block|}
-DECL|method|fromValue (int value)
-specifier|public
-specifier|static
-name|ListChangesOption
-name|fromValue
-parameter_list|(
-name|int
-name|value
-parameter_list|)
-block|{
-return|return
-name|ListChangesOption
-operator|.
-name|values
-argument_list|()
-index|[
-name|value
-index|]
-return|;
-block|}
 DECL|method|fromBits (int v)
 specifier|public
 specifier|static
