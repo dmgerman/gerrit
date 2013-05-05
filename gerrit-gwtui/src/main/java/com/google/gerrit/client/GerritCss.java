0b8ff7ef178b35383685942f12f10a8b1467039b
Jan Opacki <jan.opacki@gmail.com>
2013-05-05 15:35:25 +0200

    Refactor highlighting patch sets that have drafts
    As suggested in the comments under change
    I537db90a940c9df7c4b7c28974adac5b29c8abf4:
    - Remove an unrelated field from a PatchSet entity class
    - Remove highlighting of the patch set label
    - Add a "comment" icon to a patch set header to indicate that it has draft
      comment(s)
    
    Bug: Issue 667
    Change-Id: Ie37be962ecb7beb82e85174492e154db9df6175c
    (cherry picked from commit ad0381a9dc95eca178fb561100b566e368e9f736 [formerly 61e18b0595b48766f4aebe7431f66b012b596a1c])
    Former-commit-id: 9b426203857c4bac6ea7f0c2f12df0d07effcb62

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/GerritCss.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/GerritCss.java
@@ -893,11 +893,6 @@ name|String
 name|patchSetUserIdentity
 parameter_list|()
 function_decl|;
-DECL|method|patchSetWithDraft ()
-name|String
-name|patchSetWithDraft
-parameter_list|()
-function_decl|;
 DECL|method|patchSizeCell ()
 name|String
 name|patchSizeCell
