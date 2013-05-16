1cd0d4dae64779fd357511f459078147f16033e2
Edwin Kempin <edwin.kempin@sap.com>
2013-05-16 10:36:44 +0200

    Support to get/set/delete the full name of an account via REST
    Change-Id: I248033b4121641f5035aabada4f11a99fb6934a5
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>
    
    Former-commit-id: b6df03968d64866cc89fba9a01a6b288c3d5460c

--- gerrit-server/src/main/java/com/google/gerrit/server/account/GetName.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/GetName.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|// Copyright (C) 2012 The Android Open Source Project
+comment|// Copyright (C) 2013 The Android Open Source Project
 end_comment
 
 begin_comment
@@ -52,8 +52,36 @@ comment|// limitations under the License.
 end_comment
 
 begin_package
-DECL|package|com.google.gerrit.extensions.restapi
+DECL|package|com.google.gerrit.server.account
 package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+operator|.
+name|account
+package|;
+end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|base
+operator|.
+name|Strings
+import|;
+end_import
+
+begin_import
+import|import
 name|com
 operator|.
 name|google
@@ -63,53 +91,50 @@ operator|.
 name|extensions
 operator|.
 name|restapi
-package|;
-end_package
-
-begin_comment
-comment|/** Method is not acceptable on the resource (HTTP 405 Method Not Allowed). */
-end_comment
+operator|.
+name|RestReadView
+import|;
+end_import
 
 begin_class
-DECL|class|MethodNotAllowedException
+DECL|class|GetName
 specifier|public
 class|class
-name|MethodNotAllowedException
-extends|extends
-name|RestApiException
+name|GetName
+implements|implements
+name|RestReadView
+argument_list|<
+name|AccountResource
+argument_list|>
 block|{
-DECL|field|serialVersionUID
-specifier|private
-specifier|static
-specifier|final
-name|long
-name|serialVersionUID
-init|=
-literal|1L
-decl_stmt|;
-DECL|method|MethodNotAllowedException ()
+annotation|@
+name|Override
+DECL|method|apply (AccountResource rsrc)
 specifier|public
-name|MethodNotAllowedException
-parameter_list|()
-block|{
-name|super
-argument_list|()
-expr_stmt|;
-block|}
-comment|/** @param msg error text for client describing why the method is not allowed. */
-DECL|method|MethodNotAllowedException (String msg)
-specifier|public
-name|MethodNotAllowedException
-parameter_list|(
 name|String
-name|msg
+name|apply
+parameter_list|(
+name|AccountResource
+name|rsrc
 parameter_list|)
 block|{
-name|super
+return|return
+name|Strings
+operator|.
+name|nullToEmpty
 argument_list|(
-name|msg
+name|rsrc
+operator|.
+name|getUser
+argument_list|()
+operator|.
+name|getAccount
+argument_list|()
+operator|.
+name|getFullName
+argument_list|()
 argument_list|)
-expr_stmt|;
+return|;
 block|}
 block|}
 end_class
