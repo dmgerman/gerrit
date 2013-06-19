75db3f8f88dc096a6e914c846be969a7771b27a8
Christian Aistleitner <christian@quelltextlich.at>
2013-06-19 17:54:01 +0200

    Add test infrastructure for tests that use logging and mocking
    Change-Id: I5a38a5fd3256ca05e91d234c2bec7d18c06a75a2
    
    Former-commit-id: e362959d05519f0ceb3347365fdc918030c34d9a

---
+++ gerrit-server/src/test/java/com/google/gerrit/testutil/PassThroughKeyUtilEncoder.java
@@ -0,0 +1,125 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright (C) 2013 The Android Open Source Project
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.testutil
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|testutil
+package|;
+end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gwtorm
+operator|.
+name|client
+operator|.
+name|KeyUtil
+operator|.
+name|Encoder
+import|;
+end_import
+
+begin_class
+DECL|class|PassThroughKeyUtilEncoder
+specifier|public
+class|class
+name|PassThroughKeyUtilEncoder
+extends|extends
+name|Encoder
+block|{
+annotation|@
+name|Override
+DECL|method|encode (String e)
+specifier|public
+name|String
+name|encode
+parameter_list|(
+name|String
+name|e
+parameter_list|)
+block|{
+return|return
+name|e
+return|;
+block|}
+annotation|@
+name|Override
+DECL|method|decode (String e)
+specifier|public
+name|String
+name|decode
+parameter_list|(
+name|String
+name|e
+parameter_list|)
+block|{
+return|return
+name|e
+return|;
+block|}
+block|}
+end_class
+
+end_unit
+
