dmgerman/gerrit

commit 979f02176ccb94251d7a64fc030529c8439cd849

Remove the use of + when using StringBuilder (1 of 2)
Change-Id: I010b487de26c5a16b3dfda41130d7ea91b1587e8

Former-commit-id: b2a7d239bd2cf504a9ee031253f04eedcf045c43

gerrit-gwtexpui/src/main/java/com/google/gwtexpui/linker/server/Permutation.java
@@ -462,8 +462,11 @@ name|s
 operator|.
 name|append
 argument_list|(
-literal|"f.style.cssText"
-operator|+
+literal|"f.style.cssText)"
+argument_list|)
+operator|.
+name|append
+argument_list|(
 literal|"='position:absolute;width:0;height:0;border:none';"
 argument_list|)
 expr_stmt|;

gerrit-common/src/main/java/com/google/gerrit/common/data/ParameterizedString.java
@@ -413,12 +413,18 @@ operator|.
 name|append
 argument_list|(
 literal|"{"
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 name|prs
 operator|.
 name|size
 argument_list|()
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 literal|"}"
 argument_list|)
 expr_stmt|;

gerrit-gwtexpui/src/main/java/com/google/gwtexpui/linker/rebind/ServerPlannedIFrameLinker.java
@@ -376,7 +376,12 @@ name|r
 operator|.
 name|getStrongName
 argument_list|()
-operator|+
+argument_list|)
+expr_stmt|;
+name|table
+operator|.
+name|append
+argument_list|(
 literal|"\n"
 argument_list|)
 expr_stmt|;

gerrit-httpd/src/main/java/com/google/gerrit/httpd/ProjectBasicAuthFilter.java
@@ -1040,9 +1040,15 @@ operator|.
 name|append
 argument_list|(
 literal|"realm=\""
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 name|REALM_NAME
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 literal|"\""
 argument_list|)
 expr_stmt|;

gerrit-httpd/src/main/java/com/google/gerrit/httpd/ProjectDigestFilter.java
@@ -1961,9 +1961,15 @@ operator|.
 name|append
 argument_list|(
 literal|" realm=\""
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 name|REALM_NAME
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 literal|"\""
 argument_list|)
 expr_stmt|;
@@ -2034,9 +2040,15 @@ operator|.
 name|append
 argument_list|(
 literal|", domain=\""
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 name|url
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 literal|"\""
 argument_list|)
 expr_stmt|;
@@ -2060,7 +2072,10 @@ operator|.
 name|append
 argument_list|(
 literal|", stale="
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 name|stale
 argument_list|)
 expr_stmt|;
@@ -2070,10 +2085,16 @@ operator|.
 name|append
 argument_list|(
 literal|", nonce=\""
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 name|newNonce
 argument_list|()
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 literal|"\""
 argument_list|)
 expr_stmt|;

gerrit-httpd/src/main/java/com/google/gerrit/httpd/plugins/HttpPluginServlet.java
@@ -2435,9 +2435,15 @@ operator|.
 name|append
 argument_list|(
 literal|"## "
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 name|sectionTitle
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 literal|" ##\n"
 argument_list|)
 expr_stmt|;

gerrit-openid/src/main/java/com/google/gerrit/httpd/auth/openid/XrdsServlet.java
@@ -271,9 +271,15 @@ operator|.
 name|append
 argument_list|(
 literal|"<?xml version=\"1.0\" encoding=\""
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 name|ENC
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 literal|"\"?>"
 argument_list|)
 expr_stmt|;
@@ -331,16 +337,25 @@ operator|.
 name|append
 argument_list|(
 literal|"<URI>"
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 name|url
 operator|.
 name|get
 argument_list|()
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 name|OpenIdServiceImpl
 operator|.
 name|RETURN_URL
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 literal|"</URI>"
 argument_list|)
 expr_stmt|;

gerrit-server/src/main/java/com/google/gerrit/server/ChangeUtil.java
@@ -2459,17 +2459,28 @@ name|msgBuf
 init|=
 operator|new
 name|StringBuilder
+argument_list|()
+decl_stmt|;
+name|msgBuf
+operator|.
+name|append
 argument_list|(
 literal|"Patch Set "
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 name|patchSetId
 operator|.
 name|get
 argument_list|()
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 literal|": Reverted"
 argument_list|)
-decl_stmt|;
+expr_stmt|;
 name|msgBuf
 operator|.
 name|append
@@ -2482,7 +2493,10 @@ operator|.
 name|append
 argument_list|(
 literal|"This patchset was reverted in change: "
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 name|change
 operator|.
 name|getKey

commit 10205adb4e961678c3237030143090478d4bb08d

Fix JS syntax error
Gerrit is stuck on "Loading Gerrit Code-Review..."

Broken by 979f02176ccb94251d7a64fc030529c8439cd849 [formerly b2a7d239bd2cf504a9ee031253f04eedcf045c43]

Change-Id: I7215494ad14f01ce6fe65980a95771668f358956
Former-commit-id: 7f8b23079711993d990fc445df19f5ec361e948d

gerrit-gwtexpui/src/main/java/com/google/gwtexpui/linker/server/Permutation.java
@@ -462,8 +462,10 @@ name|s
 operator|.
 name|append
 argument_list|(
-literal|"f.style.cssText)"
+literal|"f.style.cssText"
 argument_list|)
+expr_stmt|;
+name|s
 operator|.
 name|append
 argument_list|(