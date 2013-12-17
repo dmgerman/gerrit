5209b42c360a1b7d3f5fec34bb4c6b39ab841edb
Shawn Pearce <sop@google.com>
2013-12-17 14:08:17 -0800

    Remove the "Publish and Submit" button from old UI
    The button visibility is different from before, it is now always
    visible. This is confusing users who refuse to upgrade to the new
    UI. Drop the button and make users who are still using the old UI
    click two buttons to submit.
    
    Change-Id: Ieed8776a7a99e67c34ffa75a4b65f5097200c99d
    
    Former-commit-id: dfef97f6d1292f75b7b4b5d5a38c5c80b9b50222

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeConstants.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeConstants.java
@@ -765,11 +765,6 @@ name|String
 name|buttonPublishCommentsSend
 parameter_list|()
 function_decl|;
-DECL|method|buttonPublishSubmitSend ()
-name|String
-name|buttonPublishSubmitSend
-parameter_list|()
-function_decl|;
 DECL|method|buttonPublishCommentsCancel ()
 name|String
 name|buttonPublishCommentsCancel
