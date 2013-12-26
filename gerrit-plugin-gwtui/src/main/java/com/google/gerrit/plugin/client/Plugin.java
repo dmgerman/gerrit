a3ed75dbb17c2558e4a75d27d59f5108f2a6e18c
Shawn Pearce <sop@google.com>
2013-12-26 15:32:26 -0800

    Allow plugins to add entire screens to Gerrit
    Add an extension screen which contains a hook that can be used by
    GWT and JS plugins to integrate complete screens into the Gerrit UI.
    
    Change-Id: I48a92ea49c4384db034177b04b2e7f0bea6a87b2
    
    Former-commit-id: d5c844f43e56f5b40c84f816618afb8c2e707d31

--- gerrit-plugin-gwtui/src/main/java/com/google/gerrit/plugin/client/Plugin.java
+++ gerrit-plugin-gwtui/src/main/java/com/google/gerrit/plugin/client/Plugin.java
@@ -72,6 +72,24 @@ name|com
 operator|.
 name|google
 operator|.
+name|gerrit
+operator|.
+name|plugin
+operator|.
+name|client
+operator|.
+name|screen
+operator|.
+name|Screen
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
 name|gwt
 operator|.
 name|core
@@ -191,6 +209,90 @@ name|refresh
 parameter_list|()
 comment|/*-{ return this.refresh() }-*/
 function_decl|;
+comment|/**    * Register a screen displayed at {@code /#/x/plugin/token}.    *    * @param token literal anchor token appearing after the plugin name. For    *        regular expression matching use {@code screenRegex()} .    * @param entry callback function invoked to create the screen widgets.    */
+DECL|method|screen (String token, Screen.EntryPoint entry)
+specifier|public
+specifier|final
+name|void
+name|screen
+parameter_list|(
+name|String
+name|token
+parameter_list|,
+name|Screen
+operator|.
+name|EntryPoint
+name|entry
+parameter_list|)
+block|{
+name|screen
+argument_list|(
+name|token
+argument_list|,
+name|wrap
+argument_list|(
+name|entry
+argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
+DECL|method|screen (String t, JavaScriptObject e)
+specifier|private
+specifier|final
+specifier|native
+name|void
+name|screen
+parameter_list|(
+name|String
+name|t
+parameter_list|,
+name|JavaScriptObject
+name|e
+parameter_list|)
+comment|/*-{ this.screen(t, e) }-*/
+function_decl|;
+comment|/**    * Register a screen displayed at {@code /#/x/plugin/regex}.    *    * @param regex JavaScript {@code RegExp} expression to match the anchor token    *        after the plugin name. Matching groups are exposed through the    *        {@code Screen} object passed into the {@code Screen.EntryPoint}.    * @param entry callback function invoked to create the screen widgets.    */
+DECL|method|screenRegex (String regex, Screen.EntryPoint entry)
+specifier|public
+specifier|final
+name|void
+name|screenRegex
+parameter_list|(
+name|String
+name|regex
+parameter_list|,
+name|Screen
+operator|.
+name|EntryPoint
+name|entry
+parameter_list|)
+block|{
+name|screenRegex
+argument_list|(
+name|regex
+argument_list|,
+name|wrap
+argument_list|(
+name|entry
+argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
+DECL|method|screenRegex (String p, JavaScriptObject e)
+specifier|private
+specifier|final
+specifier|native
+name|void
+name|screenRegex
+parameter_list|(
+name|String
+name|p
+parameter_list|,
+name|JavaScriptObject
+name|e
+parameter_list|)
+comment|/*-{ this.screen(new $wnd.RegExp(p), e) }-*/
+function_decl|;
 DECL|method|Plugin ()
 specifier|protected
 name|Plugin
@@ -222,6 +324,21 @@ name|String
 name|u
 parameter_list|)
 comment|/*-{ return $wnd.Gerrit.installGwt(u) }-*/
+function_decl|;
+DECL|method|wrap (Screen.EntryPoint b)
+specifier|private
+specifier|static
+specifier|final
+specifier|native
+name|JavaScriptObject
+name|wrap
+parameter_list|(
+name|Screen
+operator|.
+name|EntryPoint
+name|b
+parameter_list|)
+comment|/*-{     return $entry(function(c){       b.@com.google.gerrit.plugin.client.screen.Screen.EntryPoint::onLoad(Lcom/google/gerrit/plugin/client/screen/Screen;)(         @com.google.gerrit.plugin.client.screen.Screen::new(Lcom/google/gerrit/plugin/client/screen/Screen$Context;)(c));     });   }-*/
 function_decl|;
 block|}
 end_class
