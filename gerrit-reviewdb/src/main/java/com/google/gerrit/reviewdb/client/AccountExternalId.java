44393f8e93ec1c85ff5f484aea0b79496e4a515e
Bruce Zu <bruce.zu@sonymobile.com>
2014-02-21 12:16:52 +0800

    Enable updating the full-name of manually created account
    When Gerrit runs with LDAP backend, make it possible to
    update the full name of manually created account.
    
    Change-Id: I27aafaba263db1794fb808dccb7ccb46b393741f
    
    Former-commit-id: 9a52469b2b1dda6f8c13317902b49f5e2b97c37d

--- gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/client/AccountExternalId.java
+++ gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/client/AccountExternalId.java
@@ -105,7 +105,7 @@ specifier|final
 class|class
 name|AccountExternalId
 block|{
-comment|/**    * Scheme used for {@link AuthType#LDAP}, {@link AuthType#HTTP},    * {@link AuthType#HTTP_LDAP}, and {@link AuthType#LDAP_BIND} usernames.    *<p>    * The name {@code gerrit:} was a very poor choice.    */
+comment|/**    * Scheme used for {@link AuthType#LDAP}, {@link AuthType#CLIENT_SSL_CERT_LDAP},    * {@link AuthType#HTTP_LDAP}, and {@link AuthType#LDAP_BIND} usernames.    *<p>    * The name {@code gerrit:} was a very poor choice.    */
 DECL|field|SCHEME_GERRIT
 specifier|public
 specifier|static
