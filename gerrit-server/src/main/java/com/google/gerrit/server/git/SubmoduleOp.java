bf0dab7f05c18561be2e507571ad0d58e0412f7b
David Pursehouse <david.pursehouse@sonymobile.com>
2014-03-05 15:19:48 +0900

    RFC: Work around null commit when updating submodules
    Bug: Issue 2498
    Change-Id: I5d82ead765bc743d3392a1333cd2c3fc4cf971c7
    
    Former-commit-id: f23580b9054c3041b41d9eba42b7457f217077be

--- gerrit-server/src/main/java/com/google/gerrit/server/git/SubmoduleOp.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/SubmoduleOp.java
@@ -1883,6 +1883,15 @@ name|getValue
 argument_list|()
 argument_list|)
 decl_stmt|;
+if|if
+condition|(
+name|c
+operator|==
+literal|null
+condition|)
+block|{
+continue|continue;
+block|}
 name|msgbuf
 operator|.
 name|append
