dmgerman/gerrit

commit da87e49c23a3cab2a0dad75910862e95ac89faea

Include AvatarInfo as part of AccountInfo
Instead of the browser sending many RPCs to the server to ask for
each /accounts/{id}/avatar URL, include avatar information in the
JSON response as part of any REST API producing AccountInfo.

If avatars are not enabled on the server every AccountInfo now has
an empty avatar list ("avatars:[]") making a positive assertion
that no avatars exist for the descibed account. This avoids a large
number of requests on busy change pages.

If avatars are enabled the server automatically includes the URL for
the "size 26 pixel" image. Currently we assume this is 26px along
the height dimension. The avatar plugin API needs to be reworked
to permit better description of the image to be carried around.

Change-Id: I3f6fe0458feef10441c8ed8467c71b2ef7b00acf

Former-commit-id: edb9d2275cfbc5e4905e21786709bce51e7b9e00

gerrit-gwtui/src/main/java/com/google/gerrit/client/AvatarImage.java
@@ -90,6 +90,24 @@ name|gerrit
 operator|.
 name|client
 operator|.
+name|account
+operator|.
+name|AccountInfo
+operator|.
+name|AvatarInfo
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|client
+operator|.
 name|changes
 operator|.
 name|Util
@@ -279,12 +297,25 @@ class|class
 name|AvatarImage
 extends|extends
 name|Image
+implements|implements
+name|LoadHandler
 block|{
 DECL|method|AvatarImage ()
 specifier|public
 name|AvatarImage
 parameter_list|()
-block|{   }
+block|{
+name|setVisible
+argument_list|(
+literal|false
+argument_list|)
+expr_stmt|;
+name|addLoadHandler
+argument_list|(
+name|this
+argument_list|)
+expr_stmt|;
+block|}
 comment|/** A default sized avatar image. */
 DECL|method|AvatarImage (AccountInfo account)
 specifier|public
@@ -298,7 +329,9 @@ name|this
 argument_list|(
 name|account
 argument_list|,
-literal|0
+literal|26
+argument_list|,
+literal|true
 argument_list|)
 expr_stmt|;
 block|}
@@ -339,6 +372,11 @@ name|boolean
 name|addPopup
 parameter_list|)
 block|{
+name|addLoadHandler
+argument_list|(
+name|this
+argument_list|)
+expr_stmt|;
 name|setAccount
 argument_list|(
 name|account
@@ -364,187 +402,149 @@ name|boolean
 name|addPopup
 parameter_list|)
 block|{
-name|setUrl
-argument_list|(
-name|isGerritServer
-argument_list|(
+if|if
+condition|(
 name|account
-argument_list|)
-condition|?
-name|getGerritServerAvatarUrl
-argument_list|()
-else|:
-name|url
-argument_list|(
-name|account
-operator|.
-name|email
-argument_list|()
-argument_list|,
-name|size
-argument_list|)
-argument_list|)
-expr_stmt|;
+operator|==
+literal|null
+condition|)
+block|{
 name|setVisible
 argument_list|(
 literal|false
 argument_list|)
 expr_stmt|;
+block|}
+elseif|else
 if|if
 condition|(
-name|size
-operator|>
-literal|0
-condition|)
-block|{
-comment|// If the provider does not resize the image, force it in the browser.
-name|setSize
+name|isGerritServer
 argument_list|(
-name|size
-operator|+
-literal|"px"
-argument_list|,
-name|size
-operator|+
-literal|"px"
+name|account
 argument_list|)
-expr_stmt|;
-block|}
-name|addLoadHandler
-argument_list|(
-operator|new
-name|LoadHandler
-argument_list|()
-block|{
-annotation|@
-name|Override
-specifier|public
-name|void
-name|onLoad
-parameter_list|(
-name|LoadEvent
-name|event
-parameter_list|)
+condition|)
 block|{
 name|setVisible
 argument_list|(
 literal|true
 argument_list|)
 expr_stmt|;
-block|}
-block|}
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|addPopup
-condition|)
-block|{
-name|PopupHandler
-name|popupHandler
-init|=
-operator|new
-name|PopupHandler
+name|setResource
 argument_list|(
-name|account
-argument_list|,
-name|this
-argument_list|)
-decl_stmt|;
-name|addMouseOverHandler
-argument_list|(
-name|popupHandler
-argument_list|)
-expr_stmt|;
-name|addMouseOutHandler
-argument_list|(
-name|popupHandler
-argument_list|)
-expr_stmt|;
-block|}
-block|}
-DECL|method|isGerritServer (AccountInfo account)
-specifier|private
-specifier|static
-name|boolean
-name|isGerritServer
-parameter_list|(
-name|AccountInfo
-name|account
-parameter_list|)
-block|{
-return|return
-name|account
-operator|.
-name|_account_id
-argument_list|()
-operator|==
-literal|0
-operator|&&
-name|Util
-operator|.
-name|C
-operator|.
-name|messageNoAuthor
-argument_list|()
-operator|.
-name|equals
-argument_list|(
-name|account
-operator|.
-name|name
-argument_list|()
-argument_list|)
-return|;
-block|}
-DECL|method|getGerritServerAvatarUrl ()
-specifier|private
-specifier|static
-name|String
-name|getGerritServerAvatarUrl
-parameter_list|()
-block|{
-return|return
 name|Gerrit
 operator|.
 name|RESOURCES
 operator|.
 name|gerritAvatar
 argument_list|()
-operator|.
-name|getSafeUri
-argument_list|()
-operator|.
-name|asString
-argument_list|()
-return|;
+argument_list|)
+expr_stmt|;
 block|}
-DECL|method|url (String email, int size)
-specifier|private
-specifier|static
-name|String
-name|url
-parameter_list|(
-name|String
-name|email
-parameter_list|,
-name|int
-name|size
-parameter_list|)
-block|{
+elseif|else
 if|if
 condition|(
-name|email
-operator|==
+name|account
+operator|.
+name|has_avatar_info
+argument_list|()
+condition|)
+block|{
+name|setVisible
+argument_list|(
+literal|false
+argument_list|)
+expr_stmt|;
+name|AvatarInfo
+name|info
+init|=
+name|account
+operator|.
+name|avatar
+argument_list|(
+name|size
+argument_list|)
+decl_stmt|;
+if|if
+condition|(
+name|info
+operator|!=
 literal|null
 condition|)
 block|{
-return|return
+name|setWidth
+argument_list|(
+name|info
+operator|.
+name|width
+argument_list|()
+operator|>
+literal|0
+condition|?
+name|info
+operator|.
+name|width
+argument_list|()
+operator|+
+literal|"px"
+else|:
 literal|""
-return|;
+argument_list|)
+expr_stmt|;
+name|setHeight
+argument_list|(
+name|info
+operator|.
+name|height
+argument_list|()
+operator|>
+literal|0
+condition|?
+name|info
+operator|.
+name|height
+argument_list|()
+operator|+
+literal|"px"
+else|:
+literal|""
+argument_list|)
+expr_stmt|;
+name|setUrl
+argument_list|(
+name|info
+operator|.
+name|url
+argument_list|()
+argument_list|)
+expr_stmt|;
+name|popup
+argument_list|(
+name|account
+argument_list|,
+name|addPopup
+argument_list|)
+expr_stmt|;
 block|}
+block|}
+elseif|else
+if|if
+condition|(
+name|account
+operator|.
+name|email
+argument_list|()
+operator|!=
+literal|null
+condition|)
+block|{
+comment|// TODO Kill /accounts/*/avatar URL.
 name|String
 name|u
+init|=
+name|account
+operator|.
+name|email
+argument_list|()
 decl_stmt|;
 if|if
 condition|(
@@ -553,7 +553,7 @@ operator|.
 name|isSignedIn
 argument_list|()
 operator|&&
-name|email
+name|u
 operator|.
 name|equals
 argument_list|(
@@ -570,13 +570,6 @@ block|{
 name|u
 operator|=
 literal|"self"
-expr_stmt|;
-block|}
-else|else
-block|{
-name|u
-operator|=
-name|email
 expr_stmt|;
 block|}
 name|RestApi
@@ -614,16 +607,140 @@ argument_list|,
 name|size
 argument_list|)
 expr_stmt|;
+name|setSize
+argument_list|(
+literal|""
+argument_list|,
+name|size
+operator|+
+literal|"px"
+argument_list|)
+expr_stmt|;
 block|}
-return|return
+name|setVisible
+argument_list|(
+literal|false
+argument_list|)
+expr_stmt|;
+name|setUrl
+argument_list|(
 name|api
 operator|.
 name|url
 argument_list|()
+argument_list|)
+expr_stmt|;
+name|popup
+argument_list|(
+name|account
+argument_list|,
+name|addPopup
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
+name|setVisible
+argument_list|(
+literal|false
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+DECL|method|popup (AccountInfo account, boolean addPopup)
+specifier|private
+name|void
+name|popup
+parameter_list|(
+name|AccountInfo
+name|account
+parameter_list|,
+name|boolean
+name|addPopup
+parameter_list|)
+block|{
+if|if
+condition|(
+name|addPopup
+condition|)
+block|{
+name|PopupHandler
+name|popupHandler
+init|=
+operator|new
+name|PopupHandler
+argument_list|(
+name|account
+argument_list|,
+name|this
+argument_list|)
+decl_stmt|;
+name|addMouseOverHandler
+argument_list|(
+name|popupHandler
+argument_list|)
+expr_stmt|;
+name|addMouseOutHandler
+argument_list|(
+name|popupHandler
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+annotation|@
+name|Override
+DECL|method|onLoad (LoadEvent event)
+specifier|public
+name|void
+name|onLoad
+parameter_list|(
+name|LoadEvent
+name|event
+parameter_list|)
+block|{
+name|setVisible
+argument_list|(
+literal|true
+argument_list|)
+expr_stmt|;
+block|}
+DECL|method|isGerritServer (AccountInfo account)
+specifier|private
+specifier|static
+name|boolean
+name|isGerritServer
+parameter_list|(
+name|AccountInfo
+name|account
+parameter_list|)
+block|{
+return|return
+name|account
+operator|.
+name|_account_id
+argument_list|()
+operator|==
+literal|0
+operator|&&
+name|Util
+operator|.
+name|C
+operator|.
+name|messageNoAuthor
+argument_list|()
+operator|.
+name|equals
+argument_list|(
+name|account
+operator|.
+name|name
+argument_list|()
+argument_list|)
 return|;
 block|}
 DECL|class|PopupHandler
 specifier|private
+specifier|static
 class|class
 name|PopupHandler
 implements|implements

gerrit-gwtui/src/main/java/com/google/gerrit/client/account/AccountInfo.java
@@ -82,6 +82,22 @@ name|JavaScriptObject
 import|;
 end_import
 
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gwt
+operator|.
+name|core
+operator|.
+name|client
+operator|.
+name|JsArray
+import|;
+end_import
+
 begin_class
 DECL|class|AccountInfo
 specifier|public
@@ -117,6 +133,98 @@ name|email
 parameter_list|()
 comment|/*-{ return this.email; }-*/
 function_decl|;
+comment|/**    * @return true if the server supplied avatar information about this account.    *         The information may be an empty list, indicating no avatars are    *         available, such as when no plugin is installed. This method returns    *         false if the server did not check on avatars for the account.    */
+DECL|method|has_avatar_info ()
+specifier|public
+specifier|final
+specifier|native
+name|boolean
+name|has_avatar_info
+parameter_list|()
+comment|/*-{ return this.hasOwnProperty('avatars') }-*/
+function_decl|;
+DECL|method|avatar (int sz)
+specifier|public
+specifier|final
+name|AvatarInfo
+name|avatar
+parameter_list|(
+name|int
+name|sz
+parameter_list|)
+block|{
+name|JsArray
+argument_list|<
+name|AvatarInfo
+argument_list|>
+name|a
+init|=
+name|avatars
+argument_list|()
+decl_stmt|;
+for|for
+control|(
+name|int
+name|i
+init|=
+literal|0
+init|;
+name|a
+operator|!=
+literal|null
+operator|&&
+name|i
+operator|<
+name|a
+operator|.
+name|length
+argument_list|()
+condition|;
+name|i
+operator|++
+control|)
+block|{
+name|AvatarInfo
+name|r
+init|=
+name|a
+operator|.
+name|get
+argument_list|(
+name|i
+argument_list|)
+decl_stmt|;
+if|if
+condition|(
+name|r
+operator|.
+name|height
+argument_list|()
+operator|==
+name|sz
+condition|)
+block|{
+return|return
+name|r
+return|;
+block|}
+block|}
+return|return
+literal|null
+return|;
+block|}
+DECL|method|avatars ()
+specifier|private
+specifier|final
+specifier|native
+name|JsArray
+argument_list|<
+name|AvatarInfo
+argument_list|>
+name|avatars
+parameter_list|()
+comment|/*-{ return this.avatars }-*/
+function_decl|;
 DECL|method|create (int id, String name, String email)
 specifier|public
 specifier|static
@@ -140,6 +248,47 @@ specifier|protected
 name|AccountInfo
 parameter_list|()
 block|{   }
+DECL|class|AvatarInfo
+specifier|public
+specifier|static
+class|class
+name|AvatarInfo
+extends|extends
+name|JavaScriptObject
+block|{
+DECL|method|url ()
+specifier|public
+specifier|final
+specifier|native
+name|String
+name|url
+parameter_list|()
+comment|/*-{ return this.url }-*/
+function_decl|;
+DECL|method|height ()
+specifier|public
+specifier|final
+specifier|native
+name|int
+name|height
+parameter_list|()
+comment|/*-{ return this.height || 0 }-*/
+function_decl|;
+DECL|method|width ()
+specifier|public
+specifier|final
+specifier|native
+name|int
+name|width
+parameter_list|()
+comment|/*-{ return this.width || 0 }-*/
+function_decl|;
+DECL|method|AvatarInfo ()
+specifier|protected
+name|AvatarInfo
+parameter_list|()
+block|{     }
+block|}
 block|}
 end_class
 

gerrit-gwtui/src/main/java/com/google/gerrit/client/change/Message.java
@@ -460,8 +460,6 @@ name|info
 operator|.
 name|author
 argument_list|()
-argument_list|,
-literal|26
 argument_list|)
 expr_stmt|;
 name|avatar

gerrit-gwtui/src/main/java/com/google/gerrit/client/diff/PublishedBox.java
@@ -611,8 +611,6 @@ name|info
 operator|.
 name|author
 argument_list|()
-argument_list|,
-literal|26
 argument_list|)
 expr_stmt|;
 name|avatar

gerrit-gwtui/src/main/java/com/google/gerrit/client/ui/CommentPanel.java
@@ -1174,8 +1174,6 @@ operator|new
 name|AvatarImage
 argument_list|(
 name|author
-argument_list|,
-literal|26
 argument_list|)
 argument_list|)
 expr_stmt|;

gerrit-server/src/main/java/com/google/gerrit/server/account/AccountInfo.java
@@ -158,6 +158,22 @@ name|google
 operator|.
 name|gerrit
 operator|.
+name|extensions
+operator|.
+name|registration
+operator|.
+name|DynamicItem
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
 name|reviewdb
 operator|.
 name|client
@@ -179,6 +195,36 @@ operator|.
 name|server
 operator|.
 name|ReviewDb
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+operator|.
+name|IdentifiedUser
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+operator|.
+name|avatar
+operator|.
+name|AvatarProvider
 import|;
 end_import
 
@@ -305,6 +351,23 @@ specifier|final
 name|AccountCache
 name|accountCache
 decl_stmt|;
+DECL|field|avatar
+specifier|private
+specifier|final
+name|DynamicItem
+argument_list|<
+name|AvatarProvider
+argument_list|>
+name|avatar
+decl_stmt|;
+DECL|field|userFactory
+specifier|private
+specifier|final
+name|IdentifiedUser
+operator|.
+name|GenericFactory
+name|userFactory
+decl_stmt|;
 DECL|field|detailed
 specifier|private
 specifier|final
@@ -335,7 +398,7 @@ name|provided
 decl_stmt|;
 annotation|@
 name|Inject
-DECL|method|Loader (Provider<ReviewDb> db, AccountCache accountCache, @Assisted boolean detailed)
+DECL|method|Loader (Provider<ReviewDb> db, AccountCache accountCache, DynamicItem<AvatarProvider> avatar, IdentifiedUser.GenericFactory userFactory, @Assisted boolean detailed)
 name|Loader
 parameter_list|(
 name|Provider
@@ -346,6 +409,17 @@ name|db
 parameter_list|,
 name|AccountCache
 name|accountCache
+parameter_list|,
+name|DynamicItem
+argument_list|<
+name|AvatarProvider
+argument_list|>
+name|avatar
+parameter_list|,
+name|IdentifiedUser
+operator|.
+name|GenericFactory
+name|userFactory
 parameter_list|,
 annotation|@
 name|Assisted
@@ -364,6 +438,18 @@ operator|.
 name|accountCache
 operator|=
 name|accountCache
+expr_stmt|;
+name|this
+operator|.
+name|avatar
+operator|=
+name|avatar
+expr_stmt|;
+name|this
+operator|.
+name|userFactory
+operator|=
+name|userFactory
 expr_stmt|;
 name|this
 operator|.
@@ -524,16 +610,14 @@ operator|!=
 literal|null
 condition|)
 block|{
-name|info
-operator|.
 name|fill
 argument_list|(
+name|info
+argument_list|,
 name|state
 operator|.
 name|getAccount
 argument_list|()
-argument_list|,
-name|detailed
 argument_list|)
 expr_stmt|;
 block|}
@@ -599,13 +683,11 @@ argument_list|()
 argument_list|)
 control|)
 block|{
-name|info
-operator|.
 name|fill
 argument_list|(
-name|account
+name|info
 argument_list|,
-name|detailed
+name|account
 argument_list|)
 expr_stmt|;
 block|}
@@ -646,44 +728,158 @@ name|fill
 argument_list|()
 expr_stmt|;
 block|}
-block|}
-DECL|method|parse (Account a, boolean detailed)
-specifier|public
-specifier|static
-name|AccountInfo
-name|parse
+DECL|method|fill (AccountInfo info, Account account)
+specifier|private
+name|void
+name|fill
 parameter_list|(
-name|Account
-name|a
+name|AccountInfo
+name|info
 parameter_list|,
-name|boolean
-name|detailed
+name|Account
+name|account
 parameter_list|)
 block|{
-name|AccountInfo
-name|ai
-init|=
-operator|new
-name|AccountInfo
+name|info
+operator|.
+name|name
+operator|=
+name|Strings
+operator|.
+name|emptyToNull
 argument_list|(
-name|a
+name|account
+operator|.
+name|getFullName
+argument_list|()
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|info
+operator|.
+name|name
+operator|==
+literal|null
+condition|)
+block|{
+name|info
+operator|.
+name|name
+operator|=
+name|account
+operator|.
+name|getUserName
+argument_list|()
+expr_stmt|;
+block|}
+if|if
+condition|(
+name|detailed
+condition|)
+block|{
+name|info
+operator|.
+name|_account_id
+operator|=
+name|account
+operator|.
+name|getId
+argument_list|()
+operator|.
+name|get
+argument_list|()
+expr_stmt|;
+name|info
+operator|.
+name|email
+operator|=
+name|account
+operator|.
+name|getPreferredEmail
+argument_list|()
+expr_stmt|;
+name|info
+operator|.
+name|username
+operator|=
+name|account
+operator|.
+name|getUserName
+argument_list|()
+expr_stmt|;
+block|}
+name|info
+operator|.
+name|avatars
+operator|=
+name|Lists
+operator|.
+name|newArrayListWithCapacity
+argument_list|(
+literal|1
+argument_list|)
+expr_stmt|;
+name|AvatarProvider
+name|ap
+init|=
+name|avatar
+operator|.
+name|get
+argument_list|()
+decl_stmt|;
+if|if
+condition|(
+name|ap
+operator|!=
+literal|null
+condition|)
+block|{
+name|String
+name|u
+init|=
+name|ap
+operator|.
+name|getUrl
+argument_list|(
+name|userFactory
+operator|.
+name|create
+argument_list|(
+name|account
 operator|.
 name|getId
 argument_list|()
 argument_list|)
-decl_stmt|;
-name|ai
-operator|.
-name|fill
-argument_list|(
-name|a
 argument_list|,
-name|detailed
+literal|26
+argument_list|)
+decl_stmt|;
+if|if
+condition|(
+name|u
+operator|!=
+literal|null
+condition|)
+block|{
+name|info
+operator|.
+name|avatars
+operator|.
+name|add
+argument_list|(
+operator|new
+name|AvatarInfo
+argument_list|(
+name|u
+argument_list|,
+literal|26
+argument_list|)
 argument_list|)
 expr_stmt|;
-return|return
-name|ai
-return|;
+block|}
+block|}
+block|}
 block|}
 DECL|field|_id
 specifier|public
@@ -728,73 +924,49 @@ specifier|public
 name|String
 name|username
 decl_stmt|;
-DECL|method|fill (Account account, boolean detailed)
-specifier|private
-name|void
-name|fill
+DECL|field|avatars
+specifier|public
+name|List
+argument_list|<
+name|AvatarInfo
+argument_list|>
+name|avatars
+decl_stmt|;
+DECL|class|AvatarInfo
+specifier|public
+specifier|static
+class|class
+name|AvatarInfo
+block|{
+DECL|field|url
+name|String
+name|url
+decl_stmt|;
+DECL|field|height
+name|int
+name|height
+decl_stmt|;
+DECL|method|AvatarInfo (String url, int height)
+name|AvatarInfo
 parameter_list|(
-name|Account
-name|account
+name|String
+name|url
 parameter_list|,
-name|boolean
-name|detailed
+name|int
+name|height
 parameter_list|)
 block|{
-name|name
+name|this
+operator|.
+name|url
 operator|=
-name|Strings
-operator|.
-name|emptyToNull
-argument_list|(
-name|account
-operator|.
-name|getFullName
-argument_list|()
-argument_list|)
+name|url
 expr_stmt|;
-if|if
-condition|(
-name|name
-operator|==
-literal|null
-condition|)
-block|{
-name|name
+name|this
+operator|.
+name|height
 operator|=
-name|account
-operator|.
-name|getUserName
-argument_list|()
-expr_stmt|;
-block|}
-if|if
-condition|(
-name|detailed
-condition|)
-block|{
-name|_account_id
-operator|=
-name|account
-operator|.
-name|getId
-argument_list|()
-operator|.
-name|get
-argument_list|()
-expr_stmt|;
-name|email
-operator|=
-name|account
-operator|.
-name|getPreferredEmail
-argument_list|()
-expr_stmt|;
-name|username
-operator|=
-name|account
-operator|.
-name|getUserName
-argument_list|()
+name|height
 expr_stmt|;
 block|}
 block|}

gerrit-server/src/main/java/com/google/gerrit/server/account/CreateAccount.java
@@ -629,6 +629,16 @@ specifier|final
 name|AccountByEmailCache
 name|byEmailCache
 decl_stmt|;
+DECL|field|infoLoader
+specifier|private
+specifier|final
+name|AccountInfo
+operator|.
+name|Loader
+operator|.
+name|Factory
+name|infoLoader
+decl_stmt|;
 DECL|field|username
 specifier|private
 specifier|final
@@ -637,7 +647,7 @@ name|username
 decl_stmt|;
 annotation|@
 name|Inject
-DECL|method|CreateAccount (ReviewDb db, IdentifiedUser currentUser, GroupsCollection groupsCollection, SshKeyCache sshKeyCache, AccountCache accountCache, AccountByEmailCache byEmailCache, @Assisted String username)
+DECL|method|CreateAccount (ReviewDb db, IdentifiedUser currentUser, GroupsCollection groupsCollection, SshKeyCache sshKeyCache, AccountCache accountCache, AccountByEmailCache byEmailCache, AccountInfo.Loader.Factory infoLoader, @Assisted String username)
 name|CreateAccount
 parameter_list|(
 name|ReviewDb
@@ -657,6 +667,13 @@ name|accountCache
 parameter_list|,
 name|AccountByEmailCache
 name|byEmailCache
+parameter_list|,
+name|AccountInfo
+operator|.
+name|Loader
+operator|.
+name|Factory
+name|infoLoader
 parameter_list|,
 annotation|@
 name|Assisted
@@ -699,6 +716,12 @@ operator|.
 name|byEmailCache
 operator|=
 name|byEmailCache
+expr_stmt|;
+name|this
+operator|.
+name|infoLoader
+operator|=
+name|infoLoader
 expr_stmt|;
 name|this
 operator|.
@@ -1238,19 +1261,39 @@ operator|.
 name|email
 argument_list|)
 expr_stmt|;
+name|AccountInfo
+operator|.
+name|Loader
+name|loader
+init|=
+name|infoLoader
+operator|.
+name|create
+argument_list|(
+literal|true
+argument_list|)
+decl_stmt|;
+name|AccountInfo
+name|info
+init|=
+name|loader
+operator|.
+name|get
+argument_list|(
+name|id
+argument_list|)
+decl_stmt|;
+name|loader
+operator|.
+name|fill
+argument_list|()
+expr_stmt|;
 return|return
 name|Response
 operator|.
 name|created
 argument_list|(
-name|AccountInfo
-operator|.
-name|parse
-argument_list|(
-name|a
-argument_list|,
-literal|true
-argument_list|)
+name|info
 argument_list|)
 return|;
 block|}

gerrit-server/src/main/java/com/google/gerrit/server/account/GetAccount.java
@@ -82,6 +82,32 @@ name|RestReadView
 import|;
 end_import
 
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gwtorm
+operator|.
+name|server
+operator|.
+name|OrmException
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|inject
+operator|.
+name|Inject
+import|;
+end_import
+
 begin_class
 DECL|class|GetAccount
 specifier|public
@@ -93,6 +119,36 @@ argument_list|<
 name|AccountResource
 argument_list|>
 block|{
+DECL|field|infoFactory
+specifier|private
+specifier|final
+name|AccountInfo
+operator|.
+name|Loader
+operator|.
+name|Factory
+name|infoFactory
+decl_stmt|;
+annotation|@
+name|Inject
+DECL|method|GetAccount (AccountInfo.Loader.Factory infoFactory)
+name|GetAccount
+parameter_list|(
+name|AccountInfo
+operator|.
+name|Loader
+operator|.
+name|Factory
+name|infoFactory
+parameter_list|)
+block|{
+name|this
+operator|.
+name|infoFactory
+operator|=
+name|infoFactory
+expr_stmt|;
+block|}
 annotation|@
 name|Override
 DECL|method|apply (AccountResource rsrc)
@@ -103,22 +159,44 @@ parameter_list|(
 name|AccountResource
 name|rsrc
 parameter_list|)
+throws|throws
+name|OrmException
 block|{
-return|return
 name|AccountInfo
 operator|.
-name|parse
+name|Loader
+name|loader
+init|=
+name|infoFactory
+operator|.
+name|create
+argument_list|(
+literal|true
+argument_list|)
+decl_stmt|;
+name|AccountInfo
+name|info
+init|=
+name|loader
+operator|.
+name|get
 argument_list|(
 name|rsrc
 operator|.
 name|getUser
 argument_list|()
 operator|.
-name|getAccount
+name|getAccountId
 argument_list|()
-argument_list|,
-literal|true
 argument_list|)
+decl_stmt|;
+name|loader
+operator|.
+name|fill
+argument_list|()
+expr_stmt|;
+return|return
+name|info
 return|;
 block|}
 block|}

commit d7118dd8a21939ab972904d9ea163786eb0bd963

New review UI: Fix display of large avatar image in user popup panel
Hovering with the mouse over an avatar image opens a user popup panel
that shows additional information for this user. Beside the name and
the email address it is supposed to show a larger version of the
avatar image. This is important since the avatar images in the new
review UI (new change screen + new side-by-side diff screen) are very
tiny and the persons are difficult to recognize (note that currently
there are no user popups for the avatar images that are displayed for
reviewers and approvals, but only for change messages and inline
comments).

The avatar image in its default size is included in AccountInfo, but
if a different size was requested it didn't load the image from the
server. The problem was likely introduced by commit
da87e49c23a3cab2a0dad75910862e95ac89faea [formerly edb9d2275cfbc5e4905e21786709bce51e7b9e00].

On the old review UI this works properly, so it's a regression with
the new review UI.

Change-Id: Iafa23377aa115ad4dfbe6c0fcd939bcf75b857f7
Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

Former-commit-id: f712cdb27c74d8e19f19e3c15c9bbc32069187d2

gerrit-gwtui/src/main/java/com/google/gerrit/client/AvatarImage.java
@@ -529,6 +529,27 @@ name|addPopup
 argument_list|)
 expr_stmt|;
 block|}
+elseif|else
+if|if
+condition|(
+name|account
+operator|.
+name|email
+argument_list|()
+operator|!=
+literal|null
+condition|)
+block|{
+name|loadAvatar
+argument_list|(
+name|account
+argument_list|,
+name|size
+argument_list|,
+name|addPopup
+argument_list|)
+expr_stmt|;
+block|}
 block|}
 elseif|else
 if|if
@@ -540,6 +561,40 @@ argument_list|()
 operator|!=
 literal|null
 condition|)
+block|{
+name|loadAvatar
+argument_list|(
+name|account
+argument_list|,
+name|size
+argument_list|,
+name|addPopup
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
+name|setVisible
+argument_list|(
+literal|false
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+DECL|method|loadAvatar (AccountInfo account, int size, boolean addPopup)
+specifier|private
+name|void
+name|loadAvatar
+parameter_list|(
+name|AccountInfo
+name|account
+parameter_list|,
+name|int
+name|size
+parameter_list|,
+name|boolean
+name|addPopup
+parameter_list|)
 block|{
 comment|// TODO Kill /accounts/*/avatar URL.
 name|String
@@ -641,15 +696,6 @@ argument_list|,
 name|addPopup
 argument_list|)
 expr_stmt|;
-block|}
-else|else
-block|{
-name|setVisible
-argument_list|(
-literal|false
-argument_list|)
-expr_stmt|;
-block|}
 block|}
 DECL|method|popup (AccountInfo account, boolean addPopup)
 specifier|private