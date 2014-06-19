ecc890ee9ebcb99fc5bab75ba8a5e95531e34a3a
David Ostrovsky <david@ostrovsky.org>
2014-06-19 11:35:43 +0200

    Move files endpoint to singleton scope
    Change-Id: Ib6ede6eb66b6f0cd280f979c74e2bb537e5547d7
    
    Former-commit-id: c0bbd39c8b8342257e1ce1a6403b9498e24c3ba5

--- gerrit-server/src/main/java/com/google/gerrit/server/change/Files.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/Files.java
@@ -482,6 +482,18 @@ end_import
 
 begin_import
 import|import
+name|com
+operator|.
+name|google
+operator|.
+name|inject
+operator|.
+name|Singleton
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|eclipse
@@ -657,6 +669,8 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|Singleton
 DECL|class|Files
 specifier|public
 class|class
