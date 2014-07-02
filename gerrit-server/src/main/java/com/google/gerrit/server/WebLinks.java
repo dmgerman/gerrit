29ca77294241c044a179032a9293487c05308a40
Hugo Arès <hugo.ares@ericsson.com>
2014-07-02 13:29:17 -0400

    Remove unused imports
    Change-Id: Ifded80d3d68bf67803ca913399e455e90f19d115
    
    Former-commit-id: e45a70023b321746a0acd2e4228ea9487e1a7d6e

--- gerrit-server/src/main/java/com/google/gerrit/server/WebLinks.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/WebLinks.java
@@ -128,18 +128,6 @@ end_import
 
 begin_import
 import|import
-name|com
-operator|.
-name|google
-operator|.
-name|inject
-operator|.
-name|Inject
-import|;
-end_import
-
-begin_import
-import|import
 name|java
 operator|.
 name|util
