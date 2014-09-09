a242fcfa7c35ec006c2ce4470b2882411a196af9
Dariusz Luksza <dariusz@luksza.org>
2014-09-10 00:07:22 +0200

    Add SwitchSecureStore site program
    Add site program that will switch SecureStore implementations.
    
    Running:
    
    java -jar gerrit.war SwitchSecureStore --new-secure-store-lib $path -d
    $site
    
    will read all stored values from old SecureStore, then store them using
    new implementation, remove jar with old implementation from lib/
    directory, copy new jar to lib/ and finally set new value of
    gerrit.secureStoreClass configuration option.
    
    Change-Id: I79391e7cda5901a48b83ee35ac2776e0b977944b
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>
    
    Former-commit-id: 8f33a5170679c234c3ee8f5aad6b3b1d71190972

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/BaseInit.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/BaseInit.java
@@ -1409,7 +1409,9 @@ name|String
 operator|.
 name|format
 argument_list|(
-literal|"Different secure store was previously configured: %s."
+literal|"Different secure store was previously configured: %s. "
+operator|+
+literal|"Use SwitchSecureStore program to switch between implementations."
 argument_list|,
 name|currentSecureStoreClassName
 argument_list|)
