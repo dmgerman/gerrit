6976aee6e84c6f1348d97304e251797a168c3800
David Pletcher <dpletcher@google.com>
2014-10-28 21:57:32 -0700

    Refactor RequestUtil into a separate package
    Move RequestUtil into its own package. This facilitates
    reuse of request attribute accessor methods by external
    libraries.
    
    Change-Id: Id002eb4f72827d65087a999adbd2ba9a58795fe2
    
    Former-commit-id: 015ce0a2f254287ac839f16095607b582c1a224c

--- gerrit-util-http/src/test/java/com/google/gerrit/util/http/RequestUtilTest.java
+++ gerrit-util-http/src/test/java/com/google/gerrit/util/http/RequestUtilTest.java
@@ -52,7 +52,7 @@ comment|// limitations under the License.
 end_comment
 
 begin_package
-DECL|package|com.google.gerrit.httpd
+DECL|package|com.google.gerrit.util.http
 package|package
 name|com
 operator|.
@@ -60,7 +60,9 @@ name|google
 operator|.
 name|gerrit
 operator|.
-name|httpd
+name|util
+operator|.
+name|http
 package|;
 end_package
 
