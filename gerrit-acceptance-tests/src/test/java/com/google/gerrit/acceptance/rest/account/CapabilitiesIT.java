9b48c4b8ccd5049f2669d9ad52519f13ad387d55
David Pursehouse <david.pursehouse@sonymobile.com>
2014-12-25 13:30:55 +0900

    Organize imports
    Change-Id: I6aa7d48572b60af7c4b79eaa9017ec113cc5c77c
    
    Former-commit-id: b767b0e606c15d647551fe59955f96fcd52dca1d

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/account/CapabilitiesIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/account/CapabilitiesIT.java
@@ -74,6 +74,38 @@ name|com
 operator|.
 name|google
 operator|.
+name|common
+operator|.
+name|truth
+operator|.
+name|Truth
+operator|.
+name|assertThat
+import|;
+end_import
+
+begin_import
+import|import static
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|truth
+operator|.
+name|Truth
+operator|.
+name|assert_
+import|;
+end_import
+
+begin_import
+import|import static
+name|com
+operator|.
+name|google
+operator|.
 name|gerrit
 operator|.
 name|common
@@ -209,38 +241,6 @@ operator|.
 name|GlobalCapability
 operator|.
 name|RUN_AS
-import|;
-end_import
-
-begin_import
-import|import static
-name|com
-operator|.
-name|google
-operator|.
-name|common
-operator|.
-name|truth
-operator|.
-name|Truth
-operator|.
-name|assertThat
-import|;
-end_import
-
-begin_import
-import|import static
-name|com
-operator|.
-name|google
-operator|.
-name|common
-operator|.
-name|truth
-operator|.
-name|Truth
-operator|.
-name|assert_
 import|;
 end_import
 
