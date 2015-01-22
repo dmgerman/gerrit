dmgerman/gerrit

commit 36934dc6effc4d4c72bd5b5f7bbf70de009689c6

Remove old change screen
The following code was removed:

UI:

  * ChangeScreen (1)
  * SideBySide (1)
  * NewChangeScreenBar

Database columns in the accounts table:

  * change_screen
  * reverse_patch_set_order
  * comment_visibility_strategy

Change-Id: I7ec4a0beb51dd6cb2f7c364df40e7cb5bea786d2

gerrit-gwtui/src/main/java/com/google/gerrit/client/account/MyPreferencesScreen.java
@@ -17,7 +17,6 @@ package com.google.gerrit.client.account;
 import static com.google.gerrit.reviewdb.client.AccountGeneralPreferences.DEFAULT_PAGESIZE;
 import static com.google.gerrit.reviewdb.client.AccountGeneralPreferences.PAGESIZE_CHOICES;
 
-import com.google.gerrit.client.Dispatcher;
 import com.google.gerrit.client.Gerrit;
 import com.google.gerrit.client.StringListPanel;
 import com.google.gerrit.client.config.ConfigServerApi;
@@ -27,7 +26,6 @@ import com.google.gerrit.client.rpc.Natives;
 import com.google.gerrit.client.rpc.ScreenLoadCallback;
 import com.google.gerrit.client.ui.OnEditEnabler;
 import com.google.gerrit.reviewdb.client.AccountGeneralPreferences;
-import com.google.gerrit.reviewdb.client.AccountGeneralPreferences.CommentVisibilityStrategy;
 import com.google.gerrit.reviewdb.client.AccountGeneralPreferences.ReviewCategoryStrategy;
 import com.google.gwt.core.client.JsArray;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -49,7 +47,6 @@ public class MyPreferencesScreen extends SettingsScreen {
   private CheckBox showSiteHeader;
   private CheckBox useFlashClipboard;
   private CheckBox copySelfOnEmails;
-  private CheckBox reversePatchSetOrder;
   private CheckBox relativeDateInChangeTable;
   private CheckBox sizeBarInChangeTable;
   private CheckBox legacycidInChangeTable;
@@ -57,8 +54,6 @@ public class MyPreferencesScreen extends SettingsScreen {
   private ListBox dateFormat;
   private ListBox timeFormat;
   private ListBox reviewCategoryStrategy;
-  private ListBox commentVisibilityStrategy;
-  private ListBox changeScreen;
   private ListBox diffView;
   private StringListPanel myMenus;
   private Button save;
@@ -70,7 +65,6 @@ public class MyPreferencesScreen extends SettingsScreen {
     showSiteHeader = new CheckBox(Util.C.showSiteHeader());
     useFlashClipboard = new CheckBox(Util.C.useFlashClipboard());
     copySelfOnEmails = new CheckBox(Util.C.copySelfOnEmails());
-    reversePatchSetOrder = new CheckBox(Util.C.reversePatchSetOrder());
     maximumPageSize = new ListBox();
     for (final short v : PAGESIZE_CHOICES) {
       maximumPageSize.addItem(Util.M.rowsPerPage(v), String.valueOf(v));
@@ -92,32 +86,6 @@ public class MyPreferencesScreen extends SettingsScreen {
     reviewCategoryStrategy.addItem(
         Util.C.messageShowInReviewCategoryAbbrev(),
         AccountGeneralPreferences.ReviewCategoryStrategy.ABBREV.name());
-
-    commentVisibilityStrategy = new ListBox();
-    commentVisibilityStrategy.addItem(
-        com.google.gerrit.client.changes.Util.C.messageCollapseAll(),
-        AccountGeneralPreferences.CommentVisibilityStrategy.COLLAPSE_ALL.name());
-    commentVisibilityStrategy.addItem(
-        com.google.gerrit.client.changes.Util.C.messageExpandMostRecent(),
-        AccountGeneralPreferences.CommentVisibilityStrategy.EXPAND_MOST_RECENT.name());
-    commentVisibilityStrategy.addItem(
-        com.google.gerrit.client.changes.Util.C.messageExpandRecent(),
-        AccountGeneralPreferences.CommentVisibilityStrategy.EXPAND_RECENT.name());
-    commentVisibilityStrategy.addItem(
-        com.google.gerrit.client.changes.Util.C.messageExpandAll(),
-        AccountGeneralPreferences.CommentVisibilityStrategy.EXPAND_ALL.name());
-
-    changeScreen = new ListBox();
-    changeScreen.addItem(
-        Util.M.changeScreenServerDefault(
-            getLabel(Gerrit.getConfig().getChangeScreen())),
-        "");
-    changeScreen.addItem(
-        Util.C.changeScreenOldUi(),
-        AccountGeneralPreferences.ChangeScreen.OLD_UI.name());
-    changeScreen.addItem(
-        Util.C.changeScreenNewUi(),
-        AccountGeneralPreferences.ChangeScreen.CHANGE_SCREEN2.name());
 
     diffView = new ListBox();
     diffView.addItem(
@@ -165,7 +133,7 @@ public class MyPreferencesScreen extends SettingsScreen {
     sizeBarInChangeTable = new CheckBox(Util.C.showSizeBarInChangeTable());
     legacycidInChangeTable = new CheckBox(Util.C.showLegacycidInChangeTable());
 
-    final Grid formGrid = new Grid(13, 2);
+    final Grid formGrid = new Grid(11, 2);
 
     int row = 0;
     formGrid.setText(row, labelIdx, "");
@@ -181,11 +149,6 @@ public class MyPreferencesScreen extends SettingsScreen {
     row++;
 
     formGrid.setText(row, labelIdx, "");
-    formGrid.setWidget(row, fieldIdx, reversePatchSetOrder);
-    row++;
-
-    formGrid.setText(row, labelIdx, Util.C.reviewCategoryLabel());
-    formGrid.setWidget(row, fieldIdx, reviewCategoryStrategy);
     row++;
 
     formGrid.setText(row, labelIdx, Util.C.maximumPageSizeFieldLabel());
@@ -208,12 +171,8 @@ public class MyPreferencesScreen extends SettingsScreen {
     formGrid.setWidget(row, fieldIdx, legacycidInChangeTable);
     row++;
 
-    formGrid.setText(row, labelIdx, Util.C.commentVisibilityLabel());
-    formGrid.setWidget(row, fieldIdx, commentVisibilityStrategy);
-    row++;
-
-    formGrid.setText(row, labelIdx, Util.C.changeScreenLabel());
-    formGrid.setWidget(row, fieldIdx, changeScreen);
+    formGrid.setText(row, labelIdx, "");
+    formGrid.setWidget(row, fieldIdx, legacycidInChangeTable);
     row++;
 
     formGrid.setText(row, labelIdx, Util.C.diffViewLabel());
@@ -239,16 +198,12 @@ public class MyPreferencesScreen extends SettingsScreen {
     e.listenTo(showSiteHeader);
     e.listenTo(useFlashClipboard);
     e.listenTo(copySelfOnEmails);
-    e.listenTo(reversePatchSetOrder);
     e.listenTo(maximumPageSize);
     e.listenTo(dateFormat);
     e.listenTo(timeFormat);
     e.listenTo(relativeDateInChangeTable);
     e.listenTo(sizeBarInChangeTable);
     e.listenTo(legacycidInChangeTable);
-    e.listenTo(reviewCategoryStrategy);
-    e.listenTo(commentVisibilityStrategy);
-    e.listenTo(changeScreen);
     e.listenTo(diffView);
   }
 
@@ -268,7 +223,6 @@ public class MyPreferencesScreen extends SettingsScreen {
     showSiteHeader.setEnabled(on);
     useFlashClipboard.setEnabled(on);
     copySelfOnEmails.setEnabled(on);
-    reversePatchSetOrder.setEnabled(on);
     maximumPageSize.setEnabled(on);
     dateFormat.setEnabled(on);
     timeFormat.setEnabled(on);
@@ -276,8 +230,6 @@ public class MyPreferencesScreen extends SettingsScreen {
     sizeBarInChangeTable.setEnabled(on);
     legacycidInChangeTable.setEnabled(on);
     reviewCategoryStrategy.setEnabled(on);
-    commentVisibilityStrategy.setEnabled(on);
-    changeScreen.setEnabled(on);
     diffView.setEnabled(on);
   }
 
@@ -285,7 +237,6 @@ public class MyPreferencesScreen extends SettingsScreen {
     showSiteHeader.setValue(p.showSiteHeader());
     useFlashClipboard.setValue(p.useFlashClipboard());
     copySelfOnEmails.setValue(p.copySelfOnEmail());
-    reversePatchSetOrder.setValue(p.reversePatchSetOrder());
     setListBox(maximumPageSize, DEFAULT_PAGESIZE, p.changesPerPage());
     setListBox(dateFormat, AccountGeneralPreferences.DateFormat.STD, //
         p.dateFormat());
@@ -297,12 +248,6 @@ public class MyPreferencesScreen extends SettingsScreen {
     setListBox(reviewCategoryStrategy,
         AccountGeneralPreferences.ReviewCategoryStrategy.NONE,
         p.reviewCategoryStrategy());
-    setListBox(commentVisibilityStrategy,
-        AccountGeneralPreferences.CommentVisibilityStrategy.EXPAND_RECENT,
-        p.commentVisibilityStrategy());
-    setListBox(changeScreen,
-        null,
-        p.changeScreen());
     setListBox(diffView,
         AccountGeneralPreferences.DiffView.SIDE_BY_SIDE,
         p.diffView());
@@ -373,7 +318,6 @@ public class MyPreferencesScreen extends SettingsScreen {
     p.setShowSiteHeader(showSiteHeader.getValue());
     p.setUseFlashClipboard(useFlashClipboard.getValue());
     p.setCopySelfOnEmails(copySelfOnEmails.getValue());
-    p.setReversePatchSetOrder(reversePatchSetOrder.getValue());
     p.setMaximumPageSize(getListBox(maximumPageSize, DEFAULT_PAGESIZE));
     p.setDateFormat(getListBox(dateFormat,
         AccountGeneralPreferences.DateFormat.STD,
@@ -387,15 +331,9 @@ public class MyPreferencesScreen extends SettingsScreen {
     p.setReviewCategoryStrategy(getListBox(reviewCategoryStrategy,
         ReviewCategoryStrategy.NONE,
         ReviewCategoryStrategy.values()));
-    p.setCommentVisibilityStrategy(getListBox(commentVisibilityStrategy,
-        CommentVisibilityStrategy.EXPAND_RECENT,
-        CommentVisibilityStrategy.values()));
     p.setDiffView(getListBox(diffView,
         AccountGeneralPreferences.DiffView.SIDE_BY_SIDE,
         AccountGeneralPreferences.DiffView.values()));
-    p.setChangeScreen(getListBox(changeScreen,
-        null,
-        AccountGeneralPreferences.ChangeScreen.values()));
 
     enable(false);
     save.setEnabled(false);
@@ -411,7 +349,6 @@ public class MyPreferencesScreen extends SettingsScreen {
           public void onSuccess(Preferences prefs) {
             Gerrit.getUserAccount().setGeneralPreferences(p);
             Gerrit.applyUserPreferences();
-            Dispatcher.changeScreen2 = false;
             enable(true);
             display(prefs);
             Gerrit.refreshMenuBar();
@@ -424,20 +361,6 @@ public class MyPreferencesScreen extends SettingsScreen {
             super.onFailure(caught);
           }
         });
-  }
-
-  private static String getLabel(AccountGeneralPreferences.ChangeScreen ui) {
-    if (ui == null) {
-      return "";
-    }
-    switch (ui) {
-      case OLD_UI:
-        return Util.C.changeScreenOldUi();
-      case CHANGE_SCREEN2:
-        return Util.C.changeScreenNewUi();
-      default:
-        return ui.name();
-    }
   }
 
   private class MyMenuPanel extends StringListPanel {

gerrit-common/src/main/java/com/google/gerrit/common/data/ApprovalDetail.java
@@ -1,127 +1,0 @@
-// Copyright (C) 2008 The Android Open Source Project
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package com.google.gerrit.common.data;
-
-import com.google.gerrit.reviewdb.client.Account;
-import com.google.gerrit.reviewdb.client.PatchSetApproval;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-public class ApprovalDetail {
-  public static List<ApprovalDetail> sort(Collection<ApprovalDetail> ads,
-      final int owner) {
-    List<ApprovalDetail> sorted = new ArrayList<>(ads);
-    Collections.sort(sorted, new Comparator<ApprovalDetail>() {
-      @Override
-      public int compare(ApprovalDetail o1, ApprovalDetail o2) {
-        int byOwner = (o2.account.get() == owner ? 1 : 0)
-            - (o1.account.get() == owner ? 1 : 0);
-        return byOwner != 0 ? byOwner : (o1.hasNonZero - o2.hasNonZero);
-      }
-    });
-    return sorted;
-  }
-
-  protected Account.Id account;
-  protected List<PatchSetApproval> approvals;
-  protected boolean canRemove;
-  private Set<String> votable;
-
-  private transient Set<String> approved;
-  private transient Set<String> rejected;
-  private transient Map<String, Integer> values;
-  private transient int hasNonZero;
-
-  protected ApprovalDetail() {
-  }
-
-  public ApprovalDetail(final Account.Id id) {
-    account = id;
-    approvals = new ArrayList<>();
-  }
-
-  public Account.Id getAccount() {
-    return account;
-  }
-
-  public boolean canRemove() {
-    return canRemove;
-  }
-
-  public void setCanRemove(boolean removeable) {
-    canRemove = removeable;
-  }
-
-  public void approved(String label) {
-    if (approved == null) {
-      approved = new HashSet<>();
-    }
-    approved.add(label);
-    hasNonZero = 1;
-  }
-
-  public void rejected(String label) {
-    if (rejected == null) {
-      rejected = new HashSet<>();
-    }
-    rejected.add(label);
-    hasNonZero = 1;
-  }
-
-  public void votable(String label) {
-    if (votable == null) {
-      votable = new HashSet<>();
-    }
-    votable.add(label);
-  }
-
-  public void value(String label, int value) {
-    if (values == null) {
-      values = new HashMap<>();
-    }
-    values.put(label, value);
-    if (value != 0) {
-      hasNonZero = 1;
-    }
-  }
-
-  public boolean isApproved(String label) {
-    return approved != null && approved.contains(label);
-  }
-
-  public boolean isRejected(String label) {
-    return rejected != null && rejected.contains(label);
-  }
-
-  public boolean canVote(String label) {
-    return votable != null && votable.contains(label);
-  }
-
-  public int getValue(String label) {
-    if (values == null) {
-      return 0;
-    }
-    Integer v = values.get(label);
-    return v != null ? v : 0;
-  }
-}

gerrit-common/src/main/java/com/google/gerrit/common/data/ChangeDetail.java
@@ -1,286 +1,0 @@
-// Copyright (C) 2008 The Android Open Source Project
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package com.google.gerrit.common.data;
-
-import com.google.gerrit.extensions.common.SubmitType;
-import com.google.gerrit.reviewdb.client.Change;
-import com.google.gerrit.reviewdb.client.ChangeMessage;
-import com.google.gerrit.reviewdb.client.PatchSet;
-
-import java.util.List;
-import java.util.Set;
-
-/** Detail necessary to display a change. */
-public class ChangeDetail {
-  protected AccountInfoCache accounts;
-  protected boolean allowsAnonymous;
-  protected boolean canAbandon;
-  protected boolean canEditCommitMessage;
-  protected boolean canCherryPick;
-  protected boolean canEditHashtags;
-  protected boolean canPublish;
-  protected boolean canRebase;
-  protected boolean canRestore;
-  protected boolean canRevert;
-  protected boolean canDeleteDraft;
-  protected Change change;
-  protected boolean starred;
-  protected List<ChangeInfo> dependsOn;
-  protected List<ChangeInfo> neededBy;
-  protected List<PatchSet> patchSets;
-  protected Set<PatchSet.Id> patchSetsWithDraftComments;
-  protected List<SubmitRecord> submitRecords;
-  protected SubmitType submitType;
-  protected SubmitTypeRecord submitTypeRecord;
-  protected boolean canSubmit;
-  protected boolean mergeable;
-  protected List<ChangeMessage> messages;
-  protected PatchSet.Id currentPatchSetId;
-  protected PatchSetDetail currentDetail;
-  protected boolean canEdit;
-  protected boolean canEditTopicName;
-
-  public ChangeDetail() {
-  }
-
-  public AccountInfoCache getAccounts() {
-    return accounts;
-  }
-
-  public void setAccounts(AccountInfoCache aic) {
-    accounts = aic;
-  }
-
-  public boolean isAllowsAnonymous() {
-    return allowsAnonymous;
-  }
-
-  public void setAllowsAnonymous(final boolean anon) {
-    allowsAnonymous = anon;
-  }
-
-  public boolean canAbandon() {
-    return canAbandon;
-  }
-
-  public void setCanAbandon(final boolean a) {
-    canAbandon = a;
-  }
-
-  public boolean canEditCommitMessage() {
-    return canEditCommitMessage;
-  }
-
-  public void setCanEditCommitMessage(final boolean a) {
-    canEditCommitMessage = a;
-  }
-
-  public boolean canCherryPick() {
-    return canCherryPick;
-  }
-
-  public void setCanCherryPick(final boolean a) {
-    canCherryPick = a;
-  }
-
-  public boolean getCanEditHashtags() {
-    return canEditHashtags;
-  }
-
-  public void setCanEditHashtags(final boolean a) {
-    canEditHashtags = a;
-  }
-
-  public boolean canPublish() {
-    return canPublish;
-  }
-
-  public void setCanPublish(final boolean a) {
-    canPublish = a;
-  }
-
-  public boolean canRebase() {
-    return canRebase;
-  }
-
-  public void setCanRebase(final boolean a) {
-    canRebase = a;
-  }
-
-  public boolean canRestore() {
-    return canRestore;
-  }
-
-  public void setCanRestore(final boolean a) {
-    canRestore = a;
-  }
-
-  public boolean canRevert() {
-    return canRevert;
-  }
-
-  public void setCanRevert(boolean a) {
-      canRevert = a;
-  }
-
-  public boolean canSubmit() {
-    return canSubmit;
-  }
-
-  public void setCanSubmit(boolean a) {
-    canSubmit = a;
-  }
-
-  public boolean canDeleteDraft() {
-    return canDeleteDraft;
-  }
-
-  public void setCanDeleteDraft(boolean a) {
-    canDeleteDraft = a;
-  }
-
-  public boolean canEditTopicName() {
-    return canEditTopicName;
-  }
-
-  public void setCanEditTopicName(boolean a) {
-    canEditTopicName = a;
-  }
-
-  public Change getChange() {
-    return change;
-  }
-
-  public void setChange(final Change change) {
-    this.change = change;
-    this.currentPatchSetId = change.currentPatchSetId();
-  }
-
-  public boolean isStarred() {
-    return starred;
-  }
-
-  public void setStarred(final boolean s) {
-    starred = s;
-  }
-
-  public List<ChangeInfo> getDependsOn() {
-    return dependsOn;
-  }
-
-  public void setDependsOn(List<ChangeInfo> d) {
-    dependsOn = d;
-  }
-
-  public List<ChangeInfo> getNeededBy() {
-    return neededBy;
-  }
-
-  public void setNeededBy(List<ChangeInfo> d) {
-    neededBy = d;
-  }
-
-  public List<ChangeMessage> getMessages() {
-    return messages;
-  }
-
-  public void setMessages(List<ChangeMessage> m) {
-    messages = m;
-  }
-
-  public List<PatchSet> getPatchSets() {
-    return patchSets;
-  }
-
-  public void setPatchSets(List<PatchSet> s) {
-    patchSets = s;
-  }
-
-  public void setPatchSetsWithDraftComments(Set<PatchSet.Id> pwdc) {
-    this.patchSetsWithDraftComments = pwdc;
-  }
-
-  public boolean hasDraftComments(PatchSet.Id id) {
-    return patchSetsWithDraftComments.contains(id);
-  }
-
-  public void setSubmitRecords(List<SubmitRecord> all) {
-    submitRecords = all;
-  }
-
-  public List<SubmitRecord> getSubmitRecords() {
-    return submitRecords;
-  }
-
-  public void setSubmitTypeRecord(SubmitTypeRecord submitTypeRecord) {
-    this.submitTypeRecord = submitTypeRecord;
-  }
-
-  public SubmitTypeRecord getSubmitTypeRecord() {
-    return submitTypeRecord;
-  }
-
-  public boolean isCurrentPatchSet(final PatchSetDetail detail) {
-    return currentPatchSetId != null
-        && detail.getPatchSet().getId().equals(currentPatchSetId);
-  }
-
-  public PatchSet getCurrentPatchSet() {
-    if (currentPatchSetId != null) {
-      // We search through the list backwards because its *very* likely
-      // that the current patch set is also the last patch set.
-      //
-      for (int i = patchSets.size() - 1; i >= 0; i--) {
-        final PatchSet ps = patchSets.get(i);
-        if (ps.getId().equals(currentPatchSetId)) {
-          return ps;
-        }
-      }
-    }
-    return null;
-  }
-
-  public PatchSetDetail getCurrentPatchSetDetail() {
-    return currentDetail;
-  }
-
-  public void setCurrentPatchSetDetail(PatchSetDetail d) {
-    currentDetail = d;
-  }
-
-  public void setCurrentPatchSetId(final PatchSet.Id id) {
-    currentPatchSetId = id;
-  }
-
-  public String getDescription() {
-    return currentDetail != null ? currentDetail.getInfo().getMessage() : "";
-  }
-
-  public void setCanEdit(boolean a) {
-    canEdit = a;
-  }
-
-  public boolean canEdit() {
-    return canEdit;
-  }
-
-  public void setMergeable(boolean m) {
-    mergeable = m;
-  }
-
-  public boolean isMergeable() {
-    return mergeable;
-  }
-}

gerrit-common/src/main/java/com/google/gerrit/common/data/GerritConfig.java
@@ -16,7 +16,6 @@ package com.google.gerrit.common.data;
 
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Account.FieldName;
-import com.google.gerrit.reviewdb.client.AccountGeneralPreferences;
 import com.google.gerrit.reviewdb.client.AccountGeneralPreferences.DownloadCommand;
 import com.google.gerrit.reviewdb.client.AccountGeneralPreferences.DownloadScheme;
 import com.google.gerrit.reviewdb.client.AuthType;
@@ -53,7 +52,6 @@ public class GerritConfig implements Cloneable {
   protected String anonymousCowardName;
   protected int suggestFrom;
   protected int changeUpdateDelay;
-  protected AccountGeneralPreferences.ChangeScreen changeScreen;
   protected List<String> archiveFormats;
   protected int largeChangeSize;
   protected String replyLabel;
@@ -276,14 +274,6 @@ public class GerritConfig implements Cloneable {
 
   public void setChangeUpdateDelay(int seconds) {
     changeUpdateDelay = seconds;
-  }
-
-  public AccountGeneralPreferences.ChangeScreen getChangeScreen() {
-    return changeScreen;
-  }
-
-  public void setChangeScreen(AccountGeneralPreferences.ChangeScreen ui) {
-    this.changeScreen = ui;
   }
 
   public int getLargeChangeSize() {

gerrit-common/src/main/java/com/google/gerrit/common/data/ReviewerResult.java
@@ -1,117 +1,0 @@
-// Copyright (C) 2009 The Android Open Source Project
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package com.google.gerrit.common.data;
-
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Result from adding or removing a reviewer from a change.
- */
-public class ReviewerResult {
-  protected List<Error> errors;
-  protected ChangeDetail change;
-  protected int memberCount;
-  protected boolean askForConfirmation;
-
-  public ReviewerResult() {
-    errors = new ArrayList<>();
-  }
-
-  public void addError(final Error e) {
-    errors.add(e);
-  }
-
-  public List<Error> getErrors() {
-    return errors;
-  }
-
-  public ChangeDetail getChange() {
-    return change;
-  }
-
-  public void setChange(final ChangeDetail d) {
-    change = d;
-  }
-
-  public int getMemberCount() {
-    return memberCount;
-  }
-
-  public void setMemberCount(final int memberCount) {
-    this.memberCount = memberCount;
-  }
-
-  public boolean askForConfirmation() {
-    return askForConfirmation;
-  }
-
-  public void setAskForConfirmation(final boolean askForConfirmation) {
-    this.askForConfirmation = askForConfirmation;
-  }
-
-  public static class Error {
-    public static enum Type {
-      /** Name supplied does not match to a registered account or account group. */
-      REVIEWER_NOT_FOUND,
-
-      /** The account is inactive. */
-      ACCOUNT_INACTIVE,
-
-      /** The account is not permitted to see the change. */
-      CHANGE_NOT_VISIBLE,
-
-      /** The groups has no members. */
-      GROUP_EMPTY,
-
-      /** The groups has too many members. */
-      GROUP_HAS_TOO_MANY_MEMBERS,
-
-      /** The group is not allowed to be added as reviewer. */
-      GROUP_NOT_ALLOWED,
-
-      /** Could not remove this reviewer from the change due to ORMException. */
-      COULD_NOT_REMOVE,
-
-      /** Not permitted to remove this reviewer from the change. */
-      REMOVE_NOT_PERMITTED
-    }
-
-    protected Type type;
-    protected String name;
-
-    protected Error() {
-    }
-
-    public Error(final Type type, final String who) {
-      this.type = type;
-      this.name = who;
-    }
-
-    public Type getType() {
-      return type;
-    }
-
-    public String getName() {
-      return name;
-    }
-
-    @Override
-    public String toString() {
-      return type + " " + name;
-    }
-  }
-}

commit 2c697a5686dd57b814a89ac7c5729d40be57da8f

Fix display of control for 'Display In Review Category'
Commit 36934dc6 accidentally removed the code to display the 'Display
In Review Category' control on the MyPreferencesScreen.

Change-Id: If794b06b90f0a82cb16ef9f49765485a8e552983
Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

Former-commit-id: 4934d40d5af4c8e566c48abf17f219ea0ef89850

gerrit-gwtui/src/main/java/com/google/gerrit/client/account/MyPreferencesScreen.java
@@ -1188,7 +1188,23 @@ name|row
 argument_list|,
 name|labelIdx
 argument_list|,
-literal|""
+name|Util
+operator|.
+name|C
+operator|.
+name|reviewCategoryLabel
+argument_list|()
+argument_list|)
+expr_stmt|;
+name|formGrid
+operator|.
+name|setWidget
+argument_list|(
+name|row
+argument_list|,
+name|fieldIdx
+argument_list|,
+name|reviewCategoryStrategy
 argument_list|)
 expr_stmt|;
 name|row