dmgerman/gerrit

commit 875fc8e8a3367d2507d0f937e13b12d3ff3f6f86

Revert changes to MergeOp to batch object inserts and ref updates
The failure modes for these are problematic, making the cure worse
than the disease. Unlike a missing PatchSet record in the database, a
missing PatchSet ref causes the change screen to fail to load, and
even once abandoned, causes reindexing to fail.

A longer-term rewrite of MergeOp begins with I0771f5e8, which is
designed to have less problematic failure modes, but until then, just
revert.

This mostly reverts the following commits (except for some cleanups
and new helper methods that are not used now but will be eventually):
  c20148c8e20d62744c3654e73fb9d7256f30a205
  5dea1c0fc001e0060e2b1c2ce098d09ff5f6621a

Change-Id: Id691318a6e28f18860b34a9a39f6e834161ff0dd

gerrit-server/src/main/java/com/google/gerrit/server/git/MergeOp.java
@@ -76,19 +76,17 @@ import com.google.inject.assistedinject.Assisted;
 
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
-import org.eclipse.jgit.lib.BatchRefUpdate;
 import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.NullProgressMonitor;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectInserter;
 import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RefUpdate;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevFlag;
 import org.eclipse.jgit.revwalk.RevSort;
 import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.jgit.transport.ReceiveCommand;
 import org.joda.time.format.ISODateTimeFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -172,7 +170,6 @@ public class MergeOp {
   private Repository repo;
   private RevWalk rw;
   private RevFlag canMergeFlag;
-  private ObjectId oldBranchTip;
   private CodeReviewCommit branchTip;
   private MergeTip mergeTip;
   private ObjectInserter inserter;
@@ -260,7 +257,7 @@ public class MergeOp {
       openSchema();
       openRepository();
 
-      BatchRefUpdate branchUpdate = openBranch();
+      RefUpdate branchUpdate = openBranch();
       boolean reopen = false;
 
       ListMultimap<SubmitType, Change> toSubmit =
@@ -279,9 +276,9 @@ public class MergeOp {
             logDebug("Reopening branch");
             branchUpdate = openBranch();
           }
-          SubmitStrategy strategy = createStrategy(submitType, branchUpdate);
+          SubmitStrategy strategy = createStrategy(submitType);
           MergeTip mergeTip = preMerge(strategy, toMerge.get(submitType));
-          BatchRefUpdate update = updateBranch(strategy, branchUpdate);
+          RefUpdate update = updateBranch(strategy, branchUpdate);
           reopen = true;
 
           updateChangeStatus(toSubmit.get(submitType), mergeTip);
@@ -417,11 +414,10 @@ public class MergeOp {
     return mergeTip;
   }
 
-  private SubmitStrategy createStrategy(SubmitType submitType,
-      BatchRefUpdate branchUpdate)
+  private SubmitStrategy createStrategy(SubmitType submitType)
       throws MergeException, NoSuchProjectException {
     return submitStrategyFactory.create(submitType, db, repo, rw, inserter,
-        canMergeFlag, branchUpdate, getAlreadyAccepted(branchTip), destBranch);
+        canMergeFlag, getAlreadyAccepted(branchTip), destBranch);
   }
 
   private void openRepository() throws MergeException, NoSuchProjectException {
@@ -434,23 +430,25 @@ public class MergeOp {
       String m = "Error opening repository \"" + name.get() + '"';
       throw new MergeException(m, err);
     }
-    inserter = repo.newObjectInserter();
-    rw = CodeReviewCommit.newRevWalk(inserter.newReader());
+
+    rw = CodeReviewCommit.newRevWalk(repo);
     rw.sort(RevSort.TOPO);
     rw.sort(RevSort.COMMIT_TIME_DESC, true);
     canMergeFlag = rw.newFlag("CAN_MERGE");
+
+    inserter = repo.newObjectInserter();
   }
 
-  private BatchRefUpdate openBranch()
+  private RefUpdate openBranch()
       throws MergeException, OrmException, NoSuchChangeException {
     try {
-      BatchRefUpdate branchUpdate = repo.getRefDatabase().newBatchUpdate();
-      Ref oldRef = repo.getRef(destBranch.get());
-      oldBranchTip = oldRef != null ? oldRef.getObjectId() : ObjectId.zeroId();
-      if (!ObjectId.zeroId().equals(oldBranchTip)) {
-        branchTip = (CodeReviewCommit) rw.parseCommit(oldBranchTip);
+      RefUpdate branchUpdate = repo.updateRef(destBranch.get());
+      if (branchUpdate.getOldObjectId() != null) {
+        branchTip =
+            (CodeReviewCommit) rw.parseCommit(branchUpdate.getOldObjectId());
       } else if (repo.getFullBranch().equals(destBranch.get())) {
         branchTip = null;
+        branchUpdate.setExpectedOldObjectId(ObjectId.zeroId());
       } else {
         for (ChangeData cd : queryProvider.get().submitted(destBranch)) {
           try {
@@ -659,8 +657,8 @@ public class MergeOp {
     }
   }
 
-  private BatchRefUpdate updateBranch(SubmitStrategy strategy,
-      BatchRefUpdate branchUpdate) throws MergeException {
+  private RefUpdate updateBranch(SubmitStrategy strategy,
+      RefUpdate branchUpdate) throws MergeException {
     CodeReviewCommit currentTip =
         mergeTip != null ? mergeTip.getCurrentTip() : null;
     if (Objects.equals(branchTip, currentTip)) {
@@ -672,7 +670,7 @@ public class MergeOp {
       return null;
     }
 
-    if (RefNames.REFS_CONFIG.equals(destBranch.get())) {
+    if (RefNames.REFS_CONFIG.equals(branchUpdate.getName())) {
       logDebug("Loading new configuration from {}", RefNames.REFS_CONFIG);
       try {
         ProjectConfig cfg =
@@ -684,34 +682,27 @@ public class MergeOp {
             + destProject.getProject().getName(), e);
       }
     }
-    try {
-      inserter.flush();
-    } catch (IOException e) {
-      throw new MergeException("Cannot flush merge results", e);
-    }
 
     branchUpdate.setRefLogIdent(refLogIdent);
-    branchUpdate.setAllowNonFastForwards(false);
-    // TODO(dborowitz): This message is also used by all new patch set ref
-    // updates; find a better wording that works for that case too.
+    branchUpdate.setForceUpdate(false);
+    branchUpdate.setNewObjectId(currentTip);
     branchUpdate.setRefLogMessage("merged", true);
-    ReceiveCommand cmd =
-        new ReceiveCommand(oldBranchTip, currentTip, destBranch.get());
-    branchUpdate.addCommand(cmd);
-
     try {
-      branchUpdate.execute(rw, NullProgressMonitor.INSTANCE);
-      logDebug("Executed batch update: {}", branchUpdate);
-      switch (cmd.getResult()) {
-        case OK:
-          if (cmd.getType() == ReceiveCommand.Type.UPDATE) {
+      RefUpdate.Result result = branchUpdate.update(rw);
+      logDebug("Update of {}: {}..{} returned status {}",
+          branchUpdate.getName(), branchUpdate.getOldObjectId(),
+          branchUpdate.getNewObjectId(), result);
+      switch (result) {
+        case NEW:
+        case FAST_FORWARD:
+          if (branchUpdate.getResult() == RefUpdate.Result.FAST_FORWARD) {
             tagCache.updateFastForward(destBranch.getParentKey(),
-                destBranch.get(),
-                oldBranchTip,
+                branchUpdate.getName(),
+                branchUpdate.getOldObjectId(),
                 currentTip);
           }
 
-          if (RefNames.REFS_CONFIG.equals(destBranch.get())) {
+          if (RefNames.REFS_CONFIG.equals(branchUpdate.getName())) {
             Project p = destProject.getProject();
             projectCache.evict(p);
             destProject = projectCache.get(p.getNameKey());
@@ -730,27 +721,23 @@ public class MergeOp {
           } else {
             msg = "will not retry";
           }
-          throw new IOException(cmd.getResult().name() + ", " + msg
+          // TODO(dborowitz): Implement RefUpdate.toString().
+          throw new IOException(branchUpdate.getResult().name() + ", " + msg
               + '\n' + branchUpdate);
         default:
-          throw new IOException(cmd.getResult().name() + '\n' + branchUpdate);
+          throw new IOException(branchUpdate.getResult().name()
+              + '\n' + branchUpdate);
       }
     } catch (IOException e) {
-      throw new MergeException("Cannot update " + destBranch.get(), e);
+      throw new MergeException("Cannot update " + branchUpdate.getName(), e);
     }
   }
 
-  private void fireRefUpdated(BatchRefUpdate branchUpdate) {
-    logDebug("Firing ref updated hooks for {}", destBranch.get());
+  private void fireRefUpdated(RefUpdate branchUpdate) {
+    logDebug("Firing ref updated hooks for {}", branchUpdate.getName());
     gitRefUpdated.fire(destBranch.getParentKey(), branchUpdate);
-    Account account = getAccount(mergeTip.getCurrentTip());
-    for (ReceiveCommand cmd : branchUpdate.getCommands()) {
-      if (cmd.getRefName().equals(destBranch.get())) {
-        hooks.doRefUpdatedHook(
-            destBranch, cmd.getOldId(), cmd.getNewId(), account);
-        break;
-      }
-    }
+    hooks.doRefUpdatedHook(destBranch, branchUpdate,
+        getAccount(mergeTip.getCurrentTip()));
   }
 
   private Account getAccount(CodeReviewCommit codeReviewCommit) {

gerrit-server/src/main/java/com/google/gerrit/server/change/CherryPickChange.java
@@ -113,89 +113,87 @@ public class CherryPickChange {
 
     Project.NameKey project = change.getProject();
     IdentifiedUser identifiedUser = (IdentifiedUser) currentUser.get();
-    Repository git = null;
-    ObjectInserter oi = null;
-    RevWalk revWalk = null;
-
+    final Repository git;
     try {
       git = gitManager.openRepository(project);
-      oi = git.newObjectInserter();
-      revWalk = new RevWalk(oi.newReader());
-
-      Ref destRef = git.getRef(destinationBranch);
-      if (destRef == null) {
-        throw new InvalidChangeOperationException("Branch "
-            + destinationBranch + " does not exist.");
-      }
-
-      final RevCommit mergeTip = revWalk.parseCommit(destRef.getObjectId());
-
-      RevCommit commitToCherryPick =
-          revWalk.parseCommit(ObjectId.fromString(patch.getRevision().get()));
-
-      PersonIdent committerIdent =
-          identifiedUser.newCommitterIdent(TimeUtil.nowTs(),
-              serverTimeZone);
-
-      final ObjectId computedChangeId =
-          ChangeIdUtil
-              .computeChangeId(commitToCherryPick.getTree(), mergeTip,
-                  commitToCherryPick.getAuthorIdent(), committerIdent, message);
-      String commitMessage =
-          ChangeIdUtil.insertId(message, computedChangeId).trim() + '\n';
-
-      RevCommit cherryPickCommit;
-      ProjectState projectState = refControl.getProjectControl().getProjectState();
-      cherryPickCommit =
-          mergeUtilFactory.create(projectState).createCherryPickFromCommit(git, oi, mergeTip,
-              commitToCherryPick, committerIdent, commitMessage, revWalk);
-      oi.flush();
-
-      Change.Key changeKey;
-      final List<String> idList = cherryPickCommit.getFooterLines(
-          FooterConstants.CHANGE_ID);
-      if (!idList.isEmpty()) {
-        final String idStr = idList.get(idList.size() - 1).trim();
-        changeKey = new Change.Key(idStr);
-      } else {
-        changeKey = new Change.Key("I" + computedChangeId.name());
-      }
-
-      Branch.NameKey newDest =
-          new Branch.NameKey(change.getProject(), destRef.getName());
-      List<ChangeData> destChanges = queryProvider.get()
-          .setLimit(2)
-          .byBranchKey(newDest, changeKey);
-      if (destChanges.size() > 1) {
-        throw new InvalidChangeOperationException("Several changes with key "
-            + changeKey + " reside on the same branch. "
-            + "Cannot create a new patch set.");
-      } else if (destChanges.size() == 1) {
-        // The change key exists on the destination branch. The cherry pick
-        // will be added as a new patch set.
-        return insertPatchSet(git, revWalk, destChanges.get(0).change(),
-            cherryPickCommit, refControl, identifiedUser);
-      } else {
-        // Change key not found on destination branch. We can create a new
-        // change.
-        return createNewChange(git, revWalk, changeKey, project,
-            patch.getId(), destRef, cherryPickCommit, refControl,
-            identifiedUser, change.getTopic());
-      }
-    } catch (MergeIdenticalTreeException | MergeConflictException e) {
-      throw new MergeException("Cherry pick failed: " + e.getMessage());
     } catch (RepositoryNotFoundException e) {
       throw new NoSuchChangeException(change.getId(), e);
-    } finally {
-      if (revWalk != null) {
+    }
+
+    try {
+      RevWalk revWalk = new RevWalk(git);
+      try {
+        Ref destRef = git.getRef(destinationBranch);
+        if (destRef == null) {
+          throw new InvalidChangeOperationException("Branch "
+              + destinationBranch + " does not exist.");
+        }
+
+        final RevCommit mergeTip = revWalk.parseCommit(destRef.getObjectId());
+
+        RevCommit commitToCherryPick =
+            revWalk.parseCommit(ObjectId.fromString(patch.getRevision().get()));
+
+        PersonIdent committerIdent =
+            identifiedUser.newCommitterIdent(TimeUtil.nowTs(),
+                serverTimeZone);
+
+        final ObjectId computedChangeId =
+            ChangeIdUtil
+                .computeChangeId(commitToCherryPick.getTree(), mergeTip,
+                    commitToCherryPick.getAuthorIdent(), committerIdent, message);
+        String commitMessage =
+            ChangeIdUtil.insertId(message, computedChangeId).trim() + '\n';
+
+        RevCommit cherryPickCommit;
+        ObjectInserter oi = git.newObjectInserter();
+        try {
+          ProjectState projectState = refControl.getProjectControl().getProjectState();
+          cherryPickCommit =
+              mergeUtilFactory.create(projectState).createCherryPickFromCommit(git, oi, mergeTip,
+                  commitToCherryPick, committerIdent, commitMessage, revWalk);
+        } catch (MergeIdenticalTreeException | MergeConflictException e) {
+          throw new MergeException("Cherry pick failed: " + e.getMessage());
+        } finally {
+          oi.release();
+        }
+
+        Change.Key changeKey;
+        final List<String> idList = cherryPickCommit.getFooterLines(
+            FooterConstants.CHANGE_ID);
+        if (!idList.isEmpty()) {
+          final String idStr = idList.get(idList.size() - 1).trim();
+          changeKey = new Change.Key(idStr);
+        } else {
+          changeKey = new Change.Key("I" + computedChangeId.name());
+        }
+
+        Branch.NameKey newDest =
+            new Branch.NameKey(change.getProject(), destRef.getName());
+        List<ChangeData> destChanges = queryProvider.get()
+            .setLimit(2)
+            .byBranchKey(newDest, changeKey);
+        if (destChanges.size() > 1) {
+          throw new InvalidChangeOperationException("Several changes with key "
+              + changeKey + " reside on the same branch. "
+              + "Cannot create a new patch set.");
+        } else if (destChanges.size() == 1) {
+          // The change key exists on the destination branch. The cherry pick
+          // will be added as a new patch set.
+          return insertPatchSet(git, revWalk, destChanges.get(0).change(),
+              cherryPickCommit, refControl, identifiedUser);
+        } else {
+          // Change key not found on destination branch. We can create a new
+          // change.
+          return createNewChange(git, revWalk, changeKey, project,
+              patch.getId(), destRef, cherryPickCommit, refControl,
+              identifiedUser, change.getTopic());
+        }
+      } finally {
         revWalk.release();
       }
-      if (oi != null) {
-        oi.release();
-      }
-      if (git != null) {
-        git.close();
-      }
+    } finally {
+      git.close();
     }
   }
 

gerrit-server/src/main/java/com/google/gerrit/server/change/MergeabilityCacheImpl.java
@@ -241,7 +241,6 @@ public class MergeabilityCacheImpl implements MergeabilityCache {
               rw,
               null /*inserter*/,
               canMerge,
-              null /*batchRefUpdate*/,
               accepted,
               key.load.dest).dryRun(tip, rev);
         } finally {

gerrit-server/src/main/java/com/google/gerrit/server/change/PatchSetInserter.java
@@ -52,7 +52,6 @@ import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 
-import org.eclipse.jgit.lib.BatchRefUpdate;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.RefUpdate;
 import org.eclipse.jgit.lib.Repository;
@@ -111,7 +110,6 @@ public class PatchSetInserter {
   private boolean runHooks;
   private boolean sendMail;
   private Account.Id uploader;
-  private BatchRefUpdate batchRefUpdate;
 
   @Inject
   public PatchSetInserter(ChangeHooks hooks,
@@ -216,11 +214,6 @@ public class PatchSetInserter {
     return this;
   }
 
-  public PatchSetInserter setBatchRefUpdate(BatchRefUpdate batchRefUpdate) {
-    this.batchRefUpdate = batchRefUpdate;
-    return this;
-  }
-
   public Change insert() throws InvalidChangeOperationException, OrmException,
       IOException, NoSuchChangeException {
     init();
@@ -228,23 +221,16 @@ public class PatchSetInserter {
 
     Change c = ctl.getChange();
     Change updatedChange;
-
-    if (batchRefUpdate != null) {
-      // Caller passed in update; add command, but don't execute.
-      batchRefUpdate.addCommand(new ReceiveCommand(ObjectId.zeroId(), commit,
-        patchSet.getRefName(), ReceiveCommand.Type.CREATE));
-    } else {
-      RefUpdate ru = git.updateRef(patchSet.getRefName());
-      ru.setExpectedOldObjectId(ObjectId.zeroId());
-      ru.setNewObjectId(commit);
-      ru.disableRefLog();
-      if (ru.update(revWalk) != RefUpdate.Result.NEW) {
-        throw new IOException(String.format(
-            "Failed to create ref %s in %s: %s", patchSet.getRefName(),
-            c.getDest().getParentKey().get(), ru.getResult()));
-      }
-      gitRefUpdated.fire(c.getProject(), ru);
+    RefUpdate ru = git.updateRef(patchSet.getRefName());
+    ru.setExpectedOldObjectId(ObjectId.zeroId());
+    ru.setNewObjectId(commit);
+    ru.disableRefLog();
+    if (ru.update(revWalk) != RefUpdate.Result.NEW) {
+      throw new IOException(String.format(
+          "Failed to create ref %s in %s: %s", patchSet.getRefName(),
+          c.getDest().getParentKey().get(), ru.getResult()));
     }
+    gitRefUpdated.fire(c.getProject(), ru);
 
     final PatchSet.Id currentPatchSetId = c.currentPatchSetId();
 

gerrit-server/src/main/java/com/google/gerrit/server/changedetail/RebaseChange.java
@@ -14,7 +14,6 @@
 
 package com.google.gerrit.server.changedetail;
 
-import com.google.gerrit.common.Nullable;
 import com.google.gerrit.common.TimeUtil;
 import com.google.gerrit.common.errors.EmailException;
 import com.google.gerrit.reviewdb.client.Branch;
@@ -43,7 +42,6 @@ import com.google.inject.Provider;
 import com.google.inject.Singleton;
 
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
-import org.eclipse.jgit.lib.BatchRefUpdate;
 import org.eclipse.jgit.lib.CommitBuilder;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectInserter;
@@ -140,7 +138,7 @@ public class RebaseChange {
       rebase(git, rw, inserter, patchSetId, change,
           uploader, baseCommit, mergeUtilFactory.create(
               changeControl.getProjectControl().getProjectState(), true),
-          committerIdent, true, true, ValidatePolicy.GERRIT, null);
+          committerIdent, true, true, ValidatePolicy.GERRIT);
     } catch (MergeConflictException e) {
       throw new IOException(e.getMessage());
     } finally {
@@ -270,8 +268,6 @@ public class RebaseChange {
    * @param sendMail if a mail notification should be sent for the new patch set
    * @param runHooks if hooks should be run for the new patch set
    * @param validate if commit validation should be run for the new patch set
-   * @param batchRefUpdate if not null, a batch ref update for creating new
-   *     refs, which will not be executed.
    * @return the new patch set which is based on the given base commit
    * @throws NoSuchChangeException thrown if the change to which the patch set
    *         belongs does not exist or is not visible to the user
@@ -279,13 +275,14 @@ public class RebaseChange {
    * @throws IOException thrown if rebase is not possible or not needed
    * @throws InvalidChangeOperationException thrown if rebase is not allowed
    */
-  public PatchSet rebase(Repository git, RevWalk revWalk,
-      ObjectInserter inserter, PatchSet.Id patchSetId, Change change,
-      IdentifiedUser uploader, RevCommit baseCommit, MergeUtil mergeUtil,
-      PersonIdent committerIdent, boolean sendMail, boolean runHooks,
-      ValidatePolicy validate, @Nullable BatchRefUpdate batchRefUpdate)
-      throws NoSuchChangeException, OrmException, IOException,
-      InvalidChangeOperationException, MergeConflictException {
+  public PatchSet rebase(final Repository git, final RevWalk revWalk,
+      final ObjectInserter inserter, final PatchSet.Id patchSetId,
+      final Change change, final IdentifiedUser uploader, final RevCommit baseCommit,
+      final MergeUtil mergeUtil, PersonIdent committerIdent,
+      boolean sendMail, boolean runHooks, ValidatePolicy validate)
+          throws NoSuchChangeException,
+      OrmException, IOException, InvalidChangeOperationException,
+      MergeConflictException {
     if (!change.currentPatchSetId().equals(patchSetId)) {
       throw new InvalidChangeOperationException("patch set is not current");
     }
@@ -308,9 +305,6 @@ public class RebaseChange {
         .setUploader(uploader.getAccountId())
         .setSendMail(sendMail)
         .setRunHooks(runHooks);
-    if (batchRefUpdate != null) {
-      patchSetInserter.setBatchRefUpdate(batchRefUpdate);
-    }
 
     final PatchSet.Id newPatchSetId = patchSetInserter.getPatchSetId();
     final ChangeMessage cmsg = new ChangeMessage(

gerrit-server/src/main/java/com/google/gerrit/server/git/MergeUtil.java
@@ -43,6 +43,7 @@ import org.eclipse.jgit.errors.LargeObjectException;
 import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.errors.NoMergeBaseException;
 import org.eclipse.jgit.errors.NoMergeBaseException.MergeBaseFailureReason;
+import org.eclipse.jgit.lib.AnyObjectId;
 import org.eclipse.jgit.lib.CommitBuilder;
 import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.Constants;
@@ -66,6 +67,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -77,12 +79,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.TimeZone;
 
-/**
- * Utilities for various kinds of merges and cherry-picks.
- * <p>
- * <b>Note:</b> Unless otherwise noted, the methods in this class do not flush
- * the {@link ObjectInserter}s passed in after performing a merge.
- */
 public class MergeUtil {
   private static final Logger log = LoggerFactory.getLogger(MergeUtil.class);
   private static final String R_HEADS_MASTER =
@@ -181,7 +177,7 @@ public class MergeUtil {
     final ThreeWayMerger m = newThreeWayMerger(repo, inserter);
 
     m.setBase(originalCommit.getParent(0));
-    if (m.merge(false, mergeTip, originalCommit)) {
+    if (m.merge(mergeTip, originalCommit)) {
       ObjectId tree = m.getResultTreeId();
       if (tree.equals(mergeTip.getTree())) {
         throw new MergeIdenticalTreeException("identical tree");
@@ -193,7 +189,7 @@ public class MergeUtil {
       mergeCommit.setAuthor(originalCommit.getAuthorIdent());
       mergeCommit.setCommitter(cherryPickCommitterIdent);
       mergeCommit.setMessage(commitMsg);
-      return rw.parseCommit(inserter.insert(mergeCommit));
+      return rw.parseCommit(commit(inserter, mergeCommit));
     } else {
       throw new MergeConflictException("merge conflict");
     }
@@ -397,7 +393,7 @@ public class MergeUtil {
 
     ThreeWayMerger m = newThreeWayMerger(repo, createDryRunInserter(repo));
     try {
-      return m.merge(false, mergeTip, toMerge);
+      return m.merge(new AnyObjectId[] {mergeTip, toMerge});
     } catch (LargeObjectException e) {
       log.warn("Cannot merge due to LargeObjectException: " + toMerge.name());
       return false;
@@ -446,7 +442,7 @@ public class MergeUtil {
       try {
         ThreeWayMerger m = newThreeWayMerger(repo, createDryRunInserter(repo));
         m.setBase(toMerge.getParent(0));
-        return m.merge(false, mergeTip, toMerge);
+        return m.merge(mergeTip, toMerge);
       } catch (IOException e) {
         throw new MergeException("Cannot merge " + toMerge.name(), e);
       }
@@ -498,7 +494,7 @@ public class MergeUtil {
       throws MergeException {
     final ThreeWayMerger m = newThreeWayMerger(repo, inserter);
     try {
-      if (m.merge(false, mergeTip, n)) {
+      if (m.merge(new AnyObjectId[] {mergeTip, n})) {
         return writeMergeCommit(myIdent, rw, inserter, canMergeFlag, destBranch,
             mergeTip, m.getResultTreeId(), n);
       } else {
@@ -586,7 +582,7 @@ public class MergeUtil {
     mergeCommit.setMessage(msgbuf.toString());
 
     CodeReviewCommit mergeResult =
-        (CodeReviewCommit) rw.parseCommit(inserter.insert(mergeCommit));
+        (CodeReviewCommit) rw.parseCommit(commit(inserter, mergeCommit));
     mergeResult.setControl(n.getControl());
     return mergeResult;
   }
@@ -660,8 +656,29 @@ public class MergeUtil {
     Merger m = strategy.newMerger(repo, true);
     checkArgument(m instanceof ThreeWayMerger,
         "merge strategy %s does not support three-way merging", strategyName);
-    m.setObjectInserter(inserter);
+    m.setObjectInserter(new ObjectInserter.Filter() {
+      @Override
+      protected ObjectInserter delegate() {
+        return inserter;
+      }
+
+      @Override
+      public void flush() {
+      }
+
+      @Override
+      public void release() {
+      }
+    });
     return (ThreeWayMerger) m;
+  }
+
+  public ObjectId commit(final ObjectInserter inserter,
+      final CommitBuilder mergeCommit) throws IOException,
+      UnsupportedEncodingException {
+    ObjectId id = inserter.insert(mergeCommit);
+    inserter.flush();
+    return id;
   }
 
   public PatchSetApproval markCleanMerges(final RevWalk rw,

commit 32896cfb1d89f2a995371b294fb0b5bf63cd2118

Fix compilation broken by 875fc8e8
I had fixed this locally in the previous change but forgot to push the
new patch set.

Change-Id: I1e5469bf2655edf3d39baf218f4a6d2f7a35498c

Former-commit-id: 4d6478a83511ecbd127bdd16e60d9baf7c366a0b

gerrit-server/src/main/java/com/google/gerrit/server/git/MergeOp.java
@@ -3166,14 +3166,12 @@ block|}
 block|}
 name|logDebug
 argument_list|(
-literal|"Opened branch {}: {}, (parsed to {})"
+literal|"Opened branch {}: {}"
 argument_list|,
 name|destBranch
 operator|.
 name|get
 argument_list|()
-argument_list|,
-name|oldRef
 argument_list|,
 name|branchTip
 argument_list|)