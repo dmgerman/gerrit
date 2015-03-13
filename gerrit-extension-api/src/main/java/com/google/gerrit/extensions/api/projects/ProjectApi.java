49f988641394e4ebdac8bec6a156ce6f6bfeb061
Dave Borowitz <dborowitz@google.com>
2015-03-13 10:07:22 -0700

    ProjectApi: Throw ResourceNotFoundException from get/branch
    This can happen when the project is not found, which is a legitimate
    state before a project is created. Allow callers to recover by
    catching ResourceNotFoundException.
    
    Change-Id: I9ea2638ea996c6adee2a77b841bfc9c5c5afd45a
    
    Former-commit-id: ae4772a4eb13da238315e832253ed0f7ef17bd4c

--- gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/projects/ProjectApi.java
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/projects/ProjectApi.java
@@ -143,8 +143,10 @@ DECL|method|get ()
 name|ProjectInfo
 name|get
 parameter_list|()
+throws|throws
+name|RestApiException
 function_decl|;
-comment|/**    * Look up a branch by refname.    *<p>    *<strong>Note:</strong> This method eagerly reads the branch. Methods that    * mutate the branch do not necessarily re-read the branch. Therefore, calling    * a getter method on an instance after calling a mutation method on that same    * instance is not guaranteed to reflect the mutation. It is not recommended    * to store references to {@code BranchApi} instances.    *    * @param ref branch name, with or without "refs/heads/" prefix.    * @return API for accessing the branch.    */
+comment|/**    * Look up a branch by refname.    *<p>    *<strong>Note:</strong> This method eagerly reads the branch. Methods that    * mutate the branch do not necessarily re-read the branch. Therefore, calling    * a getter method on an instance after calling a mutation method on that same    * instance is not guaranteed to reflect the mutation. It is not recommended    * to store references to {@code BranchApi} instances.    *    * @param ref branch name, with or without "refs/heads/" prefix.    * @throws RestApiException if a problem occurred reading the project.    * @return API for accessing the branch.    */
 DECL|method|branch (String ref)
 name|BranchApi
 name|branch
@@ -152,6 +154,8 @@ parameter_list|(
 name|String
 name|ref
 parameter_list|)
+throws|throws
+name|RestApiException
 function_decl|;
 comment|/**    * A default implementation which allows source compatibility    * when adding new methods to the interface.    **/
 DECL|class|NotImplemented
@@ -203,6 +207,8 @@ specifier|public
 name|ProjectInfo
 name|get
 parameter_list|()
+throws|throws
+name|RestApiException
 block|{
 throw|throw
 operator|new
@@ -220,6 +226,8 @@ parameter_list|(
 name|String
 name|ref
 parameter_list|)
+throws|throws
+name|RestApiException
 block|{
 throw|throw
 operator|new
