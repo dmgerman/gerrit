0332a4e87b290407780679f3b002321b52aeb77c
David Pursehouse <david.pursehouse@sonymobile.com>
2015-04-14 17:23:51 +0900

    Organize imports
    Change-Id: I42d6e3acf14d72472d1d038836be5fa33dc31feb
    
    Former-commit-id: b7c57bbf333cc544adc01f7011993d118584636c

--- gerrit-lucene/src/main/java/com/google/gerrit/lucene/WrappableSearcherManager.java
+++ gerrit-lucene/src/main/java/com/google/gerrit/lucene/WrappableSearcherManager.java
@@ -18,16 +18,6 @@ end_comment
 
 begin_import
 import|import
-name|java
-operator|.
-name|io
-operator|.
-name|IOException
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
@@ -149,6 +139,16 @@ operator|.
 name|store
 operator|.
 name|Directory
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|IOException
 import|;
 end_import
 
