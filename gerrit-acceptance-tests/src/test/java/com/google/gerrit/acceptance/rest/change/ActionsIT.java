dfd97d1a4382147df3cfaf1b46300eb487d60e77
David Pursehouse <david.pursehouse@sonymobile.com>
2015-06-30 11:36:31 +0900

    Revert "AbstractSubmit, ActionsIT: Disable tests for submitWholeTopic"
    The submitWholeTopic feature is enabled on the master branch; the
    tests are only supposed to be disabled on stable-2.11.
    
    This reverts commit 2974c7cbddf5c0d4c632ab771d14b783a6a1c0a7 [formerly 1c4a7ffc354b74f835f93405d113fa2e39b3fc1f].
    
    Change-Id: Ie26594f5253c443f281f3b11e598f169a9c42353
    
    Former-commit-id: 15bd44bc47b4db2b1e1b469d0ae425d7926848f6

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change/ActionsIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change/ActionsIT.java
@@ -148,6 +148,20 @@ end_import
 
 begin_import
 import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|testutil
+operator|.
+name|ConfigSuite
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|eclipse
@@ -182,6 +196,20 @@ begin_import
 import|import
 name|org
 operator|.
+name|eclipse
+operator|.
+name|jgit
+operator|.
+name|lib
+operator|.
+name|Config
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
 name|junit
 operator|.
 name|Test
@@ -206,6 +234,22 @@ name|ActionsIT
 extends|extends
 name|AbstractDaemonTest
 block|{
+annotation|@
+name|ConfigSuite
+operator|.
+name|Config
+DECL|method|submitWholeTopicEnabled ()
+specifier|public
+specifier|static
+name|Config
+name|submitWholeTopicEnabled
+parameter_list|()
+block|{
+return|return
+name|submitWholeTopicEnabledConfig
+argument_list|()
+return|;
+block|}
 annotation|@
 name|Test
 DECL|method|revisionActionsOneChangePerTopicUnapproved ()
