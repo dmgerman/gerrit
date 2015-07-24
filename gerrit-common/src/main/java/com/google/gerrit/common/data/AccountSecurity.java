1228658097644ccebcd98d4e81b08a08ee948eef
Edwin Kempin <edwin.kempin@sap.com>
2015-07-24 11:47:57 +0200

    Add REST endpoint to set username
    This REST endpoint only allows to set the initial username. Once set
    the username cannot be changed or deleted.
    
    Use the new REST endpoint from the UI instead of the old
    AccountSecurity.changeUserName(...) RPC.
    
    The AccountSecurity.changeUserName(...) RPC is removed since it is no
    longer used.
    
    Change-Id: I48f4d7642b551e17ceef7772563a229aa83fc1ad
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>
    
    Former-commit-id: f07e98ba5cf854754eecd6146670048af2469f1a

--- gerrit-common/src/main/java/com/google/gerrit/common/data/AccountSecurity.java
+++ gerrit-common/src/main/java/com/google/gerrit/common/data/AccountSecurity.java
@@ -256,24 +256,6 @@ extends|extends
 name|RemoteJsonService
 block|{
 annotation|@
-name|Audit
-annotation|@
-name|SignInRequired
-DECL|method|changeUserName (String newName, AsyncCallback<VoidResult> callback)
-name|void
-name|changeUserName
-parameter_list|(
-name|String
-name|newName
-parameter_list|,
-name|AsyncCallback
-argument_list|<
-name|VoidResult
-argument_list|>
-name|callback
-parameter_list|)
-function_decl|;
-annotation|@
 name|SignInRequired
 DECL|method|myExternalIds (AsyncCallback<List<AccountExternalId>> callback)
 name|void
