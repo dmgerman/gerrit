ec96fe64bca7f953ad0e286f53388ed55fa2926a
Edwin Kempin <edwin.kempin@sap.com>
2015-07-24 16:31:26 +0200

    Make date formatter available to plugins
    Users can control via the user preferences how dates should be
    formatted. Allow plugins to use the same date formatter as Gerrit core
    that formats dates depending on the user preferences.
    
    Note, we cannot simply move the complete FormatUtil class to
    gerrit-gwtui-common because it needs access to the user preferences
    and some server config, like the anonymous coward name, and this
    information is retrieved differently in Gerrit core and plugins. This
    is why for now we have 2 FormatUtil classes, one in Gerrit core and
    one in gerrit-plugin-gwtui. Common parts such as the DateFormatter
    will be made available to both FormatUtil classes by putting them into
    gerrit-gwtui-common.
    
    Change-Id: Iad241b461ad0171a2bbf279fb478dc40196cc201
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>
    
    Former-commit-id: ac34c132fb5270fcc48c2a6843222b402815bb1d

---
+++ gerrit-plugin-gwtui/src/main/java/com/google/gerrit/plugin/client/FormatUtil.java
@@ -0,0 +1,187 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright (C) 2015 The Android Open Source Project
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.plugin.client
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|plugin
+operator|.
+name|client
+package|;
+end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|client
+operator|.
+name|DateFormatter
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Date
+import|;
+end_import
+
+begin_class
+DECL|class|FormatUtil
+specifier|public
+class|class
+name|FormatUtil
+block|{
+comment|/** Format a date using a really short format. */
+DECL|method|shortFormat (Date dt)
+specifier|public
+specifier|static
+name|String
+name|shortFormat
+parameter_list|(
+name|Date
+name|dt
+parameter_list|)
+block|{
+return|return
+name|createDateFormatter
+argument_list|()
+operator|.
+name|shortFormat
+argument_list|(
+name|dt
+argument_list|)
+return|;
+block|}
+comment|/** Format a date using a really short format. */
+DECL|method|shortFormatDayTime (Date dt)
+specifier|public
+specifier|static
+name|String
+name|shortFormatDayTime
+parameter_list|(
+name|Date
+name|dt
+parameter_list|)
+block|{
+return|return
+name|createDateFormatter
+argument_list|()
+operator|.
+name|shortFormatDayTime
+argument_list|(
+name|dt
+argument_list|)
+return|;
+block|}
+comment|/** Format a date using the locale's medium length format. */
+DECL|method|mediumFormat (Date dt)
+specifier|public
+specifier|static
+name|String
+name|mediumFormat
+parameter_list|(
+name|Date
+name|dt
+parameter_list|)
+block|{
+return|return
+name|createDateFormatter
+argument_list|()
+operator|.
+name|mediumFormat
+argument_list|(
+name|dt
+argument_list|)
+return|;
+block|}
+DECL|method|createDateFormatter ()
+specifier|private
+specifier|static
+name|DateFormatter
+name|createDateFormatter
+parameter_list|()
+block|{
+return|return
+operator|new
+name|DateFormatter
+argument_list|(
+name|Plugin
+operator|.
+name|get
+argument_list|()
+operator|.
+name|getUserPreferences
+argument_list|()
+argument_list|)
+return|;
+block|}
+block|}
+end_class
+
+end_unit
+
