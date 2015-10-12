f2cea28ee8db8605d9bc1028c88dc1598b8b2c8d
Dave Borowitz <dborowitz@google.com>
2015-10-12 10:00:08 -0400

    Pass correct token parameter to ConfirmEmail
    The server requires "token" but the client was sending "t", breaking
    EmailTokenVerifier implementations.
    
    Explicitly disallow null in the javadoc for EmailTokenVerifier, and
    check for null encode/decode call sites. This would have at least
    returned a 422 instead of 500 in the case we're seeing in our
    backends.
    
    Change-Id: Ibcda9f63fc55ab1f8f32a400c2c83780a8541ccd
    
    Former-commit-id: 71b2c059069b657e88223e24006201f951668193

--- gerrit-server/src/main/java/com/google/gerrit/server/mail/EmailTokenVerifier.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/mail/EmailTokenVerifier.java
@@ -108,7 +108,7 @@ specifier|public
 interface|interface
 name|EmailTokenVerifier
 block|{
-comment|/**    * Construct a token to verify an email address for a user.    *    * @param accountId the caller that wants to add an email to their account.    * @param emailAddress the address to add.    * @return an unforgeable string to email to {@code emailAddress}. Presenting    *         the string provides proof the user has the ability to read messages    *         sent to that address.    */
+comment|/**    * Construct a token to verify an email address for a user.    *    * @param accountId the caller that wants to add an email to their account.    * @param emailAddress the address to add.    * @return an unforgeable string to email to {@code emailAddress}. Presenting    *         the string provides proof the user has the ability to read messages    *         sent to that address. Must not be null.    */
 DECL|method|encode (Account.Id accountId, String emailAddress)
 specifier|public
 name|String
@@ -123,7 +123,7 @@ name|String
 name|emailAddress
 parameter_list|)
 function_decl|;
-comment|/**    * Decode a token previously created.    * @param tokenString the string created by encode.    * @return a pair of account id and email address.    * @throws InvalidTokenException the token is invalid, expired, malformed, etc.    */
+comment|/**    * Decode a token previously created.    * @param tokenString the string created by encode. Never null.    * @return a pair of account id and email address.    * @throws InvalidTokenException the token is invalid, expired, malformed, etc.    */
 DECL|method|decode (String tokenString)
 specifier|public
 name|ParsedToken
