2b46dc455d8bc1f1e70c9ad244946d3555077fab
Michael Ochmann <michael.ochmann@sap.com>
2015-10-20 15:34:29 +0200

    OAuth2 authentication for Git-over-HTTP
    OAuth2 support was only implemented for the web UI so far
    but not for Git-over-HTTP communication. This patch adds
    a mechanism similiar to that supported by Github,
    where Git clients may send OAuth2 access tokens instead
    of passwords in a Basic authentication header [1].
    
    Received access tokens will be verified by means of an
    OAuthLoginProvider, which is a new extension point.
    The OAuth2 protocol does not specify a mechanism for how to
    verify access tokens, so there is no default implementation
    for the OAuthLoginProvider interface, but a plugin must
    provide a suitable implementation.
    
    In order to enable OAuth2 authentication for Git-over-HTTP
    the configuration option auth.type must be set to OAUTH
    and auth.gitBasicAuth must be set to true. The parameter
    auth.gitOAuthProvider defines the default OAuthLoginProvider
    to use in case multiple OAuthLoginProvider implementations
    are installed and it cannot be deduced from the request,
    which OAuth provider to address.
    
    An OAuthLoginProvider implementation may also support
    HTTP Basic authentication with passwords instead of access
    tokens, if the OAuth2 backend supports the Resource Owner
    Password Credentials Grant authentication flow [2] or some
    other API for verifying password credentials. For that reason
    the second parameter of the OAuthLoginProvider interface is
    called "secret" instead of "accessToken".
    
    An example implementation for the OAuthLoginProvider
    extension point will be contributed to the cfoauth plugin.
    
    [1] https://developer.github.com/v3/auth/#basic-authentication
    [2] https://tools.ietf.org/html/rfc6749#section-4.3
    
    Change-Id: I0f00599dce38a806fd3e21758ea9e2cab49ce57f
    Signed-off-by: Michael Ochmann <michael.ochmann@sap.com>
    
    Former-commit-id: e9e046a4b3e6cc40963d22b81fef3f17f59e162d

---
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/auth/oauth/OAuthLoginProvider.java
@@ -0,0 +1,123 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright (C) 2015 The Android Open Source Project
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.extensions.auth.oauth
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|extensions
+operator|.
+name|auth
+operator|.
+name|oauth
+package|;
+end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|extensions
+operator|.
+name|annotations
+operator|.
+name|ExtensionPoint
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|IOException
+import|;
+end_import
+
+begin_interface
+annotation|@
+name|ExtensionPoint
+DECL|interface|OAuthLoginProvider
+specifier|public
+interface|interface
+name|OAuthLoginProvider
+block|{
+comment|/**    * Performs a login with an OAuth2 provider for Git over HTTP    * communication.    *    * An implementation of this interface must transmit the given    * user name and secret, which can be either an OAuth2 access token    * or a password, to the OAuth2 backend for verification.    *    * @param username the user's identifier.    * @param secret the secret to verify, e.g. a previously received    * access token or a password.    *    * @return information about the logged in user, at least    * external id, user name and email address.    *    * @throws IOException if the login failed.    */
+DECL|method|login (String username, String secret)
+name|OAuthUserInfo
+name|login
+parameter_list|(
+name|String
+name|username
+parameter_list|,
+name|String
+name|secret
+parameter_list|)
+throws|throws
+name|IOException
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
