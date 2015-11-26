8462979119f564c792171f0314c8dd0d91116fb9
David Pursehouse <david.pursehouse@sonymobile.com>
2015-11-26 09:46:26 +0900

    StaticModule: Remove unused import
    Change-Id: I41034774cb1ccdf7905bddecd7c55f91d9a85aa7
    
    Former-commit-id: c4dc9d661aa7cf8418df1613839dceef8c8bdbd0

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/raw/StaticModule.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/raw/StaticModule.java
@@ -278,16 +278,6 @@ name|javax
 operator|.
 name|servlet
 operator|.
-name|ServletException
-import|;
-end_import
-
-begin_import
-import|import
-name|javax
-operator|.
-name|servlet
-operator|.
 name|http
 operator|.
 name|HttpServlet
