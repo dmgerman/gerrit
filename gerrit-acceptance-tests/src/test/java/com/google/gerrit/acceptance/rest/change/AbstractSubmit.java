c8d8a0af1c62bb6c92a7c418b1eb80b951d4cf87
Dave Borowitz <dborowitz@google.com>
2016-01-29 14:41:30 -0500

    AbstractSubmit: Set @NoHttpd
    Tweak GerritServer to respect this annotation on any superclass.
    
    Change-Id: Ic8bd560125815b5ad2829ac7101962c6bf5d275d
    
    Former-commit-id: c0ad7dd1870938da9439d6d457bd276803e851c3

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change/AbstractSubmit.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change/AbstractSubmit.java
@@ -246,6 +246,20 @@ name|gerrit
 operator|.
 name|acceptance
 operator|.
+name|NoHttpd
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|acceptance
+operator|.
 name|PushOneCommit
 import|;
 end_import
@@ -933,6 +947,8 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|NoHttpd
 DECL|class|AbstractSubmit
 specifier|public
 specifier|abstract
