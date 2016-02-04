6762da3c0e3d42f6e8bc4f2a16505eca8caa6311
Dave Borowitz <dborowitz@google.com>
2016-02-04 13:54:16 -0500

    Upgrade Lucene to 5.4.1
    Change-Id: I9acd9302225a20980d5d5a1cd1ce95cc1c22fe61
    
    Former-commit-id: 3b553fa968afa11103784db111fd84534f572775

--- gerrit-lucene/src/main/java/com/google/gerrit/lucene/AutoCommitWriter.java
+++ gerrit-lucene/src/main/java/com/google/gerrit/lucene/AutoCommitWriter.java
@@ -511,11 +511,7 @@ throws|throws
 name|IOException
 block|{
 name|flush
-argument_list|(
-literal|true
-argument_list|,
-literal|true
-argument_list|)
+argument_list|()
 expr_stmt|;
 if|if
 condition|(
