c63a7876fb7ac0975f792497461908a86867816b
David Pursehouse <david.pursehouse@sonymobile.com>
2016-02-12 15:15:49 +0900

    AdminQueryShell: Update Javadoc
    Remove reference to `canPerformRawQuery` capability which no longer
    exists.
    
    Change-Id: Ie7c9ba9288c49c3d13363d7f6670788baa6b777c
    
    Former-commit-id: 82897bd0d9cdeda0204199cb6eac6ee59fe193be

--- gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/AdminQueryShell.java
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/AdminQueryShell.java
@@ -364,7 +364,7 @@ argument_list|()
 expr_stmt|;
 block|}
 block|}
-comment|/**    * Assert that the current user is permitted to perform raw queries.    *<p>    * As the @RequireCapability guards at various entry points of internal    * commands implicitly add administrators (which we want to avoid), we also    * check permissions within QueryShell and grant access only to those who    * canPerformRawQuery, regardless of whether they are administrators or not.    *    * @throws PermissionDeniedException    */
+comment|/**    * Assert that the current user is permitted to perform raw queries.    *<p>    * As the @RequireCapability guards at various entry points of internal    * commands implicitly add administrators (which we want to avoid), we also    * check permissions within QueryShell and grant access only to those who    * can access the database, regardless of whether they are administrators or    * not.    *    * @throws PermissionDeniedException    */
 DECL|method|checkPermission ()
 specifier|private
 name|void
