660810bab41b67663677b35a63b0bacf46ebc021
David Pursehouse <david.pursehouse@sonymobile.com>
2016-02-18 17:51:19 +0900

    Move InvalidUserNameException to com.google.gerrit.server.account
    Because that's the only package where it's used.
    
    Change-Id: I2df2ac9f519131016cde03e277d3832524a02625
    
    Former-commit-id: 8ff798add45d96c668a9b8215e5ad2edabde7e89

--- gerrit-server/src/main/java/com/google/gerrit/server/account/InvalidUserNameException.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/InvalidUserNameException.java
@@ -52,7 +52,7 @@ comment|// limitations under the License.
 end_comment
 
 begin_package
-DECL|package|com.google.gerrit.common.errors
+DECL|package|com.google.gerrit.server.account
 package|package
 name|com
 operator|.
@@ -60,9 +60,9 @@ name|google
 operator|.
 name|gerrit
 operator|.
-name|common
+name|server
 operator|.
-name|errors
+name|account
 package|;
 end_package
 
