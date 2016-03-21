44fe64fd9dfb3da23f251504175e86ae9259ea78
Stefan Beller <sbeller@google.com>
2016-03-21 12:14:58 -0700

    ACLs for superproject subscriptions
    This allows submodules of the superproject subscription feature to specify
    fine grained, who is allowed to subscribe to it. See
    Documentation/user-submodules.txt for the changed
    handling of subscriptions.
    
    The current tests were kept closely as-is, just enabling the subscription
    feature. New tests have been written to test for the denial of superproject
    subscriptions.
    
    As of this change the superproject subscription table which was an
    approximate cache for the subscriptions is dropped, and the superproject
    subscription is performed as
     * parse the submodule ACL for potential superprojects
     * check the .gitmodules file in all potential superprojects for a real
       subscription
     * perform the superproject update if the supscription is valid
    
    The cache worked semi-reliable (e.g. 2015-04-28 Submodule Subscriptions:
    Remove subscriptions by deleting .gitmodules,
    I1eaf452d5499397644e8eea3707a1352af89126d), so drop the cache and trade in
    correctness over performance.
    
    Bug: Issue 3311
    Change-Id: Id74dc5a34a50b336a22005c96b79f3c4688a36ec
    Signed-off-by: Stefan Beller <sbeller@google.com>
    
    Former-commit-id: c62f9fe5111af422d4e93cf48e1cb0dfe6a561dd

--- gerrit-server/src/main/java/com/google/gerrit/server/schema/SchemaVersion.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/SchemaVersion.java
@@ -248,11 +248,11 @@ specifier|static
 specifier|final
 name|Class
 argument_list|<
-name|Schema_119
+name|Schema_120
 argument_list|>
 name|C
 init|=
-name|Schema_119
+name|Schema_120
 operator|.
 name|class
 decl_stmt|;
