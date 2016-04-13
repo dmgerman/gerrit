d02ae47c9d1a6daf3cab00dac3e1bc315d5d0ed1
Edwin Kempin <ekempin@google.com>
2016-04-13 09:11:14 +0200

    Fix subject for "Updated Changes" lines on push
    When a change was updated it showed the subject from the previous
    patch set and not as expected the subject from the new current patch
    set.
    
    Bug: Issue 4055
    Change-Id: I33d1912cccea795fc003a07bc88800c536c301da
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    
    Former-commit-id: c33e32e0bae8445627f92aebdb66b373023f92f7

--- gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
@@ -5371,6 +5371,13 @@ name|c
 operator|.
 name|change
 argument_list|,
+name|c
+operator|.
+name|change
+operator|.
+name|getSubject
+argument_list|()
+argument_list|,
 literal|false
 argument_list|)
 argument_list|)
@@ -5528,6 +5535,13 @@ name|u
 operator|.
 name|change
 argument_list|,
+name|u
+operator|.
+name|info
+operator|.
+name|getSubject
+argument_list|()
+argument_list|,
 name|edit
 argument_list|)
 argument_list|)
@@ -5540,7 +5554,7 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-DECL|method|formatChangeUrl (String url, Change change, boolean edit)
+DECL|method|formatChangeUrl (String url, Change change, String subject, boolean edit)
 specifier|private
 specifier|static
 name|String
@@ -5551,6 +5565,9 @@ name|url
 parameter_list|,
 name|Change
 name|change
+parameter_list|,
+name|String
+name|subject
 parameter_list|,
 name|boolean
 name|edit
@@ -5592,10 +5609,7 @@ name|ChangeUtil
 operator|.
 name|cropSubject
 argument_list|(
-name|change
-operator|.
-name|getSubject
-argument_list|()
+name|subject
 argument_list|)
 argument_list|)
 decl_stmt|;
