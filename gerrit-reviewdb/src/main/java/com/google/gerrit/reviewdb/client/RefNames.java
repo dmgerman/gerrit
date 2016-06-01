048c3f272af4bdcdaa0bd60b3660f9b14f8ef193
Edwin Kempin <ekempin@google.com>
2016-06-01 21:50:12 +0200

    VisibleRefFilter: Fix NPE on check of edit ref when currAccountId is null
    Let RefNames#isRefsEditOf(String, Account.Id) return false if the
    account ID is null.
    
    Change-Id: Ibfabac411261fab74deee989d87c69f00778b69a
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    
    Former-commit-id: 9a93809908e3c6645f83d73ad99ecdf5366f7f42

--- gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/client/RefNames.java
+++ gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/client/RefNames.java
@@ -826,6 +826,17 @@ name|Id
 name|accountId
 parameter_list|)
 block|{
+if|if
+condition|(
+name|accountId
+operator|==
+literal|null
+condition|)
+block|{
+return|return
+literal|false
+return|;
+block|}
 name|String
 name|prefix
 init|=
