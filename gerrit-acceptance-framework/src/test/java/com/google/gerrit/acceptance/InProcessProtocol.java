dmgerman/gerrit

commit 145c0844caedb95a55e617026bbef426a478f469

VisibleRefFilter Avoid touching the database for drafts
This is an analogous problem to the one with search results fixed in
Ie54b9e2d. VisibleRefFilter is a little trickier because it was using
ChangeCache, which only returned Changes, not the reviewer set. Change
SearchingChangeCacheImpl to cache a different value type and return
ChangeDatas from its search method so we can use the cached
ReviewerSet where appropriate.

Unfortunately for the Reindex program we still need to support not
having a SearchingChangeCacheImpl, which means we have a completely
separate codepath for reading changes from the database.

Change-Id: Ic432a8e48a2bafc8d142b84b25101d95ffb674b7

Former-commit-id: a758640bc33c20cead19b86a32db075c776c4140

gerrit-httpd/src/main/java/com/google/gerrit/httpd/GitOverHttpServlet.java
@@ -242,22 +242,6 @@ name|server
 operator|.
 name|git
 operator|.
-name|ChangeCache
-import|;
-end_import
-
-begin_import
-import|import
-name|com
-operator|.
-name|google
-operator|.
-name|gerrit
-operator|.
-name|server
-operator|.
-name|git
-operator|.
 name|GitRepositoryManager
 import|;
 end_import
@@ -275,6 +259,22 @@ operator|.
 name|git
 operator|.
 name|ReceiveCommits
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+operator|.
+name|git
+operator|.
+name|SearchingChangeCacheImpl
 import|;
 end_import
 
@@ -357,6 +357,22 @@ operator|.
 name|validators
 operator|.
 name|UploadValidators
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+operator|.
+name|notedb
+operator|.
+name|ChangeNotes
 import|;
 end_import
 
@@ -1618,10 +1634,18 @@ specifier|final
 name|TagCache
 name|tagCache
 decl_stmt|;
+DECL|field|changeNotesFactory
+specifier|private
+specifier|final
+name|ChangeNotes
+operator|.
+name|Factory
+name|changeNotesFactory
+decl_stmt|;
 DECL|field|changeCache
 specifier|private
 specifier|final
-name|ChangeCache
+name|SearchingChangeCacheImpl
 name|changeCache
 decl_stmt|;
 DECL|field|uploadValidatorsFactory
@@ -1634,7 +1658,7 @@ name|uploadValidatorsFactory
 decl_stmt|;
 annotation|@
 name|Inject
-DECL|method|UploadFilter (Provider<ReviewDb> db, TagCache tagCache, ChangeCache changeCache, UploadValidators.Factory uploadValidatorsFactory)
+DECL|method|UploadFilter (Provider<ReviewDb> db, TagCache tagCache, ChangeNotes.Factory changeNotesFactory, SearchingChangeCacheImpl changeCache, UploadValidators.Factory uploadValidatorsFactory)
 name|UploadFilter
 parameter_list|(
 name|Provider
@@ -1646,7 +1670,12 @@ parameter_list|,
 name|TagCache
 name|tagCache
 parameter_list|,
-name|ChangeCache
+name|ChangeNotes
+operator|.
+name|Factory
+name|changeNotesFactory
+parameter_list|,
+name|SearchingChangeCacheImpl
 name|changeCache
 parameter_list|,
 name|UploadValidators
@@ -1666,6 +1695,12 @@ operator|.
 name|tagCache
 operator|=
 name|tagCache
+expr_stmt|;
+name|this
+operator|.
+name|changeNotesFactory
+operator|=
+name|changeNotesFactory
 expr_stmt|;
 name|this
 operator|.
@@ -1824,6 +1859,8 @@ operator|new
 name|VisibleRefFilter
 argument_list|(
 name|tagCache
+argument_list|,
+name|changeNotesFactory
 argument_list|,
 name|changeCache
 argument_list|,

gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
@@ -3449,7 +3449,7 @@ name|batch
 decl_stmt|;
 annotation|@
 name|Inject
-DECL|method|ReceiveCommits (ReviewDb db, Sequences seq, Provider<InternalChangeQuery> queryProvider, SchemaFactory<ReviewDb> schemaFactory, ChangeNotes.Factory notesFactory, AccountResolver accountResolver, CmdLineParser.Factory optionParserFactory, MergedSender.Factory mergedSenderFactory, GitReferenceUpdated gitRefUpdated, PatchSetInfoFactory patchSetInfoFactory, ChangeHooks hooks, ChangeMessagesUtil cmUtil, PatchSetUtil psUtil, ProjectCache projectCache, GitRepositoryManager repoManager, TagCache tagCache, AccountCache accountCache, ChangeCache changeCache, ChangesCollection changes, ChangeInserter.Factory changeInserterFactory, CommitValidators.Factory commitValidatorsFactory, @CanonicalWebUrl String canonicalWebUrl, @SendEmailExecutor ExecutorService sendEmailExecutor, @ChangeUpdateExecutor ListeningExecutorService changeUpdateExector, RequestScopePropagator requestScopePropagator, SshInfo sshInfo, AllProjectsName allProjectsName, ReceiveConfig receiveConfig, TransferConfig transferConfig, DynamicSet<ReceivePackInitializer> initializers, Provider<LazyPostReceiveHookChain> lazyPostReceive, @Assisted ProjectControl projectControl, @Assisted Repository repo, Provider<SubmoduleOp> subOpProvider, Provider<Submit> submitProvider, Provider<MergeOp> mergeOpProvider, Provider<MergeOpRepoManager> ormProvider, DynamicMap<ProjectConfigEntry> pluginConfigEntries, NotesMigration notesMigration, ChangeEditUtil editUtil, BatchUpdate.Factory batchUpdateFactory, SetHashtagsOp.Factory hashtagsFactory, ReplaceOp.Factory replaceOpFactory)
+DECL|method|ReceiveCommits (ReviewDb db, Sequences seq, Provider<InternalChangeQuery> queryProvider, SchemaFactory<ReviewDb> schemaFactory, ChangeNotes.Factory notesFactory, AccountResolver accountResolver, CmdLineParser.Factory optionParserFactory, MergedSender.Factory mergedSenderFactory, GitReferenceUpdated gitRefUpdated, PatchSetInfoFactory patchSetInfoFactory, ChangeHooks hooks, ChangeMessagesUtil cmUtil, PatchSetUtil psUtil, ProjectCache projectCache, GitRepositoryManager repoManager, TagCache tagCache, AccountCache accountCache, SearchingChangeCacheImpl changeCache, ChangesCollection changes, ChangeInserter.Factory changeInserterFactory, CommitValidators.Factory commitValidatorsFactory, @CanonicalWebUrl String canonicalWebUrl, @SendEmailExecutor ExecutorService sendEmailExecutor, @ChangeUpdateExecutor ListeningExecutorService changeUpdateExector, RequestScopePropagator requestScopePropagator, SshInfo sshInfo, AllProjectsName allProjectsName, ReceiveConfig receiveConfig, TransferConfig transferConfig, DynamicSet<ReceivePackInitializer> initializers, Provider<LazyPostReceiveHookChain> lazyPostReceive, @Assisted ProjectControl projectControl, @Assisted Repository repo, Provider<SubmoduleOp> subOpProvider, Provider<Submit> submitProvider, Provider<MergeOp> mergeOpProvider, Provider<MergeOpRepoManager> ormProvider, DynamicMap<ProjectConfigEntry> pluginConfigEntries, NotesMigration notesMigration, ChangeEditUtil editUtil, BatchUpdate.Factory batchUpdateFactory, SetHashtagsOp.Factory hashtagsFactory, ReplaceOp.Factory replaceOpFactory)
 name|ReceiveCommits
 parameter_list|(
 name|ReviewDb
@@ -3515,7 +3515,7 @@ parameter_list|,
 name|AccountCache
 name|accountCache
 parameter_list|,
-name|ChangeCache
+name|SearchingChangeCacheImpl
 name|changeCache
 parameter_list|,
 name|ChangesCollection
@@ -4168,6 +4168,8 @@ operator|new
 name|VisibleRefFilter
 argument_list|(
 name|tagCache
+argument_list|,
+name|notesFactory
 argument_list|,
 name|changeCache
 argument_list|,

gerrit-server/src/main/java/com/google/gerrit/server/project/ListTags.java
@@ -220,7 +220,7 @@ name|server
 operator|.
 name|git
 operator|.
-name|ChangeCache
+name|GitRepositoryManager
 import|;
 end_import
 
@@ -236,7 +236,7 @@ name|server
 operator|.
 name|git
 operator|.
-name|GitRepositoryManager
+name|SearchingChangeCacheImpl
 import|;
 end_import
 
@@ -269,6 +269,22 @@ operator|.
 name|git
 operator|.
 name|VisibleRefFilter
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+operator|.
+name|notedb
+operator|.
+name|ChangeNotes
 import|;
 end_import
 
@@ -526,10 +542,18 @@ specifier|final
 name|TagCache
 name|tagCache
 decl_stmt|;
+DECL|field|changeNotesFactory
+specifier|private
+specifier|final
+name|ChangeNotes
+operator|.
+name|Factory
+name|changeNotesFactory
+decl_stmt|;
 DECL|field|changeCache
 specifier|private
 specifier|final
-name|ChangeCache
+name|SearchingChangeCacheImpl
 name|changeCache
 decl_stmt|;
 annotation|@
@@ -702,7 +726,7 @@ name|matchRegex
 decl_stmt|;
 annotation|@
 name|Inject
-DECL|method|ListTags (GitRepositoryManager repoManager, Provider<ReviewDb> dbProvider, TagCache tagCache, ChangeCache changeCache)
+DECL|method|ListTags (GitRepositoryManager repoManager, Provider<ReviewDb> dbProvider, TagCache tagCache, ChangeNotes.Factory changeNotesFactory, SearchingChangeCacheImpl changeCache)
 specifier|public
 name|ListTags
 parameter_list|(
@@ -718,7 +742,12 @@ parameter_list|,
 name|TagCache
 name|tagCache
 parameter_list|,
-name|ChangeCache
+name|ChangeNotes
+operator|.
+name|Factory
+name|changeNotesFactory
+parameter_list|,
+name|SearchingChangeCacheImpl
 name|changeCache
 parameter_list|)
 block|{
@@ -739,6 +768,12 @@ operator|.
 name|tagCache
 operator|=
 name|tagCache
+expr_stmt|;
+name|this
+operator|.
+name|changeNotesFactory
+operator|=
+name|changeNotesFactory
 expr_stmt|;
 name|this
 operator|.
@@ -1138,6 +1173,8 @@ operator|new
 name|VisibleRefFilter
 argument_list|(
 name|tagCache
+argument_list|,
+name|changeNotesFactory
 argument_list|,
 name|changeCache
 argument_list|,

gerrit-server/src/main/java/com/google/gerrit/server/project/ProjectControl.java
@@ -438,7 +438,7 @@ name|server
 operator|.
 name|git
 operator|.
-name|ChangeCache
+name|GitRepositoryManager
 import|;
 end_import
 
@@ -454,7 +454,7 @@ name|server
 operator|.
 name|git
 operator|.
-name|GitRepositoryManager
+name|SearchingChangeCacheImpl
 import|;
 end_import
 
@@ -1192,6 +1192,14 @@ specifier|final
 name|GitRepositoryManager
 name|repoManager
 decl_stmt|;
+DECL|field|changeNotesFactory
+specifier|private
+specifier|final
+name|ChangeNotes
+operator|.
+name|Factory
+name|changeNotesFactory
+decl_stmt|;
 DECL|field|changeControlFactory
 specifier|private
 specifier|final
@@ -1226,7 +1234,7 @@ decl_stmt|;
 DECL|field|changeCache
 specifier|private
 specifier|final
-name|ChangeCache
+name|SearchingChangeCacheImpl
 name|changeCache
 decl_stmt|;
 DECL|field|allSections
@@ -1267,7 +1275,7 @@ name|declaredOwner
 decl_stmt|;
 annotation|@
 name|Inject
-DECL|method|ProjectControl (@itUploadPackGroups Set<AccountGroup.UUID> uploadGroups, @GitReceivePackGroups Set<AccountGroup.UUID> receiveGroups, ProjectCache pc, PermissionCollection.Factory permissionFilter, GitRepositoryManager repoManager, ChangeControl.Factory changeControlFactory, TagCache tagCache, ChangeCache changeCache, @CanonicalWebUrl @Nullable String canonicalWebUrl, @Assisted CurrentUser who, @Assisted ProjectState ps)
+DECL|method|ProjectControl (@itUploadPackGroups Set<AccountGroup.UUID> uploadGroups, @GitReceivePackGroups Set<AccountGroup.UUID> receiveGroups, ProjectCache pc, PermissionCollection.Factory permissionFilter, GitRepositoryManager repoManager, ChangeNotes.Factory changeNotesFactory, ChangeControl.Factory changeControlFactory, TagCache tagCache, SearchingChangeCacheImpl changeCache, @CanonicalWebUrl @Nullable String canonicalWebUrl, @Assisted CurrentUser who, @Assisted ProjectState ps)
 name|ProjectControl
 parameter_list|(
 annotation|@
@@ -1301,6 +1309,11 @@ parameter_list|,
 name|GitRepositoryManager
 name|repoManager
 parameter_list|,
+name|ChangeNotes
+operator|.
+name|Factory
+name|changeNotesFactory
+parameter_list|,
 name|ChangeControl
 operator|.
 name|Factory
@@ -1309,7 +1322,7 @@ parameter_list|,
 name|TagCache
 name|tagCache
 parameter_list|,
-name|ChangeCache
+name|SearchingChangeCacheImpl
 name|changeCache
 parameter_list|,
 annotation|@
@@ -1335,6 +1348,12 @@ operator|.
 name|repoManager
 operator|=
 name|repoManager
+expr_stmt|;
+name|this
+operator|.
+name|changeNotesFactory
+operator|=
+name|changeNotesFactory
 expr_stmt|;
 name|this
 operator|.
@@ -3070,6 +3089,8 @@ operator|new
 name|VisibleRefFilter
 argument_list|(
 name|tagCache
+argument_list|,
+name|changeNotesFactory
 argument_list|,
 name|changeCache
 argument_list|,

gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/LsUserRefs.java
@@ -222,7 +222,7 @@ name|server
 operator|.
 name|git
 operator|.
-name|ChangeCache
+name|GitRepositoryManager
 import|;
 end_import
 
@@ -238,7 +238,7 @@ name|server
 operator|.
 name|git
 operator|.
-name|GitRepositoryManager
+name|SearchingChangeCacheImpl
 import|;
 end_import
 
@@ -271,6 +271,22 @@ operator|.
 name|git
 operator|.
 name|VisibleRefFilter
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+operator|.
+name|notedb
+operator|.
+name|ChangeNotes
 import|;
 end_import
 
@@ -480,9 +496,18 @@ name|tagCache
 decl_stmt|;
 annotation|@
 name|Inject
+DECL|field|changeNotesFactory
+specifier|private
+name|ChangeNotes
+operator|.
+name|Factory
+name|changeNotesFactory
+decl_stmt|;
+annotation|@
+name|Inject
 DECL|field|changeCache
 specifier|private
-name|ChangeCache
+name|SearchingChangeCacheImpl
 name|changeCache
 decl_stmt|;
 annotation|@
@@ -687,6 +712,8 @@ operator|new
 name|VisibleRefFilter
 argument_list|(
 name|tagCache
+argument_list|,
+name|changeNotesFactory
 argument_list|,
 name|changeCache
 argument_list|,

gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/Upload.java
@@ -124,7 +124,7 @@ name|server
 operator|.
 name|git
 operator|.
-name|ChangeCache
+name|SearchingChangeCacheImpl
 import|;
 end_import
 
@@ -225,6 +225,22 @@ operator|.
 name|validators
 operator|.
 name|UploadValidators
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+operator|.
+name|notedb
+operator|.
+name|ChangeNotes
 import|;
 end_import
 
@@ -365,9 +381,18 @@ name|tagCache
 decl_stmt|;
 annotation|@
 name|Inject
+DECL|field|changeNotesFactory
+specifier|private
+name|ChangeNotes
+operator|.
+name|Factory
+name|changeNotesFactory
+decl_stmt|;
+annotation|@
+name|Inject
 DECL|field|changeCache
 specifier|private
-name|ChangeCache
+name|SearchingChangeCacheImpl
 name|changeCache
 decl_stmt|;
 annotation|@
@@ -452,6 +477,8 @@ operator|new
 name|VisibleRefFilter
 argument_list|(
 name|tagCache
+argument_list|,
+name|changeNotesFactory
 argument_list|,
 name|changeCache
 argument_list|,

gerrit-acceptance-framework/src/test/java/com/google/gerrit/acceptance/InProcessProtocol.java
@@ -304,22 +304,6 @@ name|server
 operator|.
 name|git
 operator|.
-name|ChangeCache
-import|;
-end_import
-
-begin_import
-import|import
-name|com
-operator|.
-name|google
-operator|.
-name|gerrit
-operator|.
-name|server
-operator|.
-name|git
-operator|.
 name|ReceiveCommits
 import|;
 end_import
@@ -337,6 +321,22 @@ operator|.
 name|git
 operator|.
 name|ReceivePackInitializer
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+operator|.
+name|git
+operator|.
+name|SearchingChangeCacheImpl
 import|;
 end_import
 
@@ -403,6 +403,22 @@ operator|.
 name|validators
 operator|.
 name|UploadValidators
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+operator|.
+name|notedb
+operator|.
+name|ChangeNotes
 import|;
 end_import
 
@@ -1530,7 +1546,7 @@ decl_stmt|;
 DECL|field|changeCache
 specifier|private
 specifier|final
-name|ChangeCache
+name|SearchingChangeCacheImpl
 name|changeCache
 decl_stmt|;
 DECL|field|projectControlFactory
@@ -1540,6 +1556,14 @@ name|ProjectControl
 operator|.
 name|GenericFactory
 name|projectControlFactory
+decl_stmt|;
+DECL|field|changeNotesFactory
+specifier|private
+specifier|final
+name|ChangeNotes
+operator|.
+name|Factory
+name|changeNotesFactory
 decl_stmt|;
 DECL|field|transferConfig
 specifier|private
@@ -1572,7 +1596,7 @@ name|threadContext
 decl_stmt|;
 annotation|@
 name|Inject
-DECL|method|Upload ( Provider<ReviewDb> dbProvider, Provider<CurrentUser> userProvider, TagCache tagCache, ChangeCache changeCache, ProjectControl.GenericFactory projectControlFactory, TransferConfig transferConfig, DynamicSet<PreUploadHook> preUploadHooks, UploadValidators.Factory uploadValidatorsFactory, ThreadLocalRequestContext threadContext)
+DECL|method|Upload ( Provider<ReviewDb> dbProvider, Provider<CurrentUser> userProvider, TagCache tagCache, SearchingChangeCacheImpl changeCache, ProjectControl.GenericFactory projectControlFactory, ChangeNotes.Factory changeNotesFactory, TransferConfig transferConfig, DynamicSet<PreUploadHook> preUploadHooks, UploadValidators.Factory uploadValidatorsFactory, ThreadLocalRequestContext threadContext)
 name|Upload
 parameter_list|(
 name|Provider
@@ -1590,13 +1614,18 @@ parameter_list|,
 name|TagCache
 name|tagCache
 parameter_list|,
-name|ChangeCache
+name|SearchingChangeCacheImpl
 name|changeCache
 parameter_list|,
 name|ProjectControl
 operator|.
 name|GenericFactory
 name|projectControlFactory
+parameter_list|,
+name|ChangeNotes
+operator|.
+name|Factory
+name|changeNotesFactory
 parameter_list|,
 name|TransferConfig
 name|transferConfig
@@ -1645,6 +1674,12 @@ operator|.
 name|projectControlFactory
 operator|=
 name|projectControlFactory
+expr_stmt|;
+name|this
+operator|.
+name|changeNotesFactory
+operator|=
+name|changeNotesFactory
 expr_stmt|;
 name|this
 operator|.
@@ -1777,6 +1812,8 @@ operator|new
 name|VisibleRefFilter
 argument_list|(
 name|tagCache
+argument_list|,
+name|changeNotesFactory
 argument_list|,
 name|changeCache
 argument_list|,

commit b941aa578962979191922a423efa144be5640dd4

Consistently annotate changeCache as @Nullable
The changeCache is null when in the context of BatchProgramModule.
The only consumer of changeCache, VisibleRefFilter, is prepared to
handle a null value, so in all code paths it is okay for it to be
null.

This makes reindex work again.  Regression introduced in
145c0844caedb95a55e617026bbef426a478f469 [formerly a758640bc33c20cead19b86a32db075c776c4140] (VisibleRefFilter: Avoid
touching the database for drafts, 2016-06-03):

 $ java -jar buck-out/gen/gerrit/gerrit.war reindex -d ../gerrit-testsite
[...]
 Reindexing changes: projects: 66% (2/3)[2016-06-06 16:18:36,781] [Index-Batch-3] WARN  com.google.gerrit.server.index.change.AllChangesIndexer : Failed to index change 18
 com.google.inject.ProvisionException: Unable to provision, see the following errors:

 1) null returned by binding at com.google.gerrit.pgm.util.BatchProgramModule.configure(BatchProgramModule.java:126)
  but parameter 8 of com.google.gerrit.server.project.ProjectControl.<init>() is not @Nullable
   while locating com.google.gerrit.server.git.SearchingChangeCacheImpl
     for parameter 8 at com.google.gerrit.server.project.ProjectControl.<init>(ProjectControl.java:177)
   while locating com.google.gerrit.server.project.ProjectControl annotated with @com.google.inject.internal.UniqueAnnotations$Internal(value=4)

Reported-by: Viktar Donich <viktard@google.com>
Change-Id: Ia0ca0e0e153c9f357e5ebc6edf4b1962a2490f2d

Former-commit-id: b273cfd04d277801910c2feb5b15971a8ed24da6

gerrit-httpd/src/main/java/com/google/gerrit/httpd/GitOverHttpServlet.java
@@ -102,6 +102,20 @@ name|gerrit
 operator|.
 name|common
 operator|.
+name|Nullable
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|common
+operator|.
 name|data
 operator|.
 name|Capable
@@ -1643,6 +1657,8 @@ name|Factory
 name|changeNotesFactory
 decl_stmt|;
 DECL|field|changeCache
+annotation|@
+name|Nullable
 specifier|private
 specifier|final
 name|SearchingChangeCacheImpl
@@ -1658,7 +1674,7 @@ name|uploadValidatorsFactory
 decl_stmt|;
 annotation|@
 name|Inject
-DECL|method|UploadFilter (Provider<ReviewDb> db, TagCache tagCache, ChangeNotes.Factory changeNotesFactory, SearchingChangeCacheImpl changeCache, UploadValidators.Factory uploadValidatorsFactory)
+DECL|method|UploadFilter (Provider<ReviewDb> db, TagCache tagCache, ChangeNotes.Factory changeNotesFactory, @Nullable SearchingChangeCacheImpl changeCache, UploadValidators.Factory uploadValidatorsFactory)
 name|UploadFilter
 parameter_list|(
 name|Provider
@@ -1675,6 +1691,8 @@ operator|.
 name|Factory
 name|changeNotesFactory
 parameter_list|,
+annotation|@
+name|Nullable
 name|SearchingChangeCacheImpl
 name|changeCache
 parameter_list|,

gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
@@ -3449,7 +3449,7 @@ name|batch
 decl_stmt|;
 annotation|@
 name|Inject
-DECL|method|ReceiveCommits (ReviewDb db, Sequences seq, Provider<InternalChangeQuery> queryProvider, SchemaFactory<ReviewDb> schemaFactory, ChangeNotes.Factory notesFactory, AccountResolver accountResolver, CmdLineParser.Factory optionParserFactory, MergedSender.Factory mergedSenderFactory, GitReferenceUpdated gitRefUpdated, PatchSetInfoFactory patchSetInfoFactory, ChangeHooks hooks, ChangeMessagesUtil cmUtil, PatchSetUtil psUtil, ProjectCache projectCache, GitRepositoryManager repoManager, TagCache tagCache, AccountCache accountCache, SearchingChangeCacheImpl changeCache, ChangesCollection changes, ChangeInserter.Factory changeInserterFactory, CommitValidators.Factory commitValidatorsFactory, @CanonicalWebUrl String canonicalWebUrl, @SendEmailExecutor ExecutorService sendEmailExecutor, @ChangeUpdateExecutor ListeningExecutorService changeUpdateExector, RequestScopePropagator requestScopePropagator, SshInfo sshInfo, AllProjectsName allProjectsName, ReceiveConfig receiveConfig, TransferConfig transferConfig, DynamicSet<ReceivePackInitializer> initializers, Provider<LazyPostReceiveHookChain> lazyPostReceive, @Assisted ProjectControl projectControl, @Assisted Repository repo, Provider<SubmoduleOp> subOpProvider, Provider<Submit> submitProvider, Provider<MergeOp> mergeOpProvider, Provider<MergeOpRepoManager> ormProvider, DynamicMap<ProjectConfigEntry> pluginConfigEntries, NotesMigration notesMigration, ChangeEditUtil editUtil, BatchUpdate.Factory batchUpdateFactory, SetHashtagsOp.Factory hashtagsFactory, ReplaceOp.Factory replaceOpFactory)
+DECL|method|ReceiveCommits (ReviewDb db, Sequences seq, Provider<InternalChangeQuery> queryProvider, SchemaFactory<ReviewDb> schemaFactory, ChangeNotes.Factory notesFactory, AccountResolver accountResolver, CmdLineParser.Factory optionParserFactory, MergedSender.Factory mergedSenderFactory, GitReferenceUpdated gitRefUpdated, PatchSetInfoFactory patchSetInfoFactory, ChangeHooks hooks, ChangeMessagesUtil cmUtil, PatchSetUtil psUtil, ProjectCache projectCache, GitRepositoryManager repoManager, TagCache tagCache, AccountCache accountCache, @Nullable SearchingChangeCacheImpl changeCache, ChangesCollection changes, ChangeInserter.Factory changeInserterFactory, CommitValidators.Factory commitValidatorsFactory, @CanonicalWebUrl String canonicalWebUrl, @SendEmailExecutor ExecutorService sendEmailExecutor, @ChangeUpdateExecutor ListeningExecutorService changeUpdateExector, RequestScopePropagator requestScopePropagator, SshInfo sshInfo, AllProjectsName allProjectsName, ReceiveConfig receiveConfig, TransferConfig transferConfig, DynamicSet<ReceivePackInitializer> initializers, Provider<LazyPostReceiveHookChain> lazyPostReceive, @Assisted ProjectControl projectControl, @Assisted Repository repo, Provider<SubmoduleOp> subOpProvider, Provider<Submit> submitProvider, Provider<MergeOp> mergeOpProvider, Provider<MergeOpRepoManager> ormProvider, DynamicMap<ProjectConfigEntry> pluginConfigEntries, NotesMigration notesMigration, ChangeEditUtil editUtil, BatchUpdate.Factory batchUpdateFactory, SetHashtagsOp.Factory hashtagsFactory, ReplaceOp.Factory replaceOpFactory)
 name|ReceiveCommits
 parameter_list|(
 name|ReviewDb
@@ -3515,6 +3515,8 @@ parameter_list|,
 name|AccountCache
 name|accountCache
 parameter_list|,
+annotation|@
+name|Nullable
 name|SearchingChangeCacheImpl
 name|changeCache
 parameter_list|,

gerrit-server/src/main/java/com/google/gerrit/server/project/ListTags.java
@@ -88,6 +88,20 @@ name|google
 operator|.
 name|gerrit
 operator|.
+name|common
+operator|.
+name|Nullable
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
 name|extensions
 operator|.
 name|api
@@ -551,6 +565,8 @@ name|Factory
 name|changeNotesFactory
 decl_stmt|;
 DECL|field|changeCache
+annotation|@
+name|Nullable
 specifier|private
 specifier|final
 name|SearchingChangeCacheImpl
@@ -726,7 +742,7 @@ name|matchRegex
 decl_stmt|;
 annotation|@
 name|Inject
-DECL|method|ListTags (GitRepositoryManager repoManager, Provider<ReviewDb> dbProvider, TagCache tagCache, ChangeNotes.Factory changeNotesFactory, SearchingChangeCacheImpl changeCache)
+DECL|method|ListTags (GitRepositoryManager repoManager, Provider<ReviewDb> dbProvider, TagCache tagCache, ChangeNotes.Factory changeNotesFactory, @Nullable SearchingChangeCacheImpl changeCache)
 specifier|public
 name|ListTags
 parameter_list|(
@@ -747,6 +763,8 @@ operator|.
 name|Factory
 name|changeNotesFactory
 parameter_list|,
+annotation|@
+name|Nullable
 name|SearchingChangeCacheImpl
 name|changeCache
 parameter_list|)

gerrit-server/src/main/java/com/google/gerrit/server/project/ProjectControl.java
@@ -1232,6 +1232,8 @@ name|TagCache
 name|tagCache
 decl_stmt|;
 DECL|field|changeCache
+annotation|@
+name|Nullable
 specifier|private
 specifier|final
 name|SearchingChangeCacheImpl
@@ -1275,7 +1277,7 @@ name|declaredOwner
 decl_stmt|;
 annotation|@
 name|Inject
-DECL|method|ProjectControl (@itUploadPackGroups Set<AccountGroup.UUID> uploadGroups, @GitReceivePackGroups Set<AccountGroup.UUID> receiveGroups, ProjectCache pc, PermissionCollection.Factory permissionFilter, GitRepositoryManager repoManager, ChangeNotes.Factory changeNotesFactory, ChangeControl.Factory changeControlFactory, TagCache tagCache, SearchingChangeCacheImpl changeCache, @CanonicalWebUrl @Nullable String canonicalWebUrl, @Assisted CurrentUser who, @Assisted ProjectState ps)
+DECL|method|ProjectControl (@itUploadPackGroups Set<AccountGroup.UUID> uploadGroups, @GitReceivePackGroups Set<AccountGroup.UUID> receiveGroups, ProjectCache pc, PermissionCollection.Factory permissionFilter, GitRepositoryManager repoManager, ChangeNotes.Factory changeNotesFactory, ChangeControl.Factory changeControlFactory, TagCache tagCache, @Nullable SearchingChangeCacheImpl changeCache, @CanonicalWebUrl @Nullable String canonicalWebUrl, @Assisted CurrentUser who, @Assisted ProjectState ps)
 name|ProjectControl
 parameter_list|(
 annotation|@
@@ -1322,6 +1324,8 @@ parameter_list|,
 name|TagCache
 name|tagCache
 parameter_list|,
+annotation|@
+name|Nullable
 name|SearchingChangeCacheImpl
 name|changeCache
 parameter_list|,

gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/LsUserRefs.java
@@ -110,6 +110,20 @@ name|gerrit
 operator|.
 name|common
 operator|.
+name|Nullable
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|common
+operator|.
 name|data
 operator|.
 name|GlobalCapability
@@ -505,6 +519,8 @@ name|changeNotesFactory
 decl_stmt|;
 annotation|@
 name|Inject
+annotation|@
+name|Nullable
 DECL|field|changeCache
 specifier|private
 name|SearchingChangeCacheImpl

gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/Upload.java
@@ -88,6 +88,20 @@ name|google
 operator|.
 name|gerrit
 operator|.
+name|common
+operator|.
+name|Nullable
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
 name|extensions
 operator|.
 name|registration
@@ -390,6 +404,8 @@ name|changeNotesFactory
 decl_stmt|;
 annotation|@
 name|Inject
+annotation|@
+name|Nullable
 DECL|field|changeCache
 specifier|private
 name|SearchingChangeCacheImpl

gerrit-acceptance-framework/src/test/java/com/google/gerrit/acceptance/InProcessProtocol.java
@@ -104,6 +104,20 @@ name|gerrit
 operator|.
 name|common
 operator|.
+name|Nullable
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|common
+operator|.
 name|data
 operator|.
 name|Capable
@@ -1544,6 +1558,8 @@ name|TagCache
 name|tagCache
 decl_stmt|;
 DECL|field|changeCache
+annotation|@
+name|Nullable
 specifier|private
 specifier|final
 name|SearchingChangeCacheImpl
@@ -1596,7 +1612,7 @@ name|threadContext
 decl_stmt|;
 annotation|@
 name|Inject
-DECL|method|Upload ( Provider<ReviewDb> dbProvider, Provider<CurrentUser> userProvider, TagCache tagCache, SearchingChangeCacheImpl changeCache, ProjectControl.GenericFactory projectControlFactory, ChangeNotes.Factory changeNotesFactory, TransferConfig transferConfig, DynamicSet<PreUploadHook> preUploadHooks, UploadValidators.Factory uploadValidatorsFactory, ThreadLocalRequestContext threadContext)
+DECL|method|Upload ( Provider<ReviewDb> dbProvider, Provider<CurrentUser> userProvider, TagCache tagCache, @Nullable SearchingChangeCacheImpl changeCache, ProjectControl.GenericFactory projectControlFactory, ChangeNotes.Factory changeNotesFactory, TransferConfig transferConfig, DynamicSet<PreUploadHook> preUploadHooks, UploadValidators.Factory uploadValidatorsFactory, ThreadLocalRequestContext threadContext)
 name|Upload
 parameter_list|(
 name|Provider
@@ -1614,6 +1630,8 @@ parameter_list|,
 name|TagCache
 name|tagCache
 parameter_list|,
+annotation|@
+name|Nullable
 name|SearchingChangeCacheImpl
 name|changeCache
 parameter_list|,

gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/git/VisibleRefFilterIT.java
@@ -166,6 +166,20 @@ name|gerrit
 operator|.
 name|common
 operator|.
+name|Nullable
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|common
+operator|.
 name|data
 operator|.
 name|AccessSection
@@ -584,6 +598,8 @@ name|projectControlFactory
 decl_stmt|;
 annotation|@
 name|Inject
+annotation|@
+name|Nullable
 DECL|field|changeCache
 specifier|private
 name|SearchingChangeCacheImpl