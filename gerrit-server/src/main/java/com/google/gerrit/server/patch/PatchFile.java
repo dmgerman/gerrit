471ea296a687e748850ca778b42ff377584029ca
Andrew Bonventre <andybons@chromium.org>
2016-09-03 22:44:06 -0400

    Specify UTF-8 instead of platform default charset
    Implicit use of the platform default charset can result in
    e.g. non-ASCII characters being silently replaced with '?'
    in many environments.
    
    Change-Id: Ibc2d9177f4a1638bcc9830eccba6012e41ebaabd
    
    Former-commit-id: 69ebca705242875a6d24db283c948707adbad55e

--- gerrit-server/src/main/java/com/google/gerrit/server/patch/PatchFile.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/patch/PatchFile.java
@@ -67,6 +67,20 @@ package|;
 end_package
 
 begin_import
+import|import static
+name|java
+operator|.
+name|nio
+operator|.
+name|charset
+operator|.
+name|StandardCharsets
+operator|.
+name|UTF_8
+import|;
+end_import
+
+begin_import
 import|import
 name|com
 operator|.
@@ -868,7 +882,9 @@ argument_list|(
 name|str
 operator|.
 name|getBytes
-argument_list|()
+argument_list|(
+name|UTF_8
+argument_list|)
 argument_list|)
 return|;
 block|}
