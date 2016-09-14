2ae17428142585c3f7be1f03df9817520dba52b5
David Pursehouse <dpursehouse@collab.net>
2016-09-14 17:05:26 +0200

    WebLinks: Prevent NPE when file history weblink is null
    Change-Id: Ib10ae08934bc517b05eb598da428b2085ef4d0b5
    
    Former-commit-id: 756cb5bac15b6576bdd7b7e011d5b0352d52769b

--- gerrit-server/src/main/java/com/google/gerrit/server/WebLinks.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/WebLinks.java
@@ -966,6 +966,17 @@ argument_list|,
 name|file
 argument_list|)
 decl_stmt|;
+if|if
+condition|(
+name|info
+operator|==
+literal|null
+condition|)
+block|{
+return|return
+literal|null
+return|;
+block|}
 name|WebLinkInfoCommon
 name|commonInfo
 init|=
