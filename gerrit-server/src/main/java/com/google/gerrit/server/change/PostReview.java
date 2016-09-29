dmgerman/gerrit

commit 71b1108399ec09c32cce7bf349813b7553f9014a

Don't compare commit message against auto-merge commit
When a merge commit is compared against its auto-merge commit, we want
to show the complete commit message as new in the diff screen (the
same as when the commit is compared against a parent commit).

Define a new class ComparisonType that encapsulates the information
about whether the comparison is done against a parent or against the
auto-merge.

If the comparison is done against a parent, include the parent number.
The parent number will be needed if we want to have magic files with
content that depends on the parent against which the comparison is
done. Include the parent number already now, so that we don't need to
invalidate the PatchListCache later once more.

Commenting on the auto-merge commit message is no longer possible.
Old comments that were done on the auto-merge commit message are
filtered out when comments are retrieved (because we don't return any
content to which they could be applied).

Change-Id: I8a75c32a2053f82f8a2f41e0d15747c7f50354c3
Signed-off-by: Edwin Kempin <ekempin@google.com>

Former-commit-id: c4ced8910f45d9bf7ad4d3044278dc6343c3d156

gerrit-server/src/main/java/com/google/gerrit/server/change/PostReview.java
@@ -2781,6 +2781,63 @@ argument_list|)
 argument_list|)
 throw|;
 block|}
+if|if
+condition|(
+name|Patch
+operator|.
+name|COMMIT_MSG
+operator|.
+name|equals
+argument_list|(
+name|path
+argument_list|)
+condition|)
+block|{
+for|for
+control|(
+name|CommentInput
+name|comment
+range|:
+name|ent
+operator|.
+name|getValue
+argument_list|()
+control|)
+block|{
+if|if
+condition|(
+name|comment
+operator|.
+name|side
+operator|==
+name|Side
+operator|.
+name|PARENT
+operator|&&
+name|comment
+operator|.
+name|parent
+operator|==
+literal|null
+condition|)
+block|{
+throw|throw
+operator|new
+name|BadRequestException
+argument_list|(
+name|String
+operator|.
+name|format
+argument_list|(
+literal|"cannot comment on %s on auto-merge"
+argument_list|,
+name|path
+argument_list|)
+argument_list|)
+throw|;
+block|}
+block|}
+block|}
 name|List
 argument_list|<
 name|CommentInput

gerrit-acceptance-framework/src/test/java/com/google/gerrit/acceptance/AbstractDaemonTest.java
@@ -3729,6 +3729,31 @@ parameter_list|)
 throws|throws
 name|Exception
 block|{
+return|return
+name|createMergeCommitChange
+argument_list|(
+name|ref
+argument_list|,
+literal|"foo"
+argument_list|)
+return|;
+block|}
+DECL|method|createMergeCommitChange (String ref, String file)
+specifier|protected
+name|PushOneCommit
+operator|.
+name|Result
+name|createMergeCommitChange
+parameter_list|(
+name|String
+name|ref
+parameter_list|,
+name|String
+name|file
+parameter_list|)
+throws|throws
+name|Exception
+block|{
 name|ObjectId
 name|initial
 init|=
@@ -3770,7 +3795,7 @@ name|ImmutableMap
 operator|.
 name|of
 argument_list|(
-literal|"foo"
+name|file
 argument_list|,
 literal|"foo-1"
 argument_list|,
@@ -3817,7 +3842,7 @@ name|ImmutableMap
 operator|.
 name|of
 argument_list|(
-literal|"foo"
+name|file
 argument_list|,
 literal|"foo-2"
 argument_list|,
@@ -3854,7 +3879,7 @@ name|ImmutableMap
 operator|.
 name|of
 argument_list|(
-literal|"foo"
+name|file
 argument_list|,
 literal|"foo-1"
 argument_list|,

gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/server/change/CommentsIT.java
@@ -350,6 +350,22 @@ name|extensions
 operator|.
 name|restapi
 operator|.
+name|BadRequestException
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|extensions
+operator|.
+name|restapi
+operator|.
 name|IdString
 import|;
 end_import
@@ -383,6 +399,22 @@ operator|.
 name|restapi
 operator|.
 name|TopLevelResource
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|reviewdb
+operator|.
+name|client
+operator|.
+name|Patch
 import|;
 end_import
 
@@ -1319,11 +1351,10 @@ range|:
 name|lines
 control|)
 block|{
-specifier|final
 name|String
 name|file
 init|=
-literal|"/COMMIT_MSG"
+literal|"foo"
 decl_stmt|;
 name|PushOneCommit
 operator|.
@@ -1333,6 +1364,8 @@ init|=
 name|createMergeCommitChange
 argument_list|(
 literal|"refs/for/master"
+argument_list|,
+name|file
 argument_list|)
 decl_stmt|;
 name|String
@@ -1520,6 +1553,302 @@ name|c4
 argument_list|)
 expr_stmt|;
 block|}
+comment|// for the commit message comments on the auto-merge are not possible
+for|for
+control|(
+name|Integer
+name|line
+range|:
+name|lines
+control|)
+block|{
+name|String
+name|file
+init|=
+name|Patch
+operator|.
+name|COMMIT_MSG
+decl_stmt|;
+name|PushOneCommit
+operator|.
+name|Result
+name|r
+init|=
+name|createMergeCommitChange
+argument_list|(
+literal|"refs/for/master"
+argument_list|)
+decl_stmt|;
+name|String
+name|changeId
+init|=
+name|r
+operator|.
+name|getChangeId
+argument_list|()
+decl_stmt|;
+name|String
+name|revId
+init|=
+name|r
+operator|.
+name|getCommit
+argument_list|()
+operator|.
+name|getName
+argument_list|()
+decl_stmt|;
+name|ReviewInput
+name|input
+init|=
+operator|new
+name|ReviewInput
+argument_list|()
+decl_stmt|;
+name|CommentInput
+name|c1
+init|=
+name|newComment
+argument_list|(
+name|file
+argument_list|,
+name|Side
+operator|.
+name|REVISION
+argument_list|,
+name|line
+argument_list|,
+literal|"ps-1"
+argument_list|)
+decl_stmt|;
+name|CommentInput
+name|c2
+init|=
+name|newCommentOnParent
+argument_list|(
+name|file
+argument_list|,
+literal|1
+argument_list|,
+name|line
+argument_list|,
+literal|"parent-1 of ps-1"
+argument_list|)
+decl_stmt|;
+name|CommentInput
+name|c3
+init|=
+name|newCommentOnParent
+argument_list|(
+name|file
+argument_list|,
+literal|2
+argument_list|,
+name|line
+argument_list|,
+literal|"parent-2 of ps-1"
+argument_list|)
+decl_stmt|;
+name|input
+operator|.
+name|comments
+operator|=
+operator|new
+name|HashMap
+argument_list|<>
+argument_list|()
+expr_stmt|;
+name|input
+operator|.
+name|comments
+operator|.
+name|put
+argument_list|(
+name|file
+argument_list|,
+name|ImmutableList
+operator|.
+name|of
+argument_list|(
+name|c1
+argument_list|,
+name|c2
+argument_list|,
+name|c3
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|revision
+argument_list|(
+name|r
+argument_list|)
+operator|.
+name|review
+argument_list|(
+name|input
+argument_list|)
+expr_stmt|;
+name|Map
+argument_list|<
+name|String
+argument_list|,
+name|List
+argument_list|<
+name|CommentInfo
+argument_list|>
+argument_list|>
+name|result
+init|=
+name|getPublishedComments
+argument_list|(
+name|changeId
+argument_list|,
+name|revId
+argument_list|)
+decl_stmt|;
+name|assertThat
+argument_list|(
+name|result
+argument_list|)
+operator|.
+name|isNotEmpty
+argument_list|()
+expr_stmt|;
+name|assertThat
+argument_list|(
+name|Lists
+operator|.
+name|transform
+argument_list|(
+name|result
+operator|.
+name|get
+argument_list|(
+name|file
+argument_list|)
+argument_list|,
+name|infoToInput
+argument_list|(
+name|file
+argument_list|)
+argument_list|)
+argument_list|)
+operator|.
+name|containsExactly
+argument_list|(
+name|c1
+argument_list|,
+name|c2
+argument_list|,
+name|c3
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+annotation|@
+name|Test
+DECL|method|postCommentOnCommitMessageOnAutoMerge ()
+specifier|public
+name|void
+name|postCommentOnCommitMessageOnAutoMerge
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|PushOneCommit
+operator|.
+name|Result
+name|r
+init|=
+name|createMergeCommitChange
+argument_list|(
+literal|"refs/for/master"
+argument_list|)
+decl_stmt|;
+name|ReviewInput
+name|input
+init|=
+operator|new
+name|ReviewInput
+argument_list|()
+decl_stmt|;
+name|CommentInput
+name|c
+init|=
+name|newComment
+argument_list|(
+name|Patch
+operator|.
+name|COMMIT_MSG
+argument_list|,
+name|Side
+operator|.
+name|PARENT
+argument_list|,
+literal|0
+argument_list|,
+literal|"comment on auto-merge"
+argument_list|)
+decl_stmt|;
+name|input
+operator|.
+name|comments
+operator|=
+operator|new
+name|HashMap
+argument_list|<>
+argument_list|()
+expr_stmt|;
+name|input
+operator|.
+name|comments
+operator|.
+name|put
+argument_list|(
+name|Patch
+operator|.
+name|COMMIT_MSG
+argument_list|,
+name|ImmutableList
+operator|.
+name|of
+argument_list|(
+name|c
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|exception
+operator|.
+name|expect
+argument_list|(
+name|BadRequestException
+operator|.
+name|class
+argument_list|)
+expr_stmt|;
+name|exception
+operator|.
+name|expectMessage
+argument_list|(
+literal|"cannot comment on "
+operator|+
+name|Patch
+operator|.
+name|COMMIT_MSG
+operator|+
+literal|" on auto-merge"
+argument_list|)
+expr_stmt|;
+name|revision
+argument_list|(
+name|r
+argument_list|)
+operator|.
+name|review
+argument_list|(
+name|input
+argument_list|)
+expr_stmt|;
 block|}
 annotation|@
 name|Test

gerrit-server/src/main/java/com/google/gerrit/server/PatchLineCommentsUtil.java
@@ -66,6 +66,20 @@ end_package
 
 begin_import
 import|import static
+name|java
+operator|.
+name|util
+operator|.
+name|stream
+operator|.
+name|Collectors
+operator|.
+name|toList
+import|;
+end_import
+
+begin_import
+import|import static
 name|com
 operator|.
 name|google
@@ -255,6 +269,22 @@ operator|.
 name|client
 operator|.
 name|Change
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|reviewdb
+operator|.
+name|client
+operator|.
+name|Patch
 import|;
 end_import
 
@@ -1721,6 +1751,8 @@ argument_list|()
 condition|)
 block|{
 return|return
+name|removeCommentsOnAncestorOfCommitMessage
+argument_list|(
 name|sort
 argument_list|(
 name|db
@@ -1736,9 +1768,12 @@ operator|.
 name|toList
 argument_list|()
 argument_list|)
+argument_list|)
 return|;
 block|}
 return|return
+name|removeCommentsOnAncestorOfCommitMessage
+argument_list|(
 name|commentsOnPatchSet
 argument_list|(
 name|notes
@@ -1753,6 +1788,67 @@ name|values
 argument_list|()
 argument_list|,
 name|psId
+argument_list|)
+argument_list|)
+return|;
+block|}
+comment|/**    * For the commit message the A side in a diff view is always empty when a    * comparison against an ancestor is done, so there can't be any comments on    * this ancestor. However earlier we showed the auto-merge commit message on    * side A when for a merge commit a comparison against the auto-merge was    * done. From that time there may still be comments on the auto-merge commit    * message and those we want to filter out.    */
+DECL|method|removeCommentsOnAncestorOfCommitMessage ( List<PatchLineComment> list)
+specifier|private
+name|List
+argument_list|<
+name|PatchLineComment
+argument_list|>
+name|removeCommentsOnAncestorOfCommitMessage
+parameter_list|(
+name|List
+argument_list|<
+name|PatchLineComment
+argument_list|>
+name|list
+parameter_list|)
+block|{
+return|return
+name|list
+operator|.
+name|stream
+argument_list|()
+operator|.
+name|filter
+argument_list|(
+name|c
+lambda|->
+name|c
+operator|.
+name|getSide
+argument_list|()
+operator|!=
+literal|0
+operator|||
+operator|!
+name|Patch
+operator|.
+name|COMMIT_MSG
+operator|.
+name|equals
+argument_list|(
+name|c
+operator|.
+name|getKey
+argument_list|()
+operator|.
+name|getParentKey
+argument_list|()
+operator|.
+name|get
+argument_list|()
+argument_list|)
+argument_list|)
+operator|.
+name|collect
+argument_list|(
+name|toList
+argument_list|()
 argument_list|)
 return|;
 block|}

gerrit-server/src/main/java/com/google/gerrit/server/patch/ComparisonType.java
@@ -1,0 +1,354 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright (C) 2016 The Android Open Source Project
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.server.patch
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+operator|.
+name|patch
+package|;
+end_package
+
+begin_import
+import|import static
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+operator|.
+name|ioutil
+operator|.
+name|BasicSerialization
+operator|.
+name|readVarInt32
+import|;
+end_import
+
+begin_import
+import|import static
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+operator|.
+name|ioutil
+operator|.
+name|BasicSerialization
+operator|.
+name|writeVarInt32
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|IOException
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|InputStream
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|OutputStream
+import|;
+end_import
+
+begin_class
+DECL|class|ComparisonType
+specifier|public
+class|class
+name|ComparisonType
+block|{
+comment|/** 1-based parent */
+DECL|field|parentNum
+specifier|private
+specifier|final
+name|Integer
+name|parentNum
+decl_stmt|;
+DECL|field|autoMerge
+specifier|private
+specifier|final
+name|boolean
+name|autoMerge
+decl_stmt|;
+DECL|method|againstOtherPatchSet ()
+specifier|static
+name|ComparisonType
+name|againstOtherPatchSet
+parameter_list|()
+block|{
+return|return
+operator|new
+name|ComparisonType
+argument_list|(
+literal|null
+argument_list|,
+literal|false
+argument_list|)
+return|;
+block|}
+DECL|method|againstParent (int parentNum)
+specifier|static
+name|ComparisonType
+name|againstParent
+parameter_list|(
+name|int
+name|parentNum
+parameter_list|)
+block|{
+return|return
+operator|new
+name|ComparisonType
+argument_list|(
+name|parentNum
+argument_list|,
+literal|false
+argument_list|)
+return|;
+block|}
+DECL|method|againstAutoMerge ()
+specifier|static
+name|ComparisonType
+name|againstAutoMerge
+parameter_list|()
+block|{
+return|return
+operator|new
+name|ComparisonType
+argument_list|(
+literal|null
+argument_list|,
+literal|true
+argument_list|)
+return|;
+block|}
+DECL|method|ComparisonType (Integer parentNum, boolean autoMerge)
+specifier|private
+name|ComparisonType
+parameter_list|(
+name|Integer
+name|parentNum
+parameter_list|,
+name|boolean
+name|autoMerge
+parameter_list|)
+block|{
+name|this
+operator|.
+name|parentNum
+operator|=
+name|parentNum
+expr_stmt|;
+name|this
+operator|.
+name|autoMerge
+operator|=
+name|autoMerge
+expr_stmt|;
+block|}
+DECL|method|isAgainstParentOrAutoMerge ()
+specifier|public
+name|boolean
+name|isAgainstParentOrAutoMerge
+parameter_list|()
+block|{
+return|return
+name|isAgainstParent
+argument_list|()
+operator|||
+name|isAgainstAutoMerge
+argument_list|()
+return|;
+block|}
+DECL|method|isAgainstParent ()
+specifier|public
+name|boolean
+name|isAgainstParent
+parameter_list|()
+block|{
+return|return
+name|parentNum
+operator|!=
+literal|null
+return|;
+block|}
+DECL|method|isAgainstAutoMerge ()
+specifier|public
+name|boolean
+name|isAgainstAutoMerge
+parameter_list|()
+block|{
+return|return
+name|autoMerge
+return|;
+block|}
+DECL|method|writeTo (OutputStream out)
+name|void
+name|writeTo
+parameter_list|(
+name|OutputStream
+name|out
+parameter_list|)
+throws|throws
+name|IOException
+block|{
+name|writeVarInt32
+argument_list|(
+name|out
+argument_list|,
+name|parentNum
+operator|!=
+literal|null
+condition|?
+name|parentNum
+else|:
+literal|0
+argument_list|)
+expr_stmt|;
+name|writeVarInt32
+argument_list|(
+name|out
+argument_list|,
+name|autoMerge
+condition|?
+literal|1
+else|:
+literal|0
+argument_list|)
+expr_stmt|;
+block|}
+DECL|method|readFrom (InputStream in)
+specifier|static
+name|ComparisonType
+name|readFrom
+parameter_list|(
+name|InputStream
+name|in
+parameter_list|)
+throws|throws
+name|IOException
+block|{
+name|int
+name|p
+init|=
+name|readVarInt32
+argument_list|(
+name|in
+argument_list|)
+decl_stmt|;
+name|Integer
+name|parentNum
+init|=
+name|p
+operator|>
+literal|0
+condition|?
+name|p
+else|:
+literal|null
+decl_stmt|;
+name|boolean
+name|autoMerge
+init|=
+name|readVarInt32
+argument_list|(
+name|in
+argument_list|)
+operator|!=
+literal|0
+decl_stmt|;
+return|return
+operator|new
+name|ComparisonType
+argument_list|(
+name|parentNum
+argument_list|,
+name|autoMerge
+argument_list|)
+return|;
+block|}
+block|}
+end_class
+
+end_unit
+

gerrit-server/src/main/java/com/google/gerrit/server/patch/PatchFile.java
@@ -424,7 +424,10 @@ if|if
 condition|(
 name|patchList
 operator|.
-name|isAgainstParent
+name|getComparisonType
+argument_list|()
+operator|.
+name|isAgainstParentOrAutoMerge
 argument_list|()
 condition|)
 block|{

gerrit-server/src/main/java/com/google/gerrit/server/patch/PatchList.java
@@ -492,11 +492,11 @@ specifier|transient
 name|ObjectId
 name|newId
 decl_stmt|;
-DECL|field|againstParent
+DECL|field|comparisonType
 specifier|private
 specifier|transient
-name|boolean
-name|againstParent
+name|ComparisonType
+name|comparisonType
 decl_stmt|;
 DECL|field|insertions
 specifier|private
@@ -517,25 +517,21 @@ name|PatchListEntry
 index|[]
 name|patches
 decl_stmt|;
-DECL|method|PatchList (@ullable final AnyObjectId oldId, final AnyObjectId newId, final boolean againstParent, final PatchListEntry[] patches)
+DECL|method|PatchList (@ullable AnyObjectId oldId, AnyObjectId newId, ComparisonType comparisonType, PatchListEntry[] patches)
 specifier|public
 name|PatchList
 parameter_list|(
 annotation|@
 name|Nullable
-specifier|final
 name|AnyObjectId
 name|oldId
 parameter_list|,
-specifier|final
 name|AnyObjectId
 name|newId
 parameter_list|,
-specifier|final
-name|boolean
-name|againstParent
+name|ComparisonType
+name|comparisonType
 parameter_list|,
-specifier|final
 name|PatchListEntry
 index|[]
 name|patches
@@ -567,9 +563,9 @@ argument_list|()
 expr_stmt|;
 name|this
 operator|.
-name|againstParent
+name|comparisonType
 operator|=
-name|againstParent
+name|comparisonType
 expr_stmt|;
 comment|// We assume index 0 contains the magic commit message entry.
 if|if
@@ -690,15 +686,15 @@ argument_list|)
 argument_list|)
 return|;
 block|}
-comment|/** @return true if {@link #getOldId} is {@link #getNewId}'s ancestor. */
-DECL|method|isAgainstParent ()
+comment|/** @return the comparison type */
+DECL|method|getComparisonType ()
 specifier|public
-name|boolean
-name|isAgainstParent
+name|ComparisonType
+name|getComparisonType
 parameter_list|()
 block|{
 return|return
-name|againstParent
+name|comparisonType
 return|;
 block|}
 comment|/** @return total number of new lines added. */
@@ -982,15 +978,11 @@ argument_list|,
 name|newId
 argument_list|)
 expr_stmt|;
-name|writeVarInt32
+name|comparisonType
+operator|.
+name|writeTo
 argument_list|(
 name|out
-argument_list|,
-name|againstParent
-condition|?
-literal|1
-else|:
-literal|0
 argument_list|)
 expr_stmt|;
 name|writeVarInt32
@@ -1095,14 +1087,14 @@ argument_list|(
 name|in
 argument_list|)
 expr_stmt|;
-name|againstParent
+name|comparisonType
 operator|=
-name|readVarInt32
+name|ComparisonType
+operator|.
+name|readFrom
 argument_list|(
 name|in
 argument_list|)
-operator|!=
-literal|0
 expr_stmt|;
 name|insertions
 operator|=

gerrit-server/src/main/java/com/google/gerrit/server/patch/PatchListKey.java
@@ -299,7 +299,7 @@ specifier|final
 name|long
 name|serialVersionUID
 init|=
-literal|22L
+literal|23L
 decl_stmt|;
 DECL|field|WHITESPACE_TYPES
 specifier|public

commit d86cfadae0b84fce57336d57ad70c28543db9709

PostReview: Avoid NPE sanity-checking /COMMIT_MSG comments
We first check

  if (Patch.isMagic(path)) { /* /COMMIT_MSG */
    for (T comment : ent.getValue()) {
      ...
    }
  }

and only afterwards check whether ent.getValue() is null.  This is
producing NullPointerException in production when ent.getValue() is
null.

It is not clear to me why we allow a null value here (what does it mean
to have a null comment list in a review?) but we allowed it before.
Move the null check so we can continue to allow it.

Regression introduced in 71b1108399ec09c32cce7bf349813b7553f9014a [formerly c4ced8910f45d9bf7ad4d3044278dc6343c3d156] (Don't
compare commit message against auto-merge commit, 2016-09-12).

Change-Id: I8768283ab7be8ebb1d06dea432ec49d02354691e

Former-commit-id: 5b8173715d419dc2ba60e613d20db888993f6593

gerrit-server/src/main/java/com/google/gerrit/server/change/PostReview.java
@@ -2926,6 +2926,31 @@ argument_list|)
 argument_list|)
 throw|;
 block|}
+name|List
+argument_list|<
+name|T
+argument_list|>
+name|list
+init|=
+name|ent
+operator|.
+name|getValue
+argument_list|()
+decl_stmt|;
+if|if
+condition|(
+name|list
+operator|==
+literal|null
+condition|)
+block|{
+name|mapItr
+operator|.
+name|remove
+argument_list|()
+expr_stmt|;
+continue|continue;
+block|}
 if|if
 condition|(
 name|Patch
@@ -2941,10 +2966,7 @@ control|(
 name|T
 name|comment
 range|:
-name|ent
-operator|.
-name|getValue
-argument_list|()
+name|list
 control|)
 block|{
 if|if
@@ -2980,31 +3002,6 @@ argument_list|)
 throw|;
 block|}
 block|}
-block|}
-name|List
-argument_list|<
-name|T
-argument_list|>
-name|list
-init|=
-name|ent
-operator|.
-name|getValue
-argument_list|()
-decl_stmt|;
-if|if
-condition|(
-name|list
-operator|==
-literal|null
-condition|)
-block|{
-name|mapItr
-operator|.
-name|remove
-argument_list|()
-expr_stmt|;
-continue|continue;
 block|}
 name|Iterator
 argument_list|<