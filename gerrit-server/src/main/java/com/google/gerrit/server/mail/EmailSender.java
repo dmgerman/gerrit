fc03cfcdfbb78fc0d6b8055e5faabb0e845a1b15
Wyatt Allen <wyatta@google.com>
2016-10-03 15:19:48 -0700

    Modify EmailSender interface to optionally accept HTML bodies
    Because some EmailSender implementations need to handle the HTML part of
    the message separately, the sender interface is modified to admit an
    additional body parameter. This new, expanded version of the send method
    signature is given a default method body so that existing
    implementations will continue to work (albeit plaintext-only).
    
    The implementation of SmtpEmailSender is modified to take advantage of
    the new signature.
    
    Bug: Issue 4589
    Change-Id: I748970aa1812a61394d6991bb4060191755eb8ad
    
    Former-commit-id: d82f352e5b99712317411e7eadc4812ff53a2311

--- gerrit-server/src/main/java/com/google/gerrit/server/mail/EmailSender.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/mail/EmailSender.java
@@ -76,6 +76,20 @@ name|gerrit
 operator|.
 name|common
 operator|.
+name|Nullable
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|common
+operator|.
 name|errors
 operator|.
 name|EmailException
@@ -126,7 +140,53 @@ name|String
 name|address
 parameter_list|)
 function_decl|;
-comment|/**    * Sends an email message.    *    * @param from who the message is from.    * @param rcpt one or more address where the message will be delivered to.    *        This list overrides any To or CC headers in {@code headers}.    * @param headers message headers.    * @param body text to appear in the body of the message.    * @throws EmailException the message cannot be sent.    */
+comment|/**    * Sends an email message. Messages always contain a text body, but messages    * can optionally include an additional HTML body. If both body types are    * present, {@code send} should construct a {@code multipart/alternative}    * message with an appropriately-selected boundary.    *    * @param from who the message is from.    * @param rcpt one or more address where the message will be delivered to.    *        This list overrides any To or CC headers in {@code headers}.    * @param headers message headers.    * @param textBody text to appear in the {@code text/plain} body of the    *        message.    * @param htmlBody optional HTML code to appear in the {@code text/html} body    *        of the message.    * @throws EmailException the message cannot be sent.    */
+DECL|method|send (Address from, Collection<Address> rcpt, Map<String, EmailHeader> headers, String textBody, @Nullable String htmlBody)
+specifier|default
+name|void
+name|send
+parameter_list|(
+name|Address
+name|from
+parameter_list|,
+name|Collection
+argument_list|<
+name|Address
+argument_list|>
+name|rcpt
+parameter_list|,
+name|Map
+argument_list|<
+name|String
+argument_list|,
+name|EmailHeader
+argument_list|>
+name|headers
+parameter_list|,
+name|String
+name|textBody
+parameter_list|,
+annotation|@
+name|Nullable
+name|String
+name|htmlBody
+parameter_list|)
+throws|throws
+name|EmailException
+block|{
+name|send
+argument_list|(
+name|from
+argument_list|,
+name|rcpt
+argument_list|,
+name|headers
+argument_list|,
+name|textBody
+argument_list|)
+expr_stmt|;
+block|}
+comment|/**    * Sends an email message with a text body only (i.e. not HTML or multipart).    *    * Authors of new implementations of this interface should not use this method    * to send a message because this method does not accept the HTML body.    * Instead, authors should use the above signature of {@code send}.    *    * This version of the method is preserved for support of legacy    * implementations.    *    * @param from who the message is from.    * @param rcpt one or more address where the message will be delivered to.    *        This list overrides any To or CC headers in {@code headers}.    * @param headers message headers.    * @param body text to appear in the body of the message.    * @throws EmailException the message cannot be sent.    */
 DECL|method|send (Address from, Collection<Address> rcpt, Map<String, EmailHeader> headers, String body)
 name|void
 name|send
