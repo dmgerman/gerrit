c7aac0462ea2e78048b398c6f4d2ff64be8c050b
Edwin Kempin <ekempin@google.com>
2016-10-13 15:13:56 +0200

    SetHashtagsOp: Fail with 405 Method Not Allowed if NoteDb is disabled
    So far we returned 400 Bad Request, but this response code should only
    be used if the request is malformed or if mandatory fields in the
    input are missing.
    
    Change-Id: Ic316eaae7ac0589b569124aa005bca3ecfb64e86
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    
    Former-commit-id: 25358fefc5cd52888d0cadb756540a9e6a26474a

--- gerrit-server/src/main/java/com/google/gerrit/server/change/SetHashtagsOp.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/SetHashtagsOp.java
@@ -230,6 +230,22 @@ name|google
 operator|.
 name|gerrit
 operator|.
+name|extensions
+operator|.
+name|restapi
+operator|.
+name|MethodNotAllowedException
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
 name|reviewdb
 operator|.
 name|client
@@ -686,6 +702,8 @@ name|AuthException
 throws|,
 name|BadRequestException
 throws|,
+name|MethodNotAllowedException
+throws|,
 name|OrmException
 throws|,
 name|IOException
@@ -701,7 +719,7 @@ condition|)
 block|{
 throw|throw
 operator|new
-name|BadRequestException
+name|MethodNotAllowedException
 argument_list|(
 literal|"Cannot add hashtags; NoteDb is disabled"
 argument_list|)
