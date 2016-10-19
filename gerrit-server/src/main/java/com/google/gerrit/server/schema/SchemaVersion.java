27f9ae0f023829e0cee8ed050c7a3265f17b1e05
Edwin Kempin <ekempin@google.com>
2016-10-19 10:44:40 +0200

    AllProjectsCreator: Allow admins and owners to create refs/meta/config
    Nowadays when a project is created in Gerrit it always has a
    refs/meta/config branch, but the refs/meta/config branch may be
    missing for imported projects. In this case it is good if the Gerrit
    administrators and the project owners can just create the missing
    refs/meta/config branch.
    
    Add a schema migration that grants the 'Create Reference' access right
    on refs/meta/config to all administrator groups and project owners in
    All-Projects.
    
    Also old projects may miss the refs/meta/config branch and having this
    'Create Reference' permission on refs/meta/config assigned makes it
    easy to add the missing refs/meta/config branch for these projects.
    
    Change-Id: I634bd21189196b479eb6d4b578d684bc9b54d49c
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    
    Former-commit-id: 541d9747d73922b105b7a59e53e99bd003ffa3a6

--- gerrit-server/src/main/java/com/google/gerrit/server/schema/SchemaVersion.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/SchemaVersion.java
@@ -258,11 +258,11 @@ specifier|static
 specifier|final
 name|Class
 argument_list|<
-name|Schema_134
+name|Schema_135
 argument_list|>
 name|C
 init|=
-name|Schema_134
+name|Schema_135
 operator|.
 name|class
 decl_stmt|;
