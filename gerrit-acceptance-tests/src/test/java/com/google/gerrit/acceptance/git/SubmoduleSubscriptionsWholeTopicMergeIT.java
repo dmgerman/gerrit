b79294ca3c35e530401ea2ca2252e883c61bd89d
Zhen Chen <czhen@google.com>
2016-10-30 21:40:56 -0700

    Fix eclipse unused variable warning
    Change-Id: I064974cdcb6278d45438f4eb600f3a3a369749a0
    
    Former-commit-id: 43eb04bf43c71ec52a0c9ebbddccc96a53ba9926

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/git/SubmoduleSubscriptionsWholeTopicMergeIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/git/SubmoduleSubscriptionsWholeTopicMergeIT.java
@@ -4312,16 +4312,13 @@ literal|"project-b"
 argument_list|)
 decl_stmt|;
 comment|// bootstrap the dev branch
-name|ObjectId
-name|a0
-init|=
 name|pushChangeTo
 argument_list|(
 name|repoA
 argument_list|,
 literal|"dev"
 argument_list|)
-decl_stmt|;
+expr_stmt|;
 comment|// bootstrap the dev branch
 name|ObjectId
 name|b0
