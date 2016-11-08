b68d21bfbe61287a34432ecabce4f4083aedfd65
Andrew Bonventre <andybons@chromium.org>
2016-11-08 09:57:21 -0800

    Fix implicit use of the platform default charset
    Otherwise it can result in e.g. non-ASCII characters being silently
    replaced with '?' in many environments.
    
    Change-Id: I62bdd126aabd4a14ef2ef4a01da9c906b8f080e1
    
    Former-commit-id: 1ebc20f535387580539660d36d39917a971a98e9

--- gerrit-server/src/main/java/com/google/gerrit/server/mail/receive/RawMailParser.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/mail/receive/RawMailParser.java
@@ -69,6 +69,20 @@ package|;
 end_package
 
 begin_import
+import|import static
+name|java
+operator|.
+name|nio
+operator|.
+name|charset
+operator|.
+name|StandardCharsets
+operator|.
+name|UTF_8
+import|;
+end_import
+
+begin_import
 import|import
 name|com
 operator|.
@@ -382,7 +396,9 @@ argument_list|(
 name|raw
 operator|.
 name|getBytes
-argument_list|()
+argument_list|(
+name|UTF_8
+argument_list|)
 argument_list|)
 argument_list|)
 expr_stmt|;
