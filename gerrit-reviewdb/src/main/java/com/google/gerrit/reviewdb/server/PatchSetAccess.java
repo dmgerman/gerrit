8b32e4db535e324e716ebdcbc8137265a2dbd09a
Dave Borowitz <dborowitz@google.com>
2016-12-22 16:41:31 -0500

    Add all() methods to more ReviewDb access classes
    Doesn't require a schema upgrade, as this is just more @Query methods,
    not even backed by an index.
    
    Change-Id: I47af680feada44d05f8014e1898e9e92f5f84269
    
    Former-commit-id: 62582a61dc2305a880b3abd408491d4552017400

--- gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/server/PatchSetAccess.java
+++ gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/server/PatchSetAccess.java
@@ -222,6 +222,18 @@ parameter_list|)
 throws|throws
 name|OrmException
 function_decl|;
+annotation|@
+name|Query
+DECL|method|all ()
+name|ResultSet
+argument_list|<
+name|PatchSet
+argument_list|>
+name|all
+parameter_list|()
+throws|throws
+name|OrmException
+function_decl|;
 block|}
 end_interface
 
--- gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/server/PatchSetApprovalAccess.java
+++ gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/server/PatchSetApprovalAccess.java
@@ -299,6 +299,18 @@ parameter_list|)
 throws|throws
 name|OrmException
 function_decl|;
+annotation|@
+name|Query
+DECL|method|all ()
+name|ResultSet
+argument_list|<
+name|PatchSetApproval
+argument_list|>
+name|all
+parameter_list|()
+throws|throws
+name|OrmException
+function_decl|;
 block|}
 end_interface
 
