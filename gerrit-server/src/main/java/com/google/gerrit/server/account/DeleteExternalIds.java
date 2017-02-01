483dacb2e754d913cbdb92013c41b5b2a6f77fa1
Edwin Kempin <ekempin@google.com>
2017-02-01 13:47:42 +0100

    Annotate DeleteExternalIds as singleton
    Change-Id: I9a55e3f908570a0cfc239b742ffaa051e5c09ecd
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    
    Former-commit-id: 19ac78e1064ba6a09db5e1e6de39cee0ae79504f

--- gerrit-server/src/main/java/com/google/gerrit/server/account/DeleteExternalIds.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/DeleteExternalIds.java
@@ -298,6 +298,18 @@ end_import
 
 begin_import
 import|import
+name|com
+operator|.
+name|google
+operator|.
+name|inject
+operator|.
+name|Singleton
+import|;
+end_import
+
+begin_import
+import|import
 name|java
 operator|.
 name|io
@@ -349,6 +361,8 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|Singleton
 DECL|class|DeleteExternalIds
 specifier|public
 class|class
