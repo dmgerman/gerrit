39ee2b3691bbae709a36529e9bd39953e646d711
Edwin Kempin <ekempin@google.com>
2017-02-08 13:38:15 +0100

    AccountApi: Add methods to get emails and delete email
    Also add a test for email deletion and add asserts for the addEmail()
    test.
    
    Bug: Issue 5385
    Change-Id: I3e192638fdaac58d0cdaa535430bb968d7d32abb
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    
    Former-commit-id: e1f9dd112006b0f202a06fff353a200f06ebe772

---
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/common/EmailInfo.java
@@ -0,0 +1,123 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright (C) 2017 The Android Open Source Project
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.extensions.common
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|extensions
+operator|.
+name|common
+package|;
+end_package
+
+begin_class
+DECL|class|EmailInfo
+specifier|public
+class|class
+name|EmailInfo
+block|{
+DECL|field|email
+specifier|public
+name|String
+name|email
+decl_stmt|;
+DECL|field|preferred
+specifier|public
+name|Boolean
+name|preferred
+decl_stmt|;
+DECL|field|pendingConfirmation
+specifier|public
+name|Boolean
+name|pendingConfirmation
+decl_stmt|;
+DECL|method|preferred (String e)
+specifier|public
+name|void
+name|preferred
+parameter_list|(
+name|String
+name|e
+parameter_list|)
+block|{
+name|this
+operator|.
+name|preferred
+operator|=
+name|e
+operator|!=
+literal|null
+operator|&&
+name|e
+operator|.
+name|equals
+argument_list|(
+name|email
+argument_list|)
+condition|?
+literal|true
+else|:
+literal|null
+expr_stmt|;
+block|}
+block|}
+end_class
+
+end_unit
+
