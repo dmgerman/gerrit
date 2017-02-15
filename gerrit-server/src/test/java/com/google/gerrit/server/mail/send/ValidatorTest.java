fbedd7f5d3721f5667ac2bfc40c8fb8d008deb5f
David Pursehouse <dpursehouse@collab.net>
2017-02-15 14:38:01 +0900

    ValidatorTest: Move to correct package
    The class under test, OutgoingEmailValidator, is in the package:
    
      com.google.gerrit.server.mail.send
    
    Move the test class to the same package.
    
    Change-Id: I2d071a692aaf5c2b2bc1f09dd68e64110a646554
    
    Former-commit-id: 341ad81d9c6f60636bd981fbe88518c40664c218

--- gerrit-server/src/test/java/com/google/gerrit/server/mail/send/ValidatorTest.java
+++ gerrit-server/src/test/java/com/google/gerrit/server/mail/send/ValidatorTest.java
@@ -52,7 +52,7 @@ comment|// limitations under the License.
 end_comment
 
 begin_package
-DECL|package|com.google.gerrit.server.mail
+DECL|package|com.google.gerrit.server.mail.send
 package|package
 name|com
 operator|.
@@ -63,6 +63,8 @@ operator|.
 name|server
 operator|.
 name|mail
+operator|.
+name|send
 package|;
 end_package
 
