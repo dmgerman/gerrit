aff24aa57aee758f075b082364c5747d838e400a
David Pursehouse <dpursehouse@collab.net>
2017-03-18 14:34:15 +0900

    Format Java files with google-java-format
    Change-Id: Ibb3f47b14e07d0ce898b6b88a64e860f0d2af9ac
    
    Former-commit-id: f0d6fc1117d1107d68829509f17814f74bfdcf7d

--- gerrit-acceptance-framework/src/test/java/com/google/gerrit/acceptance/AbstractDaemonTest.java
+++ gerrit-acceptance-framework/src/test/java/com/google/gerrit/acceptance/AbstractDaemonTest.java
@@ -4728,7 +4728,7 @@ argument_list|)
 argument_list|)
 return|;
 block|}
-DECL|method|amendChange (String changeId, String subject, String fileName, String content)
+DECL|method|amendChange ( String changeId, String subject, String fileName, String content)
 specifier|protected
 name|PushOneCommit
 operator|.
