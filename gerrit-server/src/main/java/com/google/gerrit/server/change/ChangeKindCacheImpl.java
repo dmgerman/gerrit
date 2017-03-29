dc1dc38611cca3e4cca9809263844570012fdb10
Paladox none <thomasmulhall410@yahoo.com>
2017-03-29 20:13:59 +0000

    Fix java.lang.ArrayIndexOutOfBoundsException when checking for parent
    Check to see if parent == 0 first.
    
    Bug: Issue 5866
    Change-Id: Id79fe8bdcf893592d28d475215cdeedcd92d271b
    
    Former-commit-id: fc9f79445ca30cfdee3a89ee6b0d8033f0c9e51a

--- gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeKindCacheImpl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeKindCacheImpl.java
@@ -1553,6 +1553,13 @@ literal|1
 operator|)
 operator|&&
 operator|(
+name|prior
+operator|.
+name|getParentCount
+argument_list|()
+operator|==
+literal|0
+operator|||
 operator|!
 name|onlyFirstParentChanged
 argument_list|(
@@ -1560,13 +1567,6 @@ name|prior
 argument_list|,
 name|next
 argument_list|)
-operator|||
-name|prior
-operator|.
-name|getParentCount
-argument_list|()
-operator|==
-literal|0
 operator|)
 condition|)
 block|{
