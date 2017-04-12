e72d6cc2f13c4c0e454491e84cbc75ed7ea49c69
David Pursehouse <dpursehouse@collab.net>
2017-04-12 12:57:19 +0900

    DynamicOptions: Remove redundant specification of type arguments
    Change-Id: Ic6b9d4d95618afc1e352289e275c47bd3a35cf0e
    
    Former-commit-id: be75deb9a39cd8b3c327d39a75a37e881044cf08

--- gerrit-server/src/main/java/com/google/gerrit/server/DynamicOptions.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/DynamicOptions.java
@@ -243,11 +243,7 @@ name|beansByPlugin
 operator|=
 operator|new
 name|HashMap
-argument_list|<
-name|String
-argument_list|,
-name|DynamicBean
-argument_list|>
+argument_list|<>
 argument_list|()
 expr_stmt|;
 for|for
