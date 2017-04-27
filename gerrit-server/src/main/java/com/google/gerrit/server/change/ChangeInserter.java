f21c8f2c59bfadc44675f6dc8ec9dab7adc5f53c
Logan Hanks <logan@google.com>
2017-04-27 11:43:14 +0200

    Add tag for patchsets uploaded with %wip
    The ChangeMessage produced when a patchset is added to a change (or
    when a new change is created) needs to have a tag that indicates
    that the change is being moved into the WIP state.
    
    Change-Id: I07806e1ffb6bcf98ef829d3454ff99c5877c3365
    
    Former-commit-id: 8e2284841bd17a0e3945cc438f44c73672175478

--- gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeInserter.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeInserter.java
@@ -2793,7 +2793,10 @@ name|message
 argument_list|,
 name|ChangeMessagesUtil
 operator|.
-name|TAG_UPLOADED_PATCH_SET
+name|uploadedPatchSetTag
+argument_list|(
+name|workInProgress
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|cmUtil
