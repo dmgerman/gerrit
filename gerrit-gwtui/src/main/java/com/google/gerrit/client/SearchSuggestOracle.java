a2356e23c82e6127394bedc1b188a61e6228511f
David Pursehouse <dpursehouse@collab.net>
2017-04-27 17:16:48 +0200

    Allow to search for ignored changes by "is:ignored"
    Searching by "is:ignored" is the same as "star:ignore" but is a bit
    more intuitive for users.
    
    Change-Id: I63f41662ad1023c98fa746a892d5bfa050303879
    
    Former-commit-id: 2b49892fc15820cfe3efd4016f9a9203fb402d59

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/SearchSuggestOracle.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/SearchSuggestOracle.java
@@ -806,6 +806,13 @@ name|suggestions
 operator|.
 name|add
 argument_list|(
+literal|"is:ignored"
+argument_list|)
+expr_stmt|;
+name|suggestions
+operator|.
+name|add
+argument_list|(
 literal|"status:"
 argument_list|)
 expr_stmt|;
