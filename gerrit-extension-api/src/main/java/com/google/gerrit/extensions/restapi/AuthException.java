fe3a07617184367f4b80fb432a123400147fc352
Shawn Pearce <sop@google.com>
2017-04-28 15:07:32 +0200

    Migrate Move to PermissionBackend
    Change-Id: Ice0bc9099f997e7c3c7d8cde03dd8434fa6c7e70
    
    Former-commit-id: f537319c22a70f415d190b85035bae2c14a97702

--- gerrit-extension-api/src/main/java/com/google/gerrit/extensions/restapi/AuthException.java
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/restapi/AuthException.java
@@ -102,6 +102,26 @@ name|msg
 argument_list|)
 expr_stmt|;
 block|}
+comment|/**    * @param msg message to return to the client.    * @param cause cause of this exception.    */
+DECL|method|AuthException (String msg, Throwable cause)
+specifier|public
+name|AuthException
+parameter_list|(
+name|String
+name|msg
+parameter_list|,
+name|Throwable
+name|cause
+parameter_list|)
+block|{
+name|super
+argument_list|(
+name|msg
+argument_list|,
+name|cause
+argument_list|)
+expr_stmt|;
+block|}
 block|}
 end_class
 
