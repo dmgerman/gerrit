4dd5ab5b7d3633ee62a2a190870ac9ab1c0d8067
Alice Kober-Sotzek <aliceks@google.com>
2017-05-19 16:04:54 +0200

    Prevent corruption of IntraLineDiffCache
    Values for the cache are synchronously computed but stored
    asynchronously. There is a small chance that the list of edits is
    modified before it is written to the cache. By making deep defensive
    copies, we can prevent this corruption.
    
    Change-Id: Ie7b44ae31483358b44ad968b316be66f831a9e26
    
    Former-commit-id: 6a0bfb7da6665de121dafdc592e75ae24aafbb3c

--- gerrit-server/src/main/java/com/google/gerrit/server/patch/IntraLineDiffKey.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/patch/IntraLineDiffKey.java
@@ -140,7 +140,7 @@ specifier|final
 name|long
 name|serialVersionUID
 init|=
-literal|5L
+literal|6L
 decl_stmt|;
 DECL|method|create (ObjectId aId, ObjectId bId, Whitespace whitespace)
 specifier|public
