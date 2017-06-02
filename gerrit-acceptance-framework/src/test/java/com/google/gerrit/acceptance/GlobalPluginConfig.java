fa855df9723a5078030408881ef0a35f38083fe1
David Pursehouse <dpursehouse@collab.net>
2017-06-02 16:05:35 +0900

    Introduce GlobalPluginConfig{s} annotations
    GlobalPluginConfig and GlobalPluginConfigs can be used to annotate
    test methods in plugin tests, to provide test configuration values
    in $site/etc/plugin-name.config.
    
    The annotations work in the same way as existing GerritConfig{s}, with
    addition of "pluginName" parameter.
    
    A limitation of these annotations is that they must be used together
    with @UseLocalDisk, so that the plugin configuration files may be
    written in the site's etc folder.
    
    Change-Id: I11a66eac87bec5f849858d1a4f38d3f01976ff47
    
    Former-commit-id: 52e337f818c92887737bb67e7ee33426a1ec42d4

---
+++ gerrit-acceptance-framework/src/test/java/com/google/gerrit/acceptance/GlobalPluginConfig.java
@@ -0,0 +1,189 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright (C) 2017 The Android Open Source Project
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.acceptance
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|acceptance
+package|;
+end_package
+
+begin_import
+import|import static
+name|java
+operator|.
+name|lang
+operator|.
+name|annotation
+operator|.
+name|ElementType
+operator|.
+name|METHOD
+import|;
+end_import
+
+begin_import
+import|import static
+name|java
+operator|.
+name|lang
+operator|.
+name|annotation
+operator|.
+name|RetentionPolicy
+operator|.
+name|RUNTIME
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|lang
+operator|.
+name|annotation
+operator|.
+name|Repeatable
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|lang
+operator|.
+name|annotation
+operator|.
+name|Retention
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|lang
+operator|.
+name|annotation
+operator|.
+name|Target
+import|;
+end_import
+
+begin_annotation_defn
+annotation|@
+name|Target
+argument_list|(
+block|{
+name|METHOD
+block|}
+argument_list|)
+annotation|@
+name|Retention
+argument_list|(
+name|RUNTIME
+argument_list|)
+annotation|@
+name|Repeatable
+argument_list|(
+name|GlobalPluginConfigs
+operator|.
+name|class
+argument_list|)
+DECL|annotation|GlobalPluginConfig
+specifier|public
+annotation_defn|@interface
+name|GlobalPluginConfig
+block|{
+comment|/**    * Name of the plugin, corresponding to {@code $site/etc/@pluginName.comfig}.    */
+DECL|method|pluginName ()
+name|String
+name|pluginName
+parameter_list|()
+function_decl|;
+comment|/**    * @see GerritConfig#name()    */
+DECL|method|name ()
+name|String
+name|name
+parameter_list|()
+function_decl|;
+comment|/**    * @see GerritConfig#value()    */
+DECL|method|value ()
+name|String
+name|value
+parameter_list|()
+default|default
+literal|""
+function_decl|;
+comment|/**    * @see GerritConfig#values()    */
+DECL|method|values ()
+name|String
+index|[]
+name|values
+parameter_list|()
+default|default
+literal|""
+function_decl|;
+block|}
+end_annotation_defn
+
+end_unit
+
