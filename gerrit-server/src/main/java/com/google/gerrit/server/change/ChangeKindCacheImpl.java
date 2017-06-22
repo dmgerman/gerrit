865fab0c4091dabf848235833ee22d51c260ffaf
David Pursehouse <dpursehouse@collab.net>
2017-06-22 02:18:31 +0000

    Revert "Fix java.lang.ArrayIndexOutOfBoundsException when checking for parent"
    Doesn't fix the problem it was supposed to fix (issue 5866).
    
    This reverts commit 9d40c8f3ce851c04cab66aefc8cbea1938b1d749 [formerly c74e28aea54f71bc56dda16754714a714e2869ac].
    
    Change-Id: I34e55f9c1d08fd5babf8ec3e1debf32b72abbc2b
    
    Former-commit-id: 4f57082c7b8a795e8b49ea5a34cbbfe7fac85604

--- gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeKindCacheImpl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeKindCacheImpl.java
@@ -1503,13 +1503,6 @@ literal|1
 operator|)
 operator|&&
 operator|(
-name|prior
-operator|.
-name|getParentCount
-argument_list|()
-operator|==
-literal|0
-operator|||
 operator|!
 name|onlyFirstParentChanged
 argument_list|(
@@ -1517,6 +1510,13 @@ name|prior
 argument_list|,
 name|next
 argument_list|)
+operator|||
+name|prior
+operator|.
+name|getParentCount
+argument_list|()
+operator|==
+literal|0
 operator|)
 condition|)
 block|{
