c74d3b263f4027bae4a9589fa81218eb44ed2c68
Zac Livingston <zacl@codeaurora.org>
2017-07-11 17:02:48 -0600

    Add @Options annotation
    Support using @Option annotations from other objects during command
    line parsing so that options can be shared by different classes
    using composition instead of inheritance.
    
    Change-Id: I949ae8387847057301e22bd2b2be77d1dc3c25aa
    
    Former-commit-id: 2511dd96227ee44d7e1598407fab5d2a81eac2d4

---
+++ gerrit-util-cli/src/main/java/com/google/gerrit/util/cli/Options.java
@@ -0,0 +1,154 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright (C) 2017 The Android Open Source Project
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.util.cli
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|util
+operator|.
+name|cli
+package|;
+end_package
+
+begin_import
+import|import static
+name|java
+operator|.
+name|lang
+operator|.
+name|annotation
+operator|.
+name|ElementType
+operator|.
+name|FIELD
+import|;
+end_import
+
+begin_import
+import|import static
+name|java
+operator|.
+name|lang
+operator|.
+name|annotation
+operator|.
+name|RetentionPolicy
+operator|.
+name|RUNTIME
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|lang
+operator|.
+name|annotation
+operator|.
+name|Retention
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|lang
+operator|.
+name|annotation
+operator|.
+name|Target
+import|;
+end_import
+
+begin_comment
+comment|/**  * Marks a field that refers to a class with @Option annotations  *  *<p>Any @Option annotations found on the referred class will be handled as if they were found on  * the referring class.  */
+end_comment
+
+begin_annotation_defn
+annotation|@
+name|Retention
+argument_list|(
+name|RUNTIME
+argument_list|)
+annotation|@
+name|Target
+argument_list|(
+block|{
+name|FIELD
+block|}
+argument_list|)
+DECL|annotation|Options
+specifier|public
+annotation_defn|@interface
+name|Options
+block|{
+DECL|method|prefix ()
+name|String
+name|prefix
+parameter_list|()
+default|default
+literal|""
+function_decl|;
+block|}
+end_annotation_defn
+
+end_unit
+
