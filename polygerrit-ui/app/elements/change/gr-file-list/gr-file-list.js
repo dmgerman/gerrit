dmgerman/gerrit

commit fd664628aca038d7ddb80672be8bff49ccca8ada

add r key shortcut to toggle review flag
This adds support to the PG UI for the review toggle like we had
in the old UI.

Bug: Issue 6784
Change-Id: Ib44c46d17333c2a48fc647593c377f9c0bbc6314

polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.js
@@ -136,6 +136,7 @@
       'o': '_handleOKey',
       'n': '_handleNKey',
       'p': '_handlePKey',
+      'r': '_handleRKey',
       'shift+a': '_handleCapitalAKey',
       'esc': '_handleEscKey',
     },
@@ -579,6 +580,17 @@
       } else {
         this.$.diffCursor.moveToPreviousChunk();
       }
+    },
+
+    _handleRKey(e) {
+      if (this.shouldSuppressKeyboardShortcut(e) ||
+          this.modifierPressed(e) && !this.isModifierPressed(e, 'shiftKey')) {
+        return;
+      }
+
+      e.preventDefault();
+      if (!this._files[this.$.fileCursor.index]) { return; }
+      this._reviewFile(this._files[this.$.fileCursor.index].__path);
     },
 
     _handleCapitalAKey(e) {

polygerrit-ui/app/elements/change/gr-file-list/gr-file-list_test.html
@@ -385,6 +385,21 @@ limitations under the License.
         }
       });
 
+      test('r key toggles reviewed flag', () => {
+        flushAsynchronousOperations();
+
+        // Default state should be unreviewed.
+        assert.equal(element._reviewed.length, 0);
+
+        // Press the review key to toggle it (set the flag).
+        MockInteractions.pressAndReleaseKeyOn(element, 82, null, 'r');
+        assert.equal(element._reviewed.length, 1);
+
+        // Press the review key to toggle it (clear the flag).
+        MockInteractions.pressAndReleaseKeyOn(element, 82, null, 'r');
+        assert.equal(element._reviewed.length, 0);
+      });
+
       suite('_handleOKey', () => {
         let interact;
 

polygerrit-ui/app/elements/core/gr-keyboard-shortcuts-dialog/gr-keyboard-shortcuts-dialog.html
@@ -235,6 +235,10 @@ limitations under the License.
             <td>Show selected file</td>
           </tr>
           <tr>
+            <td><span class="key">r</span></td>
+            <td>Toggle review flag on selected file</td>
+          </tr>
+          <tr>
             <td>
               <span class="key modifier">Shift</span>
               <span class="key">i</span>

commit 73fcbb752c7fbdbcca53d5c8652a621f2c96838c

Fix bug in r-key handling
R-key capturing by file list should return if ever there is a modifier
pressed. In this case, it was capturing cmd+shift+r.

Bug introduced by fd66462.

Bug: Issue 7291
Change-Id: I46deda841ea5b500678166ff0e842cc64fdc308c

Former-commit-id: e8a77e2bc3e98284d53c2a2251ef3c295c871a5a

polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.js
@@ -612,8 +612,7 @@
     },
 
     _handleRKey(e) {
-      if (this.shouldSuppressKeyboardShortcut(e) ||
-          this.modifierPressed(e) && !this.isModifierPressed(e, 'shiftKey')) {
+      if (this.shouldSuppressKeyboardShortcut(e) || this.modifierPressed(e)) {
         return;
       }
 