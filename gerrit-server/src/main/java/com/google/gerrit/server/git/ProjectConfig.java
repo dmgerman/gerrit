39fdb276d49099bc488226848fa8770ddfd9f394
Sven Selberg <svense@axis.com>
2017-10-27 15:39:00 +0200

    ProjectConfig: Move public getExtensionPanelSections after ctor
    Change-Id: I86973104d4be379b257fb06d16c9eb8e1f275e79
    
    Former-commit-id: d5ae26c467fd700a746453334de75a84419478a4

--- gerrit-server/src/main/java/com/google/gerrit/server/git/ProjectConfig.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/ProjectConfig.java
@@ -1843,24 +1843,6 @@ return|return
 name|r
 return|;
 block|}
-DECL|method|getExtensionPanelSections ()
-specifier|public
-name|Map
-argument_list|<
-name|String
-argument_list|,
-name|List
-argument_list|<
-name|String
-argument_list|>
-argument_list|>
-name|getExtensionPanelSections
-parameter_list|()
-block|{
-return|return
-name|extensionPanelSections
-return|;
-block|}
 DECL|method|buildCommentLink (Config cfg, String name, boolean allowRaw)
 specifier|public
 specifier|static
@@ -2121,6 +2103,24 @@ parameter_list|()
 block|{
 return|return
 name|accountsSection
+return|;
+block|}
+DECL|method|getExtensionPanelSections ()
+specifier|public
+name|Map
+argument_list|<
+name|String
+argument_list|,
+name|List
+argument_list|<
+name|String
+argument_list|>
+argument_list|>
+name|getExtensionPanelSections
+parameter_list|()
+block|{
+return|return
+name|extensionPanelSections
 return|;
 block|}
 DECL|method|getAccessSection (String name)
