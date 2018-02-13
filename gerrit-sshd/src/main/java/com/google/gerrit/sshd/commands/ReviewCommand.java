3116d1b0981532f4024ed52d03bbc2008e2495ed
Borui Tao <borui.tao@ericsson.com>
2018-02-13 08:04:32 -0500

    Revert "SSH commands: Set task name for ReviewCommand"
    This reverts commit 026474122cacf5a0323c55f4ee44091240806c5d [formerly 9dd0e0855aa53ba46834c94102dd0de772da7f70].
    
    The change was created to remove the unreadable multi line logs in
    error_log for the review command by setting the task name(thread name)
    to "gerrit review" instead of the command line. Because task name is
    also used in the output of show-queue command, that change is also
    affecting the output of show-queue for review tasks.
    
    Getting rid of all the arguments is removing information that could be
    useful, example which change the review command is for. This is not
    really an issue for the thread name in error_log but it could become one
    for the task name is output of show-queue command.
    
    Instead of removing all the arguments of the review command, the
    proposal is to revert that change and merge the following one which
    keeps all the arguments but trims the multi line ones.
    
    Change-Id: I046951a42ef6271d3f960086e1acef9f4b643d5c
    
    Former-commit-id: 85f4551fbabc73221677e4b28b3716bb1de7df8f

--- gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/ReviewCommand.java
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/ReviewCommand.java
@@ -1651,18 +1651,6 @@ argument_list|)
 throw|;
 block|}
 block|}
-annotation|@
-name|Override
-DECL|method|getTaskDescription ()
-specifier|protected
-name|String
-name|getTaskDescription
-parameter_list|()
-block|{
-return|return
-literal|"gerrit review"
-return|;
-block|}
 DECL|method|applyReview (PatchSet patchSet, final ReviewInput review)
 specifier|private
 name|void
