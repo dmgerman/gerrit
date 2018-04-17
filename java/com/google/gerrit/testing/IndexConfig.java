c928a45a5d3a8873272c6562eebeff5250d33d17
Changcheng Xiao <xchangcheng@google.com>
2018-04-17 08:56:31 +0200

    Move IndexConfig to "gerrit/testing" package
    This is a utility class for those query classes, it's better to move
    it out from the test package ("javatests/..."). Also "gerrit/testing"
    looks like the best destination.
    
    Change-Id: Ida9aa179318ba2da7decac356bf7e0bc21fd52c6
    
    Former-commit-id: 98ec6ed50d64b26569c54e52b099d7a7eca41e77

--- java/com/google/gerrit/testing/IndexConfig.java
+++ java/com/google/gerrit/testing/IndexConfig.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|// Copyright (C) 2017 The Android Open Source Project
+comment|// Copyright (C) 2018 The Android Open Source Project
 end_comment
 
 begin_comment
@@ -52,7 +52,7 @@ comment|// limitations under the License.
 end_comment
 
 begin_package
-DECL|package|com.google.gerrit.server.query
+DECL|package|com.google.gerrit.testing
 package|package
 name|com
 operator|.
@@ -60,9 +60,7 @@ name|google
 operator|.
 name|gerrit
 operator|.
-name|server
-operator|.
-name|query
+name|testing
 package|;
 end_package
 
