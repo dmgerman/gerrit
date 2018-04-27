558880715de3fb54eb2c2c988ffcfcdad5354bd3
Marco Miller <marco.miller@ericsson.com>
2018-04-27 17:23:46 -0400

    Elasticsearch: replace native API in prod w/ REST
    Remove jest API which depends on Elasticsearch native API (also
    removed), so no longer depend on lucene for this scope. Replace such
    removed dependencies with the Elasticsearch low-level API, which does
    not depend on Lucene. That now used API is a REST client with minimal
    dependencies, indeed. -As opposed to Elastic's current high-level API,
    which still depends on lucene and other Gerrit-constraining libraries.
    
    Do so in order to decouple the elasticsearch client from lucene in
    Gerrit. That REST client does not logically require Lucene anyway. Most
    importantly, such decoupling now enables upcoming support for more than
    just one Elasticsearch server version. This includes the new possibility
    of bumping the latter to multiple yet later versions, such as 5 and 6.x.
    The currently used version (2.4) should also still be kept, for a while.
    
    Bumping such versions will likely require some Elasticsearch client code
    adaptations, so that Gerrit can (dynamically?) switch between either
    Elasticsearch server version to eventually support (hopefully soon).
    
    Doing the same for the elasticsearch tests in Gerrit is to be done using
    another change or more changes.
    
    Add a partial and customized fork of [1], based on [1]'s commit [2], to
    preserve the ability of building proper json requests for Elasticsearch.
    Put that forked json-generating code under a new 'builders' sub-package.
    There should be a possibility in some near future to consider removing
    that fork, based on potential progress such as the one proposed in [3].
    Meanwhile, this fork shall be maintained to usual Gerrit quality levels.
    
    [1] https://github.com/elastic/elasticsearch
    [2] tag: v2.4.4
    [3] https://github.com/elastic/elasticsearch/issues/30791
    
    Bug: Issue 6094
    Change-Id: I720c9885c9eab2388acc328eecb9eaa6940ced0c
    
    Former-commit-id: 9b4b09ee61701914dbf64949a98c6d7a442ba7fa

---
+++ gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/builders/QuerySourceBuilder.java
@@ -0,0 +1,137 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright (C) 2018 The Android Open Source Project, 2009-2015 Elasticsearch
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.elasticsearch.builders
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|elasticsearch
+operator|.
+name|builders
+package|;
+end_package
+
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|IOException
+import|;
+end_import
+
+begin_comment
+comment|/**  * A trimmed down and further altered version of {@link  * org.elasticsearch.action.support.QuerySourceBuilder} for this very package.  */
+end_comment
+
+begin_class
+DECL|class|QuerySourceBuilder
+class|class
+name|QuerySourceBuilder
+block|{
+DECL|field|queryBuilder
+specifier|private
+specifier|final
+name|QueryBuilder
+name|queryBuilder
+decl_stmt|;
+DECL|method|QuerySourceBuilder (QueryBuilder queryBuilder)
+name|QuerySourceBuilder
+parameter_list|(
+name|QueryBuilder
+name|queryBuilder
+parameter_list|)
+block|{
+name|this
+operator|.
+name|queryBuilder
+operator|=
+name|queryBuilder
+expr_stmt|;
+block|}
+DECL|method|innerToXContent (XContentBuilder builder)
+name|void
+name|innerToXContent
+parameter_list|(
+name|XContentBuilder
+name|builder
+parameter_list|)
+throws|throws
+name|IOException
+block|{
+name|builder
+operator|.
+name|field
+argument_list|(
+literal|"query"
+argument_list|)
+expr_stmt|;
+name|queryBuilder
+operator|.
+name|toXContent
+argument_list|(
+name|builder
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+end_class
+
+end_unit
+
