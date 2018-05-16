2e5add1b0a00e0d49d878fc31ea805afeadda77f
Edwin Kempin <ekempin@google.com>
2018-05-16 14:49:03 +0200

    GitwebServlet: Log unexpected errors on error level
    Especially it's seems odd that copyStderrToLog logs the input on error
    level but failing to do so results only in a debug log.
    
    Change-Id: I52302be34e8f4a62639015acffe26d0a11a5c8da
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    
    Former-commit-id: 510dc555903874e01dab97581cf14ad7c198a417

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/gitweb/GitwebServlet.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/gitweb/GitwebServlet.java
@@ -4004,7 +4004,7 @@ parameter_list|)
 block|{
 name|log
 operator|.
-name|debug
+name|error
 argument_list|(
 literal|"Unexpected error copying input to CGI"
 argument_list|,
@@ -4094,7 +4094,7 @@ parameter_list|)
 block|{
 name|log
 operator|.
-name|debug
+name|error
 argument_list|(
 literal|"Unexpected error copying stderr from CGI"
 argument_list|,
