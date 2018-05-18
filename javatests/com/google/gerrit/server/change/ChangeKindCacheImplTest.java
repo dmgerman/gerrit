a8299214c98a02c98bc67f30b767600031efa708
Dave Borowitz <dborowitz@google.com>
2018-05-18 09:10:17 -0400

    Use ProtoTruth#assertThat in more places
    Change-Id: I7b5d6a5abdaf2c51fbbf03668b71a60208c9ca6e
    
    Former-commit-id: 17df4f66a225a5b683f738f3f129f0fa49212659

--- javatests/com/google/gerrit/server/change/ChangeKindCacheImplTest.java
+++ javatests/com/google/gerrit/server/change/ChangeKindCacheImplTest.java
@@ -88,6 +88,26 @@ name|com
 operator|.
 name|google
 operator|.
+name|common
+operator|.
+name|truth
+operator|.
+name|extensions
+operator|.
+name|proto
+operator|.
+name|ProtoTruth
+operator|.
+name|assertThat
+import|;
+end_import
+
+begin_import
+import|import static
+name|com
+operator|.
+name|google
+operator|.
 name|gerrit
 operator|.
 name|server
