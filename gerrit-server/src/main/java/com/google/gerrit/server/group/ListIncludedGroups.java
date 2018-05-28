3dd58118c6ee31bcb46f487dc0a5261cd1585a6e
David Pursehouse <dpursehouse@collab.net>
2018-05-28 11:44:32 +0900

    Consistently import org.slf4j.LoggerFactory
    Change-Id: Iae7b3f6a6471b0e633840607d3a1cec412ef95e8
    
    Former-commit-id: 8f589c93cf8a7e8280e7d1843a41b4df51309696

--- gerrit-server/src/main/java/com/google/gerrit/server/group/ListIncludedGroups.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/group/ListIncludedGroups.java
@@ -294,6 +294,16 @@ name|Logger
 import|;
 end_import
 
+begin_import
+import|import
+name|org
+operator|.
+name|slf4j
+operator|.
+name|LoggerFactory
+import|;
+end_import
+
 begin_class
 annotation|@
 name|Singleton
@@ -314,10 +324,6 @@ specifier|final
 name|Logger
 name|log
 init|=
-name|org
-operator|.
-name|slf4j
-operator|.
 name|LoggerFactory
 operator|.
 name|getLogger
