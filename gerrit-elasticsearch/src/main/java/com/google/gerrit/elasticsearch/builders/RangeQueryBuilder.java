ace6e70de348d5f048f71bf8df0828f75d00e986
David Pursehouse <dpursehouse@collab.net>
2018-06-02 17:06:56 +0900

    Elasticsearch: Tidy up Javadoc in builders package
    Change-Id: I9022793db78be47cb028eb015872e2fb1445e7c2
    
    Former-commit-id: 33ad7eef1331feb1ff5cfe84531928ab8d1ebbc7

--- gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/builders/RangeQueryBuilder.java
+++ gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/builders/RangeQueryBuilder.java
@@ -77,7 +77,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * A Query that matches documents within an range of terms. A trimmed down version of  * org.elasticsearch.index.query.RangeQueryBuilder for this very package.  */
+comment|/**  * A Query that matches documents within an range of terms.  *  *<p>A trimmed down version of org.elasticsearch.index.query.RangeQueryBuilder.  */
 end_comment
 
 begin_class
