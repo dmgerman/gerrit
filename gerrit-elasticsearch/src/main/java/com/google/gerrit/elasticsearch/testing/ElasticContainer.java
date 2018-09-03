12d956e0868fe1772e306104cca60bf19ff7e447
David Pursehouse <dpursehouse@collab.net>
2018-09-03 16:43:55 +0900

    ElasticContainer: Test against Elasticsearch version 5.6.11
    Version 5.6.11 is the latest on the 5.6.x series, and according
    to the release notes [1] there are no breaking changes since the
    previous release (5.6.10).
    
    [1] https://www.elastic.co/guide/en/elasticsearch/reference/5.6/release-notes-5.6.11.html
    
    Change-Id: I5341ae0fce3da52f5496dfc090a10302f244edda
    
    Former-commit-id: 87bb52bcaede9da0e3d88d44b38731ab273eff13

--- gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/testing/ElasticContainer.java
+++ gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/testing/ElasticContainer.java
@@ -272,7 +272,7 @@ case|case
 name|V5_6
 case|:
 return|return
-literal|"docker.elastic.co/elasticsearch/elasticsearch:5.6.10"
+literal|"docker.elastic.co/elasticsearch/elasticsearch:5.6.11"
 return|;
 case|case
 name|V6_2
