dmgerman/gerrit

commit 2b4476a82c42365375c2048575f4ab6cb51cc974

Revert "PolyGerrit: Respect register URL when provided by auth scheme"
This reverts commit e36486918697526cb08616154b4b93030cb616f5.

Reason for revert: Issue 9770

Change-Id: I1c1730cae3ea809a60243786c02f89d1386b35a1

polygerrit-ui/app/elements/core/gr-main-header/gr-main-header.js
@@ -62,13 +62,6 @@
     },
   ];
 
-  // Set of authentication methods that can provide custom registration page.
-  const AUTH_TYPES_WITH_REGISTER_URL = new Set([
-    'LDAP',
-    'LDAP_BIND',
-    'CUSTOM_EXTENSION',
-  ]);
-
   Polymer({
     is: 'gr-main-header',
 
@@ -122,14 +115,6 @@
       _topMenus: {
         type: Array,
         value() { return []; },
-      },
-      _registerText: {
-        type: String,
-        value: 'Sign up',
-      },
-      _registerURL: {
-        type: String,
-        value: null,
       },
     },
 
@@ -250,10 +235,7 @@
 
     _loadConfig() {
       this.$.restAPI.getConfig()
-          .then(config => {
-            this._retrieveRegisterURL(config);
-            this.getDocsBaseUrl(config, this.$.restAPI);
-          })
+          .then(config => this.getDocsBaseUrl(config, this.$.restAPI))
           .then(docBaseUrl => { this._docBaseUrl = docBaseUrl; });
     },
 
@@ -264,19 +246,6 @@
         this._userLinks =
             prefs.my.map(this._fixCustomMenuItem).filter(this._isSupportedLink);
       });
-    },
-
-    _retrieveRegisterURL(config) {
-      if (AUTH_TYPES_WITH_REGISTER_URL.has(config.auth.auth_type)) {
-        this._registerURL = config.auth.register_url;
-        if (config.auth.register_text) {
-          this._registerText = config.auth.register_text;
-        }
-      }
-    },
-
-    _computeIsInvisible(registerURL) {
-      return registerURL ? '' : 'invisible';
     },
 
     _fixCustomMenuItem(linkObj) {

polygerrit-ui/app/elements/core/gr-main-header/gr-main-header.html
@@ -112,14 +112,12 @@ limitations under the License.
         margin: 5px 4px;
         text-decoration: none;
       }
-      .invisible,
       .settingsButton,
       gr-account-dropdown {
         display: none;
       }
       :host([loading]) .accountContainer,
-      :host([logged-in]) .loginButton,
-      :host([logged-in]) .registerButton {
+      :host([logged-in]) .loginButton {
         display: none;
       }
       :host([logged-in]) .settingsButton,
@@ -137,7 +135,7 @@ limitations under the License.
         text-overflow: ellipsis;
         white-space: nowrap;
       }
-      .loginButton, .registerButton {
+      .loginButton {
         color: var(--header-text-color);
         padding: .5em 1em;
       }
@@ -196,13 +194,6 @@ limitations under the License.
             class="hideOnMobile"
             name="header-browse-source"></gr-endpoint-decorator>
         <div class="accountContainer" id="accountContainer">
-          <div class$="[[_computeIsInvisible(_registerURL)]]">
-            <a
-                class="registerButton"
-                href$="[[_registerURL]]">
-              [[_registerText]]
-            </a>
-          </div>
           <a class="loginButton" href$="[[_loginURL]]">Sign in</a>
           <a
               class="settingsButton"

polygerrit-ui/app/elements/core/gr-main-header/gr-main-header_test.html
@@ -63,16 +63,12 @@ limitations under the License.
           'none');
       assert.notEqual(getComputedStyle(element.$$('.loginButton')).display,
           'none');
-      assert.notEqual(getComputedStyle(element.$$('.registerButton')).display,
-          'none');
       assert.equal(getComputedStyle(element.$$('gr-account-dropdown')).display,
           'none');
       assert.equal(getComputedStyle(element.$$('.settingsButton')).display,
           'none');
       element.loggedIn = true;
       assert.equal(getComputedStyle(element.$$('.loginButton')).display,
-          'none');
-      assert.equal(getComputedStyle(element.$$('.registerButton')).display,
           'none');
       assert.notEqual(getComputedStyle(element.$$('gr-account-dropdown'))
           .display,
@@ -189,35 +185,6 @@ limitations under the License.
           url: 'https://gerrit/plugins/plugin-manager/static/index.html',
         }],
       }]);
-    });
-
-    test('register URL', () => {
-      const config = {
-        auth: {
-          auth_type: 'LDAP',
-          register_url: 'https//gerrit.example.com/register',
-        },
-      };
-      element._retrieveRegisterURL(config);
-      assert.equal(element._registerURL, config.auth.register_url);
-      assert.equal(element._registerText, 'Sign up');
-
-      config.auth.register_text = 'Create account';
-      element._retrieveRegisterURL(config);
-      assert.equal(element._registerURL, config.auth.register_url);
-      assert.equal(element._registerText, config.auth.register_text);
-    });
-
-    test('register URL ignored for wrong auth type', () => {
-      const config = {
-        auth: {
-          auth_type: 'OPENID',
-          register_url: 'https//gerrit.example.com/register',
-        },
-      };
-      element._retrieveRegisterURL(config);
-      assert.equal(element._registerURL, null);
-      assert.equal(element._registerText, 'Sign up');
     });
   });
 </script>

commit 0c8a08859b7b153a4d5608f32cf140631573f7bc

Add missing return statement
Fixes the bug introduced in 2b4476a. An arrow function was changed from
a simple one-line with implicit return to one with a complex body (no
implicit return).

Bug: Issue 9770
Change-Id: I04dd4de0250719fed2bff826f3a8b521a428523c

Former-commit-id: f8237b956f88bf49e8b31fc3dae6e431da8fea0d

polygerrit-ui/app/elements/core/gr-main-header/gr-main-header.js
@@ -252,7 +252,7 @@
       this.$.restAPI.getConfig()
           .then(config => {
             this._retrieveRegisterURL(config);
-            this.getDocsBaseUrl(config, this.$.restAPI);
+            return this.getDocsBaseUrl(config, this.$.restAPI);
           })
           .then(docBaseUrl => { this._docBaseUrl = docBaseUrl; });
     },