101bb85c9347e7c450b50a90f6bafa5abb7dab3f
David Pursehouse <dpursehouse@collab.net>
2018-10-09 12:49:21 +0900

    AccountResolver: Minor improvements in Javadoc
    Change-Id: I081c2404e75d8cdcaf27ccda173f15ea2cd3511c
    
    Former-commit-id: 87251536add3af3a266d10d44834540326502d4a

--- java/com/google/gerrit/server/account/AccountResolver.java
+++ java/com/google/gerrit/server/account/AccountResolver.java
@@ -420,7 +420,7 @@ operator|=
 name|emails
 expr_stmt|;
 block|}
-comment|/**    * Locate exactly one account matching the input string.    *    * @param input a string of the format "Full Name&lt;email@example&gt;", just the email address    *     ("email@example"), a full name ("Full Name"), an account id ("18419") or an user name    *     ("username").    * @return the single account that matches; null if no account matches or there are multiple    *     candidates. If {@code input} is a numeric string, returns an account if and only if that    *     number corresponds to an actual account.    */
+comment|/**    * Locate exactly one account matching the input string.    *    * @param input a string of the format "Full Name&lt;email@example&gt;", just the email address    *     ("email@example"), a full name ("Full Name"), an account ID ("18419") or a user name    *     ("username").    * @return the single account that matches; null if no account matches or there are multiple    *     candidates. If {@code input} is a numeric string, returns an account if and only if that    *     number corresponds to an actual account ID.    */
 DECL|method|find (String input)
 specifier|public
 name|Account
@@ -564,7 +564,7 @@ return|return
 name|match
 return|;
 block|}
-comment|/**    * Find all accounts matching the input string.    *    * @param input a string of the format "Full Name&lt;email@example&gt;", just the email address    *     ("email@example"), a full name ("Full Name"), an account id ("18419") or an user name    *     ("username").    * @return the accounts that match, empty set if none. Never null. If {@code input} is a numeric    *     string, returns a singleton set if that number corresponds to a real account, and an empty    *     set otherwise if it does not.    */
+comment|/**    * Find all accounts matching the input string.    *    * @param input a string of the format "Full Name&lt;email@example&gt;", just the email address    *     ("email@example"), a full name ("Full Name"), an account ID ("18419") or a user name    *     ("username").    * @return the accounts that match, empty set if none. Never null. If {@code input} is a numeric    *     string, returns a singleton set if that number corresponds to a real account ID, and an    *     empty set otherwise if it does not.    */
 DECL|method|findAll (String input)
 specifier|public
 name|Set
