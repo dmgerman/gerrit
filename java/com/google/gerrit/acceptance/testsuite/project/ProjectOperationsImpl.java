e594b012ee0dab80b8aa8d6e70f71cd5f6ce6f42
David Pursehouse <dpursehouse@collab.net>
2018-11-21 08:57:14 +0900

    ProjectOperationsImpl: Use Inject from com.google.inject instead of javax.inject
    Change-Id: I9454c6a2aa7206e372c0ed51cae89e4cfff612db
    
    Former-commit-id: b1726f8869dd6f96febb9e8a37a882350796249b

--- java/com/google/gerrit/acceptance/testsuite/project/ProjectOperationsImpl.java
+++ java/com/google/gerrit/acceptance/testsuite/project/ProjectOperationsImpl.java
@@ -154,6 +154,18 @@ end_import
 
 begin_import
 import|import
+name|com
+operator|.
+name|google
+operator|.
+name|inject
+operator|.
+name|Inject
+import|;
+end_import
+
+begin_import
+import|import
 name|java
 operator|.
 name|util
@@ -169,16 +181,6 @@ operator|.
 name|util
 operator|.
 name|Collections
-import|;
-end_import
-
-begin_import
-import|import
-name|javax
-operator|.
-name|inject
-operator|.
-name|Inject
 import|;
 end_import
 
