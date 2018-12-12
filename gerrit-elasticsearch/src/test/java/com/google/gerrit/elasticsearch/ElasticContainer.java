48d53738e7eedb72960ac6301433ee82b1f9ba81
David Pursehouse <dpursehouse@collab.net>
2018-12-12 18:51:38 +0900

    ElasticContainer: Use 6.5.3 for V6_5 tests
    Change-Id: Ia42794ec7fb4db7c77e0be9a3b9d7224ab7394bd
    
    Former-commit-id: 368419410bb54fb8b5fac5d5f84f26a880948b56

--- gerrit-elasticsearch/src/test/java/com/google/gerrit/elasticsearch/ElasticContainer.java
+++ gerrit-elasticsearch/src/test/java/com/google/gerrit/elasticsearch/ElasticContainer.java
@@ -213,7 +213,7 @@ case|case
 name|V6_5
 case|:
 return|return
-literal|"docker.elastic.co/elasticsearch/elasticsearch-oss:6.5.2"
+literal|"docker.elastic.co/elasticsearch/elasticsearch-oss:6.5.3"
 return|;
 case|case
 name|V7_0
