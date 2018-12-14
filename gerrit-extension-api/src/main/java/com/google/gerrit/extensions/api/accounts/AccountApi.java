09a8a6ae199a231901b35de2aab2800a9051da80
David Pursehouse <dpursehouse@collab.net>
2018-12-14 11:40:26 +0900

    AccountApi: Add method to set account name
    Change-Id: Ice3756a71b877adc461ca6ec296e407051e5cd0e
    
    Former-commit-id: 2c2bd89cb66f36e9661e0e1a576fdb511a897dec

--- gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/accounts/AccountApi.java
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/accounts/AccountApi.java
@@ -702,6 +702,16 @@ parameter_list|)
 throws|throws
 name|RestApiException
 function_decl|;
+DECL|method|setName (String name)
+name|void
+name|setName
+parameter_list|(
+name|String
+name|name
+parameter_list|)
+throws|throws
+name|RestApiException
+function_decl|;
 comment|/**    * A default implementation which allows source compatibility when adding new methods to the    * interface.    */
 DECL|class|NotImplemented
 class|class
@@ -1361,6 +1371,25 @@ argument_list|<
 name|String
 argument_list|>
 name|externalIds
+parameter_list|)
+throws|throws
+name|RestApiException
+block|{
+throw|throw
+operator|new
+name|NotImplementedException
+argument_list|()
+throw|;
+block|}
+annotation|@
+name|Override
+DECL|method|setName (String name)
+specifier|public
+name|void
+name|setName
+parameter_list|(
+name|String
+name|name
 parameter_list|)
 throws|throws
 name|RestApiException
