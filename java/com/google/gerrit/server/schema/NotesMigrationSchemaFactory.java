635f8c4469e9a85d43015b16fa16f3af62e2d785
Dave Borowitz <dborowitz@google.com>
2018-12-14 15:13:11 -0800

    Remove outdated NotesMigrationSchemaFactory comments
    Change-Id: I3d19cfdb41f87514bca59cf5c31644e093058c58
    
    Former-commit-id: 63a175e4810df59949d3dbacae2b82a42f83e8c7

--- java/com/google/gerrit/server/schema/NotesMigrationSchemaFactory.java
+++ java/com/google/gerrit/server/schema/NotesMigrationSchemaFactory.java
@@ -179,32 +179,8 @@ parameter_list|()
 throws|throws
 name|OrmException
 block|{
-comment|// There are two levels at which this class disables access to Changes and related tables,
-comment|// corresponding to two phases of the NoteDb migration:
-comment|//
-comment|// 1. When changes are read from NoteDb but some changes might still have their primary storage
-comment|//    in ReviewDb, it is generally programmer error to read changes from ReviewDb. However,
-comment|//    since ReviewDb is still the primary storage for most or all changes, we still need to
-comment|//    support writing to ReviewDb. This behavior is accomplished by wrapping in a
-comment|//    DisallowedReviewDb.
-comment|//
-comment|//    Some codepaths might need to be able to read from ReviewDb if they really need to,
-comment|//    because they need to operate on the underlying source of truth, for example when reading
-comment|//    a change to determine its primary storage. To support this, ReviewDbUtil#unwrapDb can
-comment|//    detect and unwrap databases of this type.
-comment|//
-comment|// 2. After all changes have their primary storage in NoteDb, we can completely shut off access
-comment|//    to the change tables. At this point in the migration, we are by definition not using the
-comment|//    ReviewDb tables at all; we could even delete the tables at this point, and Gerrit would
-comment|//    continue to function.
-comment|//
-comment|//    This is accomplished by setting the delegate ReviewDb *underneath*
-comment|//    DisallowReadFromChanges to be a complete no-op, with NoChangesReviewDb. With this
-comment|//    stub implementation, all read operations return no results, and write operations silently
-comment|//    do nothing. This implementation is not a public class and callers couldn't do anything
-comment|//    useful with it even if it were.
-comment|// Disable writes to change tables in ReviewDb (ReviewDb access for changes are No-Ops); all
-comment|// other table accesses throw runtime exceptions.
+comment|// TODO(dborowitz): This class is purely vestigial, and documenting the historical reasons for
+comment|// this specific behavior is not worth it. Remove this class instead.
 name|ReviewDb
 name|db
 init|=
@@ -212,9 +188,6 @@ operator|new
 name|NoChangesReviewDb
 argument_list|()
 decl_stmt|;
-comment|// Second create the wrappers which can be removed by ReviewDbUtil#unwrapDb(ReviewDb).
-comment|// ReviewDb is no longer supported, make sure that any attempt to read a change from ReviewDb
-comment|// anyway fails with an exception.
 name|db
 operator|=
 operator|new
