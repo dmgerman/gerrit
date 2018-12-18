cdf852aea910835b0b30d7cc87f5fac95ddac30f
Dave Borowitz <dborowitz@google.com>
2018-12-18 10:50:22 -0500

    Fix case-insensitive searching of hashtags
    The index field has always been stored lowercase, since we intended
    hashtag searches to be case-insensitive. However, we never converted the
    input string to lower case, with the unexpected result that searching
    for [hashtag:ACamelCaseTag] would not return results for changes who
    contain that exact hashtag.
    
    Change-Id: I86019d18fd0d183d857d5a91d2af20d3445c6c25
    (cherry picked from commit 618e1b908b6639a09caf2165235ad672ceaab034 [formerly c8d1c47f1fe2b8807850c35af5dcdb1a5ca10862])
    
    Former-commit-id: d9e1b93daaf9e2386e4c2a487e902bef6a70df5f

--- javatests/com/google/gerrit/server/query/change/AbstractQueryChangesTest.java
+++ javatests/com/google/gerrit/server/query/change/AbstractQueryChangesTest.java
@@ -12354,6 +12354,8 @@ argument_list|,
 literal|"bar"
 argument_list|,
 literal|"a tag"
+argument_list|,
+literal|"ACamelCaseTag"
 argument_list|)
 expr_stmt|;
 name|gApi
@@ -12503,6 +12505,30 @@ expr_stmt|;
 name|assertQuery
 argument_list|(
 literal|"hashtag:\"# #a tag\""
+argument_list|,
+name|changes
+operator|.
+name|get
+argument_list|(
+literal|1
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertQuery
+argument_list|(
+literal|"hashtag:acamelcasetag"
+argument_list|,
+name|changes
+operator|.
+name|get
+argument_list|(
+literal|1
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertQuery
+argument_list|(
+literal|"hashtag:ACamelCaseTAg"
 argument_list|,
 name|changes
 operator|.
