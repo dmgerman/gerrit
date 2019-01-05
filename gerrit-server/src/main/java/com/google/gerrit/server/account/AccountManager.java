d65ccf9ea2e186896ab9a6a11b16373ed6536d03
David Ostrovsky <david@ostrovsky.org>
2019-01-05 17:04:59 +0100

    Revert "Fix the missing DB entry in Gerrit DB"
    This reverts commit 56e68da66b125f2fc142654f328f9e9afa12fbb0 [formerly fcc90699fdec17b941a473221d228c832a533fc0].
    
    Reason for revert: Security vulnerability for OAuth and OpenID auth
    schemes.
    
    OAuth and OpenID authentication schemes support multiple identity
    providers, e.g.: CAS-OAuth2 and GitHub-OAuth2. An attacker can easily
    impersonate existing Gerrit user by creating account on a different
    provider with exactly the same username as the existing Gerrit account.
    Instead of creating a fresh new user, the new account is erroneously
    linked to the existing Gerrit account, even though, account linking
    feature was not triggered from the Gerrit UI.
    
    The original commit tried to fix intermittent database corruption
    problem, with missing record in the database, in the context of single
    identity provider (LDAP) where such problem doesn't exist, as there is
    no way that one single username can belong to physical different users.
    Nevertheless, there should be found another workaround, as trying to
    recover on the fly and introducing severe security breach for other auth
    schemes supported in Gerrit.
    
    If all else fails, the missing database record has to be inserted
    manually and the corresponding account must be re-indexed.
    
    Bug: Issue 7652
    Bug: Issue 10242
    Change-Id: Icba3452c153b2ae3cc1a4ebc569342641f38c07c
    
    Former-commit-id: 2afce52141f7495d6867a9aeb0bcbd0c7494288f

--- gerrit-server/src/main/java/com/google/gerrit/server/account/AccountManager.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/AccountManager.java
@@ -68,24 +68,6 @@ end_package
 
 begin_import
 import|import static
-name|com
-operator|.
-name|google
-operator|.
-name|gerrit
-operator|.
-name|server
-operator|.
-name|account
-operator|.
-name|ExternalId
-operator|.
-name|SCHEME_USERNAME
-import|;
-end_import
-
-begin_import
-import|import static
 name|java
 operator|.
 name|util
@@ -809,90 +791,8 @@ operator|==
 literal|null
 condition|)
 block|{
-if|if
-condition|(
-name|who
-operator|.
-name|getUserName
-argument_list|()
-operator|!=
-literal|null
-condition|)
-block|{
-name|ExternalId
-operator|.
-name|Key
-name|key
-init|=
-name|ExternalId
-operator|.
-name|Key
-operator|.
-name|create
-argument_list|(
-name|SCHEME_USERNAME
-argument_list|,
-name|who
-operator|.
-name|getUserName
-argument_list|()
-argument_list|)
-decl_stmt|;
-name|ExternalId
-name|existingId
-init|=
-name|findExternalId
-argument_list|(
-name|db
-argument_list|,
-name|key
-argument_list|)
-decl_stmt|;
-if|if
-condition|(
-name|existingId
-operator|!=
-literal|null
-condition|)
-block|{
-comment|// An inconsistency is detected in the database, having a record for scheme
-comment|// "username:"
-comment|// but no record for scheme "gerrit:". Try to recover by linking
-comment|// "gerrit:" identity to the existing account.
-name|log
-operator|.
-name|warn
-argument_list|(
-literal|"User {} already has an account; link new identity to the existing account."
-argument_list|,
-name|who
-operator|.
-name|getUserName
-argument_list|()
-argument_list|)
-expr_stmt|;
-return|return
-name|link
-argument_list|(
-name|existingId
-operator|.
-name|accountId
-argument_list|()
-argument_list|,
-name|who
-argument_list|)
-return|;
-block|}
-block|}
 comment|// New account, automatically create and return.
 comment|//
-name|log
-operator|.
-name|debug
-argument_list|(
-literal|"External ID not found. Attempting to create new account."
-argument_list|)
-expr_stmt|;
 return|return
 name|create
 argument_list|(
@@ -2247,13 +2147,6 @@ name|open
 argument_list|()
 init|)
 block|{
-name|log
-operator|.
-name|debug
-argument_list|(
-literal|"Link another authentication identity to an existing account"
-argument_list|)
-expr_stmt|;
 name|ExternalId
 name|extId
 init|=
@@ -2296,13 +2189,6 @@ literal|"Identity in use by another account"
 argument_list|)
 throw|;
 block|}
-name|log
-operator|.
-name|debug
-argument_list|(
-literal|"Updating existing external ID data"
-argument_list|)
-expr_stmt|;
 name|update
 argument_list|(
 name|db
@@ -2315,13 +2201,6 @@ expr_stmt|;
 block|}
 else|else
 block|{
-name|log
-operator|.
-name|debug
-argument_list|(
-literal|"Linking new external ID to the existing account"
-argument_list|)
-expr_stmt|;
 name|externalIdsUpdateFactory
 operator|.
 name|create
