bf740350d9a34fbe77abef81bd49381afa971752
Dave Borowitz <dborowitz@google.com>
2019-02-22 09:31:51 -0800

    Move CommitSubject and ObjectIdSubject to git.testing package
    They belong there right next to the JGit PushResultSubject, they were
    just put in the wrong place initially.
    
    Change-Id: Iaef009136888c813f1bd20b7988e4f59259e860e
    
    Former-commit-id: c079ccd23a83bc24646c1358183ea2eaaadb61f6

--- java/com/google/gerrit/git/testing/CommitSubject.java
+++ java/com/google/gerrit/git/testing/CommitSubject.java
@@ -52,7 +52,7 @@ comment|// limitations under the License.
 end_comment
 
 begin_package
-DECL|package|com.google.gerrit.server.testing
+DECL|package|com.google.gerrit.git.testing
 package|package
 name|com
 operator|.
@@ -60,7 +60,7 @@ name|google
 operator|.
 name|gerrit
 operator|.
-name|server
+name|git
 operator|.
 name|testing
 package|;
