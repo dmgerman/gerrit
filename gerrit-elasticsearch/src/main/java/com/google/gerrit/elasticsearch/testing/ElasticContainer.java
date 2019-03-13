f672f5d16ca54a3733d2a94aa2e2edabc9eb9897
Marco Miller <marco.miller@ericsson.com>
2019-03-13 11:42:43 -0400

    ElasticContainer: Update the 5.6 version to latest
    There were no breaking changes since the previously used version,
    according to the release notes [1] and [2]. Considering the latter as
    well since the former was just an overall Elastic stack version update
    for Elasticsearch.
    
    [1] https://www.elastic.co/guide/en/elasticsearch/reference/5.6/release-notes-5.6.15.html
    [2] https://www.elastic.co/guide/en/elasticsearch/reference/5.6/release-notes-5.6.14.html
    
    Change-Id: I7fe9f99265958f8d600c982e131134c3b5a484db
    
    Former-commit-id: 43455de277c1d876a7968ab1fcb044e8ed9748bb

--- gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/testing/ElasticContainer.java
+++ gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/testing/ElasticContainer.java
@@ -272,7 +272,7 @@ case|case
 name|V5_6
 case|:
 return|return
-literal|"docker.elastic.co/elasticsearch/elasticsearch:5.6.13"
+literal|"docker.elastic.co/elasticsearch/elasticsearch:5.6.15"
 return|;
 case|case
 name|V6_2
