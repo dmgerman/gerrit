43f3b8460a46fb7c7da1cf65735fb40c6f270ed0
David Pursehouse <dpursehouse@collab.net>
2019-03-25 15:35:18 +0100

    RestApiException: Fix Javadoc
    Change-Id: I63f5de3d78aac5a709fa353984fe6e4c9f3a15e8
    
    Former-commit-id: e1b6b3db95e3859ac6b5f8350753257b1393ea49

--- java/com/google/gerrit/extensions/restapi/RestApiException.java
+++ java/com/google/gerrit/extensions/restapi/RestApiException.java
@@ -67,7 +67,7 @@ package|;
 end_package
 
 begin_comment
-comment|/** Root exception type for JSON API failures. */
+comment|/** Root exception type for REST API failures. */
 end_comment
 
 begin_class
