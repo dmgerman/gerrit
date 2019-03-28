f6b09b7bef8f7eee8f01523011a5c81e12ec1baf
Marco Miller <marco.miller@ericsson.com>
2019-03-27 20:20:55 -0400

    ElasticContainer: Use version 7.0.0-rc1 for V7_0 tests
    Change-Id: I4d415ca36033098286e32f2de26c7b711972af55
    
    Former-commit-id: 34142466923aa426476ef28e726bc2df583786c5

--- gerrit-elasticsearch/src/test/java/com/google/gerrit/elasticsearch/ElasticContainer.java
+++ gerrit-elasticsearch/src/test/java/com/google/gerrit/elasticsearch/ElasticContainer.java
@@ -231,7 +231,7 @@ case|case
 name|V7_0
 case|:
 return|return
-literal|"docker.elastic.co/elasticsearch/elasticsearch-oss:7.0.0-beta1"
+literal|"docker.elastic.co/elasticsearch/elasticsearch-oss:7.0.0-rc1"
 return|;
 block|}
 throw|throw
