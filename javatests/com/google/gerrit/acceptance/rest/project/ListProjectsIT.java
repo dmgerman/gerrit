8222e844778a96c934e3cc9678b37d3b875fa0e7
Dave Borowitz <dborowitz@google.com>
2019-05-24 10:16:41 -0700

    Error Prone: Fix DefaultCharset in ListProjectsIT
    Change-Id: Ia2fbb143d47031f0692854505e99f1be63cb0408
    
    Former-commit-id: 229ee21afa0ddddbe5a21636c97e3d13f353bd37

--- javatests/com/google/gerrit/acceptance/rest/project/ListProjectsIT.java
+++ javatests/com/google/gerrit/acceptance/rest/project/ListProjectsIT.java
@@ -126,6 +126,20 @@ begin_import
 import|import static
 name|java
 operator|.
+name|nio
+operator|.
+name|charset
+operator|.
+name|StandardCharsets
+operator|.
+name|UTF_8
+import|;
+end_import
+
+begin_import
+import|import static
+name|java
+operator|.
 name|util
 operator|.
 name|stream
@@ -1311,6 +1325,8 @@ name|displayOut
 operator|.
 name|toByteArray
 argument_list|()
+argument_list|,
+name|UTF_8
 argument_list|)
 argument_list|)
 decl_stmt|;
@@ -1481,6 +1497,8 @@ name|displayOut
 operator|.
 name|toByteArray
 argument_list|()
+argument_list|,
+name|UTF_8
 argument_list|)
 decl_stmt|;
 name|Gson
