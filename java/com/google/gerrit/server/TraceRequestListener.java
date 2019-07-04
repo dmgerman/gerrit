7d4ecbdb05127d90b7f6e55aadee1503edcfed9a
Edwin Kempin <ekempin@google.com>
2019-07-04 08:35:58 +0200

    Add a request listener that sets a logging tag with the project
    If a request is done for a project we are now setting a logging tag with
    the project name that is associated with each log that is done for the
    request. This way one can easily see for all errors/warnings in the log
    for which project they occurred.
    
    Future changes may add advanced tracing capabilities to the newly added
    TraceRequestListener. E.g. there could be configuration that enables
    tracing for a certain user or project. TraceRequestListener could read
    the config and then enable the tracing if the user/project matches.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I7401c1bcef5c16039401013445dea32432d9098e
    
    Former-commit-id: e41d0cf4aac714f53520892ed453d96095397099

---
+++ java/com/google/gerrit/server/TraceRequestListener.java
@@ -0,0 +1,127 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|// Copyright (C) 2019 The Android Open Source Project
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Licensed under the Apache License, Version 2.0 (the "License");
+end_comment
+
+begin_comment
+comment|// you may not use this file except in compliance with the License.
+end_comment
+
+begin_comment
+comment|// You may obtain a copy of the License at
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// http://www.apache.org/licenses/LICENSE-2.0
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Unless required by applicable law or agreed to in writing, software
+end_comment
+
+begin_comment
+comment|// distributed under the License is distributed on an "AS IS" BASIS,
+end_comment
+
+begin_comment
+comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+end_comment
+
+begin_comment
+comment|// See the License for the specific language governing permissions and
+end_comment
+
+begin_comment
+comment|// limitations under the License.
+end_comment
+
+begin_package
+DECL|package|com.google.gerrit.server
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|gerrit
+operator|.
+name|server
+package|;
+end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|inject
+operator|.
+name|Singleton
+import|;
+end_import
+
+begin_class
+annotation|@
+name|Singleton
+DECL|class|TraceRequestListener
+specifier|public
+class|class
+name|TraceRequestListener
+implements|implements
+name|RequestListener
+block|{
+annotation|@
+name|Override
+DECL|method|onRequest (RequestInfo requestInfo)
+specifier|public
+name|void
+name|onRequest
+parameter_list|(
+name|RequestInfo
+name|requestInfo
+parameter_list|)
+block|{
+name|requestInfo
+operator|.
+name|project
+argument_list|()
+operator|.
+name|ifPresent
+argument_list|(
+name|p
+lambda|->
+name|requestInfo
+operator|.
+name|traceContext
+argument_list|()
+operator|.
+name|addTag
+argument_list|(
+literal|"project"
+argument_list|,
+name|p
+argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+end_class
+
+end_unit
+
