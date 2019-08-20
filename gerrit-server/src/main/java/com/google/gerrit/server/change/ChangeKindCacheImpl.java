988d7af7f243b89b4aede5ad88d3ecf2876cdc90
Edwin Kempin <ekempin@google.com>
2019-08-20 13:29:20 +0200

    Fix detecting changes of parent trees when computing change kind for merge commit
    A new patch set of a merge change is considered as NO_CHANGE if the
    commits have the same delta and trees. For merge commits this includes
    comparing the trees of the parent commits.
    
    The comparison of the trees of the parent commits was broken, since we
    forgot to parse the parent commits before accessing their trees. If a
    commit is not parsed the tree information is not available and the
    getTree() method returns null. Since Objects.equals(null, null) returns
    true, we wrongly assumed that the parent trees didn't change. Due to
    this new patch sets of merge changes were detected as NO_CHANGE even if
    the tree of the second parent was changed.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: Ib4ea0501bf5674e479f23510d8e2ceadcb025c3d
    
    Former-commit-id: f4e432bfc8ccd9952acc52d4d27dfae086ceba56

--- gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeKindCacheImpl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeKindCacheImpl.java
@@ -1588,6 +1588,8 @@ if|if
 condition|(
 name|isSameDeltaAndTree
 argument_list|(
+name|rw
+argument_list|,
 name|prior
 argument_list|,
 name|next
@@ -1610,6 +1612,8 @@ if|if
 condition|(
 name|isSameDeltaAndTree
 argument_list|(
+name|rw
+argument_list|,
 name|prior
 argument_list|,
 name|next
@@ -1985,18 +1989,23 @@ name|toSet
 argument_list|()
 return|;
 block|}
-DECL|method|isSameDeltaAndTree (RevCommit prior, RevCommit next)
+DECL|method|isSameDeltaAndTree (RevWalk rw, RevCommit prior, RevCommit next)
 specifier|private
 specifier|static
 name|boolean
 name|isSameDeltaAndTree
 parameter_list|(
+name|RevWalk
+name|rw
+parameter_list|,
 name|RevCommit
 name|prior
 parameter_list|,
 name|RevCommit
 name|next
 parameter_list|)
+throws|throws
+name|IOException
 block|{
 if|if
 condition|(
@@ -2067,6 +2076,31 @@ name|i
 operator|++
 control|)
 block|{
+comment|// Parse parent commits so that their trees are available.
+name|rw
+operator|.
+name|parseCommit
+argument_list|(
+name|prior
+operator|.
+name|getParent
+argument_list|(
+name|i
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|rw
+operator|.
+name|parseCommit
+argument_list|(
+name|next
+operator|.
+name|getParent
+argument_list|(
+name|i
+argument_list|)
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|next
