d72c6b1eef91de168f6f7cc8734a4938bee87c68
Han-Wen Nienhuys <hanwen@google.com>
2019-10-30 17:15:39 +0000

    Revert "restapi: add an alternative CreateChange endpoint"
    This reverts commit 651b28e9f5d33315644f17a63b5a2b350820fc90 [formerly 44c724e185247d075500212b29ba0d009b2d2011].
    
    Reason for revert: this did not fix our slowness problem, which
    was slow CreateChange calls on Android's platform/frameworks/base
    when specifying a merge. On closer inspection, generating the merge
    reads too much data.
    
    Change-Id: I9fb9a074bc7ae9da13ef262a5a9a7a2596ec0387
    
    Former-commit-id: fccb35cf19a5abb0193864f49e92b16d2c6d792a

--- javatests/com/google/gerrit/acceptance/rest/binding/ProjectsRestApiBindingsIT.java
+++ javatests/com/google/gerrit/acceptance/rest/binding/ProjectsRestApiBindingsIT.java
@@ -625,13 +625,6 @@ argument_list|)
 argument_list|,
 name|RestCall
 operator|.
-name|post
-argument_list|(
-literal|"/projects/%s/create.change"
-argument_list|)
-argument_list|,
-name|RestCall
-operator|.
 name|get
 argument_list|(
 literal|"/projects/%s/children"
